7a86d9ff1a80784413ccbe8a92e6299ff2424234
Kunal Mehta <legoktm@gmail.com>
2015-03-11 13:11:14 -0700

    Simplify deploying zuul with fabric
    fabric helps automate tasks on remote machines. It's been useful in managing
    and deploying labs tools, so I set one up for zuul.
    
    It follows the same procedure outlined at:
    <https://www.mediawiki.org/wiki/Continuous_integration/Zuul#Deploy_configuration>
    
    Steps to use:
    * Install fabric
    * Configure your .ssh/config
    * Run $ fab deploy_zuul
    
    Change-Id: Ie98ce427c81aa4a9e8f4b3379bec71c5366b4c70

---
+++ fabfile.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python2
+"""
+Usage:
+
+* Install fabric (<http://www.fabfile.org/>) via
+  pip install --user fabric or a virtualenv
+* Configure your .ssh/config so it can
+  access gallium.wikimedia.org and uses the
+  proper username and key.
+* Run $ fab deploy_zuul
+
+"""
+from fabric.api import *  # noqa
+from fabric.contrib.console import confirm
+
+env.hosts = ['gallium.wikimedia.org']
+env.sudo_user = 'zuul'
+env.sudo_prefix = 'sudo -ni '
+env.use_ssh_config = True
+
+
+@task
+def deploy_zuul():
+    with cd('/etc/zuul/wikimedia'):
+        sudo('git remote update')
+        sudo('git --no-pager log -p HEAD..origin/master zuul')
+        if confirm('Does the diff look good?'):
+            sudo('git rebase')
+            sudo('/etc/init.d/zuul reload')
