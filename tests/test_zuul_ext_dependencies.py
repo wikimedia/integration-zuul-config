e81ad6c7897044816aa740a24790aa48271c4087
Kunal Mehta <legoktm@gmail.com>
2015-04-28 09:24:09 -0700

    Use generic phpunit job for extensions with dependencies
    We use a custom zuul function that stores the mapping of extensions to
    their dependencies and adds them to an environment variable when the job
    is triggered.
    
    Currently only configured for the "Thanks" extension for testing.
    
    Bug: T96690
    Change-Id: I37c92630fa7809caeead0c15353aaad1a399eb17

---
+++ tests/test_zuul_ext_dependencies.py
@@ -0,0 +1,50 @@
+import os
+import unittest
+
+from fakes import FakeJob
+
+dependencies = {}  # defined for flake8
+set_ext_dependencies = None  # defined for flake8
+# Import function
+execfile(os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),
+    '../zuul/ext_dependencies.py'))
+
+
+class TestExtDependencies(unittest.TestCase):
+    def assertHasDependencies(self, params):
+        self.assertIn('EXT_DEPENDENCIES', params)
+
+    def assertMissingDependencies(self, params):
+        self.assertNotIn('EXT_DEPENDENCIES', params)
+
+    def fetch_dependencies(self, job_name=None, project=None):
+        if project:
+            params = {'ZUUL_PROJECT': project}
+        else:
+            params = {'ZUUL_PROJECT': 'mediawiki/extensions/Example'}
+        job = FakeJob(job_name if job_name else 'mwext-testextension-hhvm')
+        set_ext_dependencies(None, job, params)
+        return params
+
+    def test_resolvable_dependencies(self):
+        """verifies that we can resolve all of the dependencies"""
+        for ext_name in dependencies:
+            self.assertHasDependencies(self.fetch_dependencies(
+                project='mediawiki/extensions/' + ext_name))
+
+    def test_job_name(self):
+        self.assertHasDependencies(self.fetch_dependencies(
+            job_name='mwext-testextension-hhvm'))
+        self.assertMissingDependencies(self.fetch_dependencies(
+            job_name='mediawiki-core-phplint'))
+
+    def test_zuul_project_name(self):
+        self.assertHasDependencies(self.fetch_dependencies(
+            project='mediawiki/extensions/Example'))
+        self.assertMissingDependencies(self.fetch_dependencies(
+            project='mediawiki/extensions'))
+        self.assertMissingDependencies(self.fetch_dependencies(
+            project='mediawiki/extensions/Example/vendor'))
+        self.assertMissingDependencies(self.fetch_dependencies(
+            project='foo/bar/baz'))
