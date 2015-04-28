7bea9df103f7ca2f5f0a9d73392fb14adffc3859
Kunal Mehta <legoktm@gmail.com>
2015-04-28 09:24:09 -0700

    Use generic phpunit job for extensions with dependencies
    We use a custom zuul function that stores the mapping of extensions to
    their dependencies and adds them to an environment variable when the job
    is triggered.
    
    Currently only configured for the "Thanks" extension for testing.
    
    Bug: T96690
    Change-Id: I37c92630fa7809caeead0c15353aaad1a399eb17

---
+++ zuul/ext_dependencies.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python2
+
+dependencies = {
+    'Flow': ['AbuseFilter', 'CheckUser', 'Mantle',
+             'MobileFrontend', 'SpamBlacklist', 'Echo', 'EventLogging'],
+    'Thanks': ['Echo', 'Flow'],
+}
+
+
+def set_ext_dependencies(item, job, params):
+    """
+    Reads dependencies from the yaml file and adds them as a parameter
+    :type item: zuul.model.QueueItem
+    :type job: zuul.model.Job
+    :type params: dict
+    """
+    if not job.name.startswith(('mwext-testextension', 'mwext-qunit')):
+        return
+
+    if not params['ZUUL_PROJECT'].startswith('mediawiki/extensions/'):
+        return
+    # mediawiki/extensions/FooBar
+    split = params['ZUUL_PROJECT'].split('/')
+    if len(split) != 3:
+        # mediawiki/extensions/FooBar/blah
+        # mediawiki/extensions
+        return
+
+    # FooBar
+    ext_name = split[-1]
+    deps = get_dependencies(ext_name, dependencies)
+    # Export with a literal \n character and have bash expand it later
+    params['EXT_DEPENDENCIES'] = '\\n'.join(sorted(deps))
+
+
+def get_dependencies(ext_name, mapping):
+    """
+    Get the full set of dependencies required by an extension
+    :param ext_name: extension name
+    :param mapping: mapping of extensions to their dependencies
+    :return: set of dependencies, recursively processed
+    """
+    deps = set()
+    if ext_name in mapping:
+        for dep in mapping[ext_name]:
+            deps.add(dep)
+            # TODO: Max recursion limit?
+            deps = deps.union(get_dependencies(dep, mapping))
+    return deps
