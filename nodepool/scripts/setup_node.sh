a42fe057595dab09e1a5db34eed3df69761472a6
Antoine Musso <hashar@free.fr>
2015-09-18 14:52:30 +0200

    nodepool: run integration/config puppet manifest on setup
    The ciimage.pp is not applied on setup. Thus the snapshot images are outdated.
    
    Hold a mirror of integration/config in the image
    Update the puppet repo
    Apply our /dib/puppet/ciimage.pp manifest
    
    Bug: T112821
    Change-Id: I540bd6b724854c1f5e7ea28ea4b8b35d4ee1e643

--- nodepool/scripts/setup_node.sh
+++ nodepool/scripts/setup_node.sh
@@ -15,3 +15,14 @@ echo "127.0.0.1 $HOSTNAME" | sudo tee -a /etc/hosts
 
 # https://review.openstack.org/#/c/222759/
 sudo mkdir -p /opt/nodepool-scripts
+
+echo "Cloning integration/config"
+mkdir -p /opt/git/integration
+git clone /srv/git/integration/config.git /opt/git/integration
+git -C /opt/git/integration/config remote set-url origin https://gerrit.wikimedia.org/r/p/integration/config.git
+git -C /opt/git/integration/config pull
+
+echo "Running puppet"
+git -C /puppet pull
+
+/usr/local/bin/puppet-apply /opt/git/integration/config/dib/puppet/ciimage.pp
