e07c4b07c0ea082f20c8c5a7b0dd5958d26363ad
Antoine Musso <hashar@free.fr>
2015-09-24 17:19:05 +0200

    nodepoo: realize puppet ::apt first
    Use puppet stages to make sure ::apt is realized first. Else packages
    that are in jessie-wikimedia/backports (for example), will not be found.
    
    Change-Id: I9edfe66ab5e6bff2a2fa5d8b6ad7f56610bfbaa5

--- dib/puppet/ciimage.pp
+++ dib/puppet/ciimage.pp
@@ -2,6 +2,14 @@
 #
 # Use operations/puppet.git classes to build our CI image.
 
-include apt
+# Needs ::apt first so we get jessie-wikimedia and jessie-backports configured
+# and pinned. Else our packages are not up-to-date / not found.
+stage { 'first':
+    before => Stage['main'],
+}
+class { '::apt':
+    stage => first,
+}
+
 include contint::packages::base
 include contint::packages::python
