a82e67524d6f177a92049bcb1393f86d7fe59145
Kunal Mehta <legoktm@member.fsf.org>
2016-02-10 00:22:07 -0800

    Test to verify skins do not run 'testextension' jobs
    Bug: T117710
    Change-Id: Idafea874b3b337d2656d421d91630248e27378de

--- tests/test_zuul_scheduler.py
+++ tests/test_zuul_scheduler.py
@@ -165,6 +165,14 @@ class TestZuulScheduler(unittest.TestCase):
             % (name, pipeline)
             )
 
+    def assertProjectHasNoExtensionTests(self, name, definition, pipeline):
+        self.assertFalse(
+            all([job for job in definition
+                 if 'testextension' in job]),
+            'Project %s pipeline %s cannot have "testextension" jobs'
+            % (name, pipeline)
+        )
+
     def test_repos_have_required_jobs(self):
         repos = {
             'mediawiki/core$': [
@@ -179,7 +187,8 @@ class TestZuulScheduler(unittest.TestCase):
             'mediawiki/skins/': [
                 self.assertProjectHasComposerValidate,
                 self.assertProjectHasPhplint,
-                self.assertProjectHasSkinTests
+                self.assertProjectHasSkinTests,
+                self.assertProjectHasNoExtensionTests
             ],
             'mediawiki/vendor$': [
                 self.assertProjectHasComposerValidate,
