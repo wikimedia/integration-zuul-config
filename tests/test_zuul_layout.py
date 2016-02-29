22ed61041a627bc52e5e91916a7e7ffd539b712b
Paladox <thomasmulhall410@yahoo.com>
2016-02-29 16:19:02 +0000

    Remove extension-jslint test from zuul
    Change-Id: I638d5909d7d013269e31a6d1ff63094c09290dfd

--- tests/test_zuul_layout.py
+++ tests/test_zuul_layout.py
@@ -20,18 +20,3 @@ class TestZuulLayout(unittest.TestCase):
             '../zuul/layout.yaml')
         with open(wmf_zuul_layout, 'r') as f:
             cls.layout = yaml.load(f)
-
-    def test_mwext_legacy_jslint_replaced_by_npm(self):
-        exts = sorted([
-            p['name']
-            for p in self.layout['projects']
-            if p['name'].startswith('mediawiki/extensions/')
-            and {'name': 'npm'} in p.get('template', [])
-            and {'name': 'extension-jslint'} in p.get('template', [])
-        ])
-
-        self.maxDiff = None
-        self.longMessage = True
-        self.assertListEqual(
-            [], exts,
-            'MediaWiki extensions having npm do not need "extension-jslint"')
