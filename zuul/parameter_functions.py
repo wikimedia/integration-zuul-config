02b86e1d42bc090461b6cd3a058405e91e512fb7
Paladox <thomasmulhall410@yahoo.com>
2016-03-03 20:19:00 +0000

    [SemanticSifter] Add dependencie on SemanticMediaWiki
    20:20:57 No syntax errors detected in
    /mnt/jenkins-workspace/workspace/mwext-testextension-php55-non-voting/src/LocalSettings.php
    20:20:57 [mwext-testextension-php55-non-voting] $ /bin/bash -xe
    /tmp/hudson6594842695402327737.sh
    20:20:57 +
    /srv/deployment/integration/slave-scripts/bin/mw-run-update-script.sh
    20:20:57 SemanticSifter extension requires SemanticMediaWiki
    20:20:57 Build step 'Execute shell' marked build as failure
    20:20:57 Recording test results
    20:20:57 ERROR: Step ‘Publish JUnit test result report’ failed: No test
    report files were found. Configuration error?
    20:20:57 [PostBuildScript] - Execution post build scripts.
    20:20:57 [mwext-testextension-php55-non-voting] $ /bin/bash -xe
    /tmp/hudson2671691361295237082.sh
    20:20:57 +
    /srv/deployment/integration/slave-scripts/bin/mw-teardown-mysql.sh
    
    Change-Id: I70aca227bfaf09e236341bd313f3769294d9fc1b

--- zuul/parameter_functions.py
+++ zuul/parameter_functions.py
@@ -88,6 +88,7 @@ dependencies = {
     'RelatedArticles': ['BetaFeatures', 'Cards', 'MobileFrontend'],
     'Score': ['VisualEditor'],
     'SemanticFormsInputs': ['SemanticForms'],
+    'SemanticSifter': ['SemanticMediaWiki'],
     'SimpleSurvey': ['PrefSwitch'],
     'SolrStore': ['SemanticMediaWiki'],
     'Spreadsheet': ['PHPExcel'],
