511e91bf5fa72d39e090bd471e446df66b940d03
Paladox <thomasmulhall410@yahoo.com>
2016-05-24 16:51:11 +0100

    Follow-Up: I4eebe5d812d3994a61b133e9d214b52b8b51e71b
    Forgot to update parameter_functions.py file so nodepool_params will run
    npm-run-{script}-node-4.3. By changing it to node-4.3 all npm tests that
    are npm-node-4.3 or npm-run-{script}-node-4.3 will work now
    
    Better to just do node-4.3 so we doint have to add it to all the tests.
    
    Also added node-0.10 required for
    I4f2048625e9c568180393b636650430f3d8f004b
    
    Bug: T128091
    Change-Id: I369a3cb21379567aac2c82533bced458b634c352

--- zuul/parameter_functions.py
+++ zuul/parameter_functions.py
@@ -43,7 +43,7 @@ def set_parameters(item, job, params):
         nodepool_params(item, job, params)
     if job.name.endswith('-trusty'):
         nodepool_params(item, job, params)
-    elif job.name.endswith('npm-node-4.3'):
+    elif job.name.endswith('node-4.3'):
         nodepool_params(item, job, params)
     elif job.name in ['integration-jjb-config-diff']:
         nodepool_params(item, job, params)
