04382ad7f0ca7f0566c6d42671671a754840b36a
Paladox <thomasmulhall410@yahoo.com>
2016-05-31 17:08:11 +0100

    Enable zuul/jenkins to run unit tests in skin projects
    This patch will allow us to test skins like we do with extensions. But it
    will test differently to extensions due to the fact skins are different to
    extensions.
    
    Skins can also depend on other skins to test against same as extensions
    can.
    
    Ive also added mediawiki-skins-(qunit|phpflavour) to gate and submit.
    
    It is experimental so i have not added it as a test in mw core just in
    experimental:
    
    Metrolook, MonoBook and Vector are in gate and submit for testing but will
    only run if you do check experimental
    
    Depends-On: Iac5b1be23dd9b657f43bd7d5a3fe5ddff326e25f
    
    Bug: T68926
    Change-Id: I1be4454b1aa7935ec2e9ac3703eee3907e5101e8

--- tests/test_zuul_skin_dependencies.py
+++ tests/test_zuul_skin_dependencies.py
@@ -3,8 +3,8 @@ import unittest
 
 from fakes import FakeJob
 
-ext_dependencies = {}  # defined for flake8
-get_ext_dependencies = None  # defined for flake8
+skin_dependencies = {}  # defined for flake8
+get_skin_dependencies = None  # defined for flake8
 set_parameters = None  # defined for flake8
 # Import function
 execfile(os.path.join(
@@ -12,28 +12,28 @@ execfile(os.path.join(
     '../zuul/parameter_functions.py'))
 
 
-class TestExtDependencies(unittest.TestCase):
+class TestSkinDependencies(unittest.TestCase):
     def assertHasDependencies(self, params):
-        self.assertIn('EXT_DEPENDENCIES', params)
+        self.assertIn('SKIN_DEPENDENCIES', params)
 
     def assertMissingDependencies(self, params):
-        self.assertNotIn('EXT_DEPENDENCIES', params)
+        self.assertNotIn('SKIN_DEPENDENCIES', params)
 
     def fetch_dependencies(self, job_name=None, project=None):
         if project:
             params = {'ZUUL_PROJECT': project}
         else:
-            params = {'ZUUL_PROJECT': 'mediawiki/extensions/Example'}
-        job = FakeJob(job_name if job_name else 'mwext-testextension-hhvm')
+            params = {'ZUUL_PROJECT': 'mediawiki/skins/Example'}
+        job = FakeJob(job_name if job_name else 'mwskin-testskin-hhvm')
         set_parameters(None, job, params)
         return params
 
-    def test_ext_name(self):
+    def test_skin_name(self):
         params = self.fetch_dependencies(
-            project='mediawiki/extensions/Example')
+            project='mediawiki/skins/Example')
 
-        self.assertIn('EXT_NAME', params)
-        self.assertEqual(params['EXT_NAME'], 'Example')
+        self.assertIn('SKIN_NAME', params)
+        self.assertEqual(params['SKIN_NAME'], 'Example')
 
     def test_cyclical_dependencies(self):
         """verifies that cyclical dependencies are possible"""
@@ -41,31 +41,31 @@ class TestExtDependencies(unittest.TestCase):
         mapping = {'Foo': ['Bar'], 'Bar': ['Foo']}
 
         self.assertEqual(
-                         get_ext_dependencies('Foo', mapping), set(['Foo', 'Bar'])
+                         get_skin_dependencies('Foo', mapping), set(['Foo', 'Bar'])
                          )
 
     def test_resolvable_dependencies(self):
         """verifies that we can resolve all of the dependencies"""
-        for ext_name in ext_dependencies:
+        for skin_name in skin_dependencies:
             self.assertHasDependencies(self.fetch_dependencies(
-                project='mediawiki/extensions/' + ext_name))
+                project='mediawiki/skins/' + skin_name))
 
     def test_job_name(self):
         self.assertHasDependencies(self.fetch_dependencies(
-            job_name='mwext-testextension-hhvm'))
+            job_name='mwskin-testskin-hhvm'))
         self.assertHasDependencies(self.fetch_dependencies(
-            job_name='mwext-qunit'))
+            job_name='mwskin-qunit'))
         self.assertHasDependencies(self.fetch_dependencies(
-            job_name='mwext-mw-selenium'))
+            job_name='mwskin-mw-selenium'))
         self.assertMissingDependencies(self.fetch_dependencies(
             job_name='mediawiki-core-phplint'))
 
     def test_zuul_project_name(self):
         self.assertHasDependencies(self.fetch_dependencies(
-            project='mediawiki/extensions/Example'))
+            project='mediawiki/skins/Example'))
         self.assertMissingDependencies(self.fetch_dependencies(
-            project='mediawiki/extensions'))
+            project='mediawiki/skins'))
         self.assertMissingDependencies(self.fetch_dependencies(
-            project='mediawiki/extensions/Example/vendor'))
+            project='mediawiki/skins/Example/vendor'))
         self.assertMissingDependencies(self.fetch_dependencies(
             project='foo/bar/baz'))
