b8daede5bfaaebe516601cdfbcd88c8a3ecce784
Antoine Musso <hashar@free.fr>
2016-10-06 17:29:06 +0200

    dib: craft our own hiera lookup data
    When building an image, we dont rely on external datasources such as
    wikitech or httpyaml on the labs-puppetmaster.  The first is never used,
    and the second is unreacheable from the internet.
    
    Craft our own custom little hiera configuration which solely rely on
    puppet.git /hieradata/
    
    That fix build on Jessie.
    
    Change-Id: I2baaa225521c5648f957d3e9678f62b45ee8011d

--- dib/puppet/bootstrap.pp
+++ dib/puppet/bootstrap.pp
@@ -5,8 +5,17 @@ package { 'puppetmaster-common':
     ensure => present,
 }
 
-# Have a basic hiera lookup hierarchy. Required by various classes from
-# operations/puppet.git
-class { '::puppetmaster::hiera':
-    source => 'puppet:///modules/puppetmaster/labs.hiera.yaml',
+# Similar to puppetmaster::hiera
+file { '/etc/puppet/hiera.yaml':
+    content => ':backends:
+  - nuyaml
+:nuyaml:
+  :datadir: /etc/puppet/hieradata
+:hierarchy:
+  - "labs/%{::labsproject}/common"
+  - "labs"
+  - "common"
+',
+    require => Package['puppetmaster-common'],
 }
+require_package('puppetmaster-common')
