231aa917e5be8a51fb0ef4917810a2b5f98f400a
harej <jamesmhare@gmail.com>
2016-10-19 06:07:50 -0400

    Adding VisualEditor as a dependency for CollaborationKit.
    Change-Id: I773304a1e30bf625c47fbf23970593d2444a96b3

--- zuul/parameter_functions.py
+++ zuul/parameter_functions.py
@@ -95,7 +95,7 @@ dependencies = {
     'CirrusSearch': ['MwEmbedSupport', 'TimedMediaHandler', 'PdfHandler',
                      'Cite'],
     'CodeEditor': ['WikiEditor'],
-    'CollaborationKit': ['EventLogging'],
+    'CollaborationKit': ['EventLogging', 'VisualEditor'],
     'ContentTranslation': ['Echo', 'EventLogging', 'GuidedTour',
                            'UniversalLanguageSelector', 'Wikidata'],
     'Disambiguator': ['VisualEditor'],
