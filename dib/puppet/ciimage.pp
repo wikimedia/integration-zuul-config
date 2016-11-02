174937769880a8dfd775ac553d3ad81453c7df03
Antoine Musso <hashar@free.fr>
2016-11-02 11:44:37 +0100

    dib: doxygen on Jessie images
    So we can migrate the various documentation generation jobs.
    contint::packages::doxygen has been introduced in puppet.git via
    d32f3c8af06816024f77b561dc5405e1f8214f3b
    
    Bug: T119140
    Change-Id: I1b02ec03ecce8f497c14d5955a3ae2386f1020d2

--- dib/puppet/ciimage.pp
+++ dib/puppet/ciimage.pp
@@ -89,6 +89,7 @@ class apache2_allow_execution {
   }
 }
 if os_version('debian >= jessie') {
+    include contint::packages::doxygen
     include contint::packages::python
 
     # Qunit/Selenium related
