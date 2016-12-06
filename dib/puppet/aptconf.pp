1cc8a366cc6089d0f12bd8f765db930507898e92
Antoine Musso <hashar@free.fr>
2016-12-06 17:08:30 +0100

    dib: ensure /etc/apt/sources.list exists
    diskimage-builder deletes /etc/apt/sources.list however since we have
    enabled backports repository for Debian, our puppet manifests attempt to
    comment out old lines from it. Given the file does not exist, that fails
    and breaks provisioning.
    
    Should be fixed by https://gerrit.wikimedia.org/r/#/c/325570/
    
    Meanwhile touch the file so it exists.
    
    Change-Id: Id35aeb2ea08196eda1c5e92c062c9b6c714b0dcf

--- dib/puppet/aptconf.pp
+++ dib/puppet/aptconf.pp
@@ -3,5 +3,16 @@ $labsproject = 'contintcloud'
 
 require_package('apt-transport-https')
 
+# dib deletes /etc/apt/sources.list however our apt::repository for
+# jessie-backports attempts to comment out old lines from it.
+#
+# Should be fixed by https://gerrit.wikimedia.org/r/#/c/325570/
+#
+exec { 'Ensure sources.list exists':
+    command => '/usr/bin/touch /etc/apt/sources.list',
+    creates => '/etc/apt/sources.list',
+    before  => Class['::apt'],
+}
+
 class { '::apt': }
 include contint::packages::apt
