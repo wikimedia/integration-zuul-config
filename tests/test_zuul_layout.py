4912bc1c6cd15ee3d72aacf7473f4128e7b7e652
Antoine Musso <hashar@free.fr>
2017-01-18 23:33:30 +0100

    test: mw ext/skins have test templates in Zuul
    Add a test that crawl the Zuul layout.yaml files for MediaWiki
    extensions and skins. Make sure each have a Zuul template that contains
    PHPUnit jobs (extension-unittests-*, skin-tests*, extension-gate).
    
    Skip archived repos and sub repositories of extensions.
    
    Change-Id: Iaecaaa3457af4426d130ff25f6bc1100b312f7ef

--- tests/test_zuul_layout.py
+++ tests/test_zuul_layout.py
@@ -10,6 +10,12 @@ import yaml
 
 
 class TestZuulLayout(unittest.TestCase):
+    """
+    Tests solely asserting on the Zuul YAML configuration.
+
+    Notably expansion of templates are not available which is provied by the
+    scheduler and tested via test_zuul_scheduler.py
+    """
 
     layout = None
 
@@ -20,3 +26,35 @@ class TestZuulLayout(unittest.TestCase):
             '../zuul/layout.yaml')
         with open(wmf_zuul_layout, 'r') as f:
             cls.layout = yaml.load(f)
+
+    def test_mediawiki_ext_skins_have_test_templates(self):
+        one_of_templates = (
+            'archived',
+            'extension-unittests',
+            'skin-tests',
+            'extension-gate',
+            )
+        errors = []
+        for project in self.layout['projects']:
+            try:
+                if not project['name'].startswith((
+                        'mediawiki/skins', 'mediawiki/extensions')):
+                    continue
+
+                if len(project['name'].split('/')) != 3:
+                    # Skip sub repositories
+                    continue
+
+                has_extension_unittests = any([
+                    template['name'].startswith(one_of_templates)
+                    for template in project.get('template', {})
+                    ])
+                self.assertTrue(
+                    has_extension_unittests,
+                    'Project %s in Zuul lacks an extension-unittests* '
+                    'template' % project['name'])
+            except AssertionError, e:
+                errors.append(str(e))
+
+        self.maxDiff = None
+        self.assertListEqual([], errors)
