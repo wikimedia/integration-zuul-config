61fb4b50ae07e5c6f0914c21b6f5ba630db52e80
Antoine Musso <hashar@free.fr>
2017-02-23 11:09:39 +0100

    Delete php53lint jobs
    Replace it with php55lint where relevant.
    Do not replace it in check pipelines which will be phased out
    eventually.
    
    Delete:
        mediawiki-core-php53lint
        php53lint
    
    Bug: T158652
    Change-Id: Ie6cf3de265c61cf7b5aba4c11045286fd05286f5

--- tests/test_zuul_scheduler.py
+++ tests/test_zuul_scheduler.py
@@ -133,7 +133,7 @@ class TestZuulScheduler(unittest.TestCase):
     def assertProjectHasPhplint(self, name, definition, pipeline):
         self.assertTrue(
             any([job for job in definition
-                 if job.endswith(('php53lint', 'php55lint')) or
+                 if job.endswith(('php55lint')) or
                  job.startswith('composer-')]),
             'Project %s pipeline %s must have either '
             'phplint or a composer-* job'
