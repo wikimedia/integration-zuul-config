a802bfff7f2c0e9ed3cb92461727615c27b8d1d9
Antoine Musso <hashar@free.fr>
2017-03-01 13:49:41 +0100

    3D depends on MultimediaViewer
    Bug: T159243
    Change-Id: Ie26875fe6cf1c6128e7bf99d5348224d959d3ba1

--- zuul/parameter_functions.py
+++ zuul/parameter_functions.py
@@ -128,6 +128,7 @@ dependencies = {
 
     # Extensions
     # One can add a skin by using: 'skin/XXXXX'
+    '3D': ['MultimediaViewer'],
     'AbuseFilter': ['AntiSpoof'],
     'ApiFeatureUsage': ['Elastica'],
     'Arrays': ['Loops', 'ParserFunctions', 'Variables'],
