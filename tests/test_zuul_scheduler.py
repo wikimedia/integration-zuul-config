f71d22d7ed491e10f73e425ab1fab5ee754b10bc
Antoine Musso <hashar@free.fr>
2017-03-13 16:00:44 +0100

    mwext-testextension-php55-composer to nodepool
    Delete mwext-testextension-php55-composer
    Replace it with mwext-testextension-php55-composer-trusty
    
    Bug: T137199
    Change-Id: Ic994a9839662436a87852f206d486588da1dfe3c

--- tests/test_zuul_scheduler.py
+++ tests/test_zuul_scheduler.py
@@ -124,7 +124,7 @@ class TestZuulScheduler(unittest.TestCase):
         self.assertTrue(
             any([job for job in definition
                  if job.startswith(('composer', 'composer-'))
-                 or job == 'mwext-testextension-php55-composer'
+                 or job == 'mwext-testextension-php55-composer-trusty'
                  or job == 'mwext-testextension-hhvm-composer']),
             'Project %s pipeline %s must have either '
             'composer-validate or a composer-* job'
