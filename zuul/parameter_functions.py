4c3a6662232ea17be42640820c9b18edfa558054
Antoine Musso <hashar@free.fr>
2017-03-13 13:54:09 +0100

    Replace php53 jobs with php55 equivalents
    Delete:
    mwext-testextension-php53
    mwext-testextension-php53-composer
    mwext-testextension-php53-composer-non-voting
    mwext-testextension-php53-non-voting
    
    Bug: T158652
    Change-Id: I8e81b8f5cdfdc30921e4ff5ae55720f5b15311e4

--- zuul/parameter_functions.py
+++ zuul/parameter_functions.py
@@ -31,8 +31,6 @@ def set_parameters(item, job, params):
         )
 
     # Sets a $PHP_BIN variable based on the job name
-    if 'php53' in job.name:
-        params['PHP_BIN'] = 'php5'
     if 'php55' in job.name:
         params['PHP_BIN'] = 'php5'
     elif 'php70' in job.name:
