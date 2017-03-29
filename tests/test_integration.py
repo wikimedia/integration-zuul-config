1e9f887151941b8741df41c39f53b1fc2b538416
Antoine Musso <hashar@free.fr>
2017-03-29 14:53:50 +0200

    (WIP) ensure all jobs in Zuul are in JJB
    Add jenkins-job-builder as a test dependency.
    Generate all jobs in a temp directory.
    Invoke zuul-server layout validation against the list of generated jobs.
    
    That is similar to integration-zuul-layoutvalidation-gate which uses the
    actual Jenkins to get the list of jobs.
    
    Yield output such as:
    
    FAILURE: Job analytics-libanon not defined
    FAILURE: Job analytics-udp-filters not defined
    FAILURE: Job analytics-webstatscollector not defined
    FAILURE: Job analytics-wikistats not defined
    
    Bug: T103847
    Change-Id: I12917a96b554bca7a79c0fa1a80400f403a532f4

---
+++ tests/test_integration.py
@@ -0,0 +1,60 @@
+# Generate Jenkins jobs
+
+import argparse
+import os
+import shutil
+import tempfile
+import unittest
+
+from jenkins_jobs.cmd import main as jjb
+import zuul.cmd.server
+
+
+class IntegrationTests(unittest.TestCase):
+
+    jjb_out_dir = None
+
+    @classmethod
+    def setUpClass(cls):
+        cls.jjb_out_dir = tempfile.mkdtemp()
+        cls.generate_jjb_jobs(cls.jjb_out_dir)
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.jjb_out_dir:
+            shutil.rmtree(cls.jjb_out_dir)
+
+    @staticmethod
+    def generate_jjb_jobs(out_dir):
+        jjb_dir = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)),
+            '../jjb')
+        jjb(argv=['test', jjb_dir, '-o', out_dir])
+
+    # Merely to show that the config has been generated in setUpClass
+    def test_jjb_generate_jobs(self):
+        self.assertNotEqual(
+            [], os.listdir(self.jjb_out_dir),
+            'jjb must have generated jobs')
+
+    def test_jjb_zuul_jobs(self):
+        'Zuul jobs are defined by JJB'
+
+        jjb_jobs_file = tempfile.NamedTemporaryFile()
+        jjb_jobs_file.write(
+            "\n".join(os.listdir(self.jjb_out_dir)))
+        jjb_jobs_file.flush()
+
+        this_dir = os.path.dirname(os.path.abspath(__file__))
+
+        zuul_server = zuul.cmd.server.Server()
+        zuul_server.args = argparse.Namespace(
+            config=os.path.join(this_dir, 'fixtures/zuul-dummy.conf'),
+            validate=jjb_jobs_file.name,
+        )
+        zuul_server.read_config()
+        zuul_server.config.set(
+            'zuul', 'layout_config',
+            os.path.join(this_dir, '../zuul/layout.yaml'))
+
+        self.assertFalse(zuul_server.test_config(jjb_jobs_file.name))
