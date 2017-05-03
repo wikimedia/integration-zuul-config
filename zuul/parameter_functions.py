93dff2b9337fe2fbb58d7dc90301be243718c53b
Antoine Musso <hashar@free.fr>
2017-05-03 21:39:58 +0200

    Revert "Add set-phpflavor macro"
    Each shell stanza stats with a fresh shell environment. Hence one can
    not set a variable in a shell and expect it to still be set in the next
    shell.
    
    This reverts commit d1901366f0dce7656ee85207c559b584e365abaa.
    
    Bug: T157750
    Change-Id: I0b0dc5e4a7b02967d7c5cba8bec6149912679b57

--- zuul/parameter_functions.py
+++ zuul/parameter_functions.py
@@ -21,6 +21,7 @@ def set_parameters(item, job, params):
         'mw-testskin',
         'mw-testskin-non-voting',
         'mediawiki-core-phpcs-trusty',
+        'mw-tools-codesniffer-mwcore-testrun',
         )
     php5_jobs = (
         # Qunit localhost uses apache mod_php which is Zend. Lets be consistent
