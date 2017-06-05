deee538b3c26210ad7bb50bd34a9e166c0e32f8a
Hashar <hashar@free.fr>
2017-06-05 13:41:25 +0000

    Revert "Run composer test from mediawiki-extensions jobs"
    Conditional build step does not allow multiple shell scripts :(
    
    This reverts commit db90837f7eeb5606e29adbceb713caaa3db7bc18.
    
    Change-Id: I95c327dfacfdf00e7169a7cfca44a786831b64ff

--- tests/test_zuul_scheduler.py
+++ tests/test_zuul_scheduler.py
@@ -140,7 +140,6 @@ class TestZuulScheduler(unittest.TestCase):
             any([job for job in definition
                  if (
                      job.startswith(('composer', 'mwgate-composer'))
-                     or job.startswith('mediawiki-extensions-')
                      or (job.startswith('mwext-testextension-')
                          and 'non-voting' not in job)
                  )]),
@@ -156,8 +155,7 @@ class TestZuulScheduler(unittest.TestCase):
 
         hasVotingTests = any([
             job for job in definition
-            if job.startswith(
-                ('mwext-testextension-', 'mediawiki-extensions-'))
+            if job.startswith('mwext-testextension-')
             and 'non-voting' not in job
             ])
         hasComposerTest = any([
@@ -174,7 +172,6 @@ class TestZuulScheduler(unittest.TestCase):
         self.assertTrue(
             any([job for job in definition
                  if job.endswith(('php55lint')) or
-                 job.startswith('mediawiki-extensions-') or
                  job.startswith(('composer-', 'mwgate-composer'))]),
             'Project %s pipeline %s must have either '
             'phplint or a composer-* job'
