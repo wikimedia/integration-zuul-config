b5e10c8aa1ae27e6cbd4cd2c8a93bf2410b6f09f
addshore <addshorewiki@gmail.com>
2017-08-29 11:11:05 +0300

    dockerfiles: use multi-stage builds
    Not sure why but the size of this image seems much smaller:
    wmfreleng/operations-puppet:0.1.0 - 786MB (from dockerhub)
    wmfreleng/operations-puppet:latest - 580MB (from this commit)
    
    Change-Id: Ic63e6e3b1f3c5c27dd3ccc2fe87faf0cef7cbee2

---
+++ dockerfiles/contint-operations-puppet/example-run.sh
@@ -0,0 +1,8 @@
+docker run \
+    --rm --tty \
+    --env ZUUL_URL=https://gerrit.wikimedia.org/r \
+    --env ZUUL_PROJECT=operations/puppet \
+    --env ZUUL_COMMIT=72d31ffb0fa612482d268b6f5484785842cd0fda \
+    --env ZUUL_REF=refs/changes/49/374349/2 \
+    --volume /$(pwd)/log://var/lib/jenkins/log \
+     wmfreleng/operations-puppet
