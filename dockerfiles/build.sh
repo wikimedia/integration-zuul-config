711a8b072c10299fea2ee13234ca141faa912d67
addshore <addshorewiki@gmail.com>
2017-08-29 11:11:05 +0300

    dockerfiles: use multi-stage builds
    Change-Id: Ic63e6e3b1f3c5c27dd3ccc2fe87faf0cef7cbee2

--- dockerfiles/build.sh
+++ dockerfiles/build.sh
@@ -20,34 +20,9 @@ for dockerbuild in "$BASE_DIR"/contint-*/Dockerfile; do
     BUILD_CONTAINER_TAG="${CONTAINER_NAME:8}-build"
 
     pushd "$CONTAINER_DIR" &>/dev/null
+    pushd "$CONTAINER_DIR" &>/dev/null
     info "BUILDING $CONTAINER_NAME"
 
-    # Run a standard build step if there is one
-    if [[ -f "Dockerfile.build" ]]; then
-        info "...Creating build image ${BUILD_IMG_TAG}"
-        touch .cache-buster
-        docker build \
-            -t "${BUILD_IMG_TAG}" \
-            -f "Dockerfile.build" .
-
-        info "...Creating build container ${BUILD_CONTAINER_TAG} from ${BUILD_IMG_TAG}"
-
-        # Remove any container that may exist with that container name first
-        docker rm $BUILD_CONTAINER_TAG || /bin/true
-
-        docker create --name "${BUILD_CONTAINER_TAG}" "${BUILD_IMG_TAG}"
-
-        # Make sure to purge old cache before making new one
-        if [[ -d "cache" ]]; then
-            info "...Purging old 'cache'"
-            rm -rf "cache"
-        fi
-
-        info "...Copying ${BUILD_CONTAINER_TAG}:/tmp/cache artifact to 'cache'"
-        docker cp "${BUILD_CONTAINER_TAG}:/tmp/cache" cache
-    fi
-
-    info "...Creating final image ${IMG_TAG}"
     docker build \
         -t "${IMG_TAG}" \
         -f "Dockerfile" .
