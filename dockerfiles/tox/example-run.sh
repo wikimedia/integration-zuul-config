a9c0369d047631910a8e64dcba67a0d48d7bdbed
addshore <addshorewiki@gmail.com>
2017-09-11 22:28:54 +0200

    WIP (I dont know tox) tox Dockefile
    Change-Id: Ia32a5b3deb6376e37dec0029dce7ea290550e9db

---
+++ dockerfiles/tox/example-run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker run \
+    --rm --tty \
+    --env ZUUL_URL=https://gerrit.wikimedia.org/r \
+    --env ZUUL_PROJECT=analytics/quarry/web \
+    --env ZUUL_COMMIT=a6173a23ae90dddef446eeea02019b685228e7ea \
+    --env ZUUL_REF=refs/changes/43/377043/1 \
+    --volume /$(pwd)/log://var/lib/jenkins/log \
+     wmfreleng/tox:latest
