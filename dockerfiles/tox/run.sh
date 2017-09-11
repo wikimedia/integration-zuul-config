52eb9d99eeff22eb7f9f6797d0397aad2c23b102
addshore <addshorewiki@gmail.com>
2017-09-11 22:28:54 +0200

    WIP (I dont know tox) tox Dockefile
    Change-Id: Ia32a5b3deb6376e37dec0029dce7ea290550e9db

---
+++ dockerfiles/tox/run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+SRC_DIR="/srv/src"
+
+LOG_DIR="$HOME/log"
+export LOG_DIR
+
+capture_logs() {
+    # Save logs
+    mv "${SRC_DIR}"/.tox/*/log/*.log "${LOG_DIR}/" || /bin/true
+    mv "${SRC_DIR}"/.tox/log/* "${LOG_DIR}/" || /bin/true
+}
+
+trap capture_logs EXIT
+
+cd "$SRC_DIR"
+
+git init
+git fetch --depth 2 --quiet "${ZUUL_URL}/${ZUUL_PROJECT}" "$ZUUL_REF"
+git checkout --quiet FETCH_HEAD
+git submodule --quiet update --init --recursive
+
+# Run tests
+TOX_TESTENV_PASSENV=PY_COLORS PY_COLORS=1 tox -v | tee "${LOG_DIR}/stdout.log"
