5427b161d20b6e0ae8774ffaec1469ecb6c5dea7
addshore <addshorewiki@gmail.com>
2017-09-18 23:19:34 +0100

    docker: updated cache-buster cmd for operations-puppet
    Change-Id: Iebf99adcb48b3eea5622c72ece88681d47e11b32

--- dockerfiles/operations-puppet/prebuild.sh
+++ dockerfiles/operations-puppet/prebuild.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-set -eu
-set -o pipefail
+set -e
 
 # This is copied in Dockerfile to ensure that a build step grabs a fresh
 # copy of the git repo when it is updated rather than using a layer from
 # the local Docker cache.
 
-curl -s https://gerrit.wikimedia.org/r/projects/operations%2Fpuppet/branches/production | grep revision | xargs > .cache-buster
+git ls-remote --exit-code https://gerrit.wikimedia.org/r/p/operations/puppet.git refs/heads/production > .cache-buster-operations-puppet
