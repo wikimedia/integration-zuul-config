7e3ca4005a88a9db45a88982a20d55f455518255
Antoine Musso <hashar@free.fr>
2017-09-25 15:29:29 +0200

    dib: add some spec test
    Attempts to compile the puppet catalog of the CI image.
    Clone operations/puppet in the Jenkins job.
    Assume that operations/puppet.git is available.
    
    Change-Id: I7ae0d53844e333f4c1e05619d80ddcce1076bc57

---
+++ dib/puppet/spec/hosts/ci_jessie_wikimedia_spec.rb
@@ -0,0 +1,23 @@
+require 'rspec-puppet'
+require 'puppetlabs_spec_helper/puppet_spec_helper'
+
+RSpec.configure do |c|
+    c.manifest = File.expand_path(File.join(__dir__, '..', '..', 'ciimage.pp'))
+    if ENV['PUPPET_DIR']
+        c.module_path = File.expand_path(
+            File.join( ENV['PUPPET_DIR'], 'modules'))
+    else
+        c.module_path = File.expand_path(
+            File.join(
+                __dir__, '../../../../../../operations/puppet/modules'))
+    end
+end
+
+describe 'ci-jessie-wikimedia' do
+    let(:facts) { {
+        :initsystem => 'systemd',
+        :lsbdistrelease => 'Jessie',
+        :lsbdistid => 'Debian',
+    } }
+    it { should compile }
+end
