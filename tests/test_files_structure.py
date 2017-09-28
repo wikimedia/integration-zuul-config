5da787ff077003c9cedc145612b7e0782bdd7193
Antoine Musso <hashar@free.fr>
2017-09-28 20:47:26 +0200

    Convert tabs to 4 spaces
    Would update all jobs using the assert-no-mediawiki-errors script.
    Add a generic test to ensure that no files contain tabs.
    Skip checking binary files (such as Sury GPG key).
    
    Change-Id: I06ee99fbffb06a510677b5193d05d5f13d78b6b1

---
+++ tests/test_files_structure.py
@@ -0,0 +1,15 @@
+import subprocess
+import unittest
+
+
+class TestFilesStructure(unittest.TestCase):
+
+    def test_have_no_tabs(self):
+        try:
+            out = subprocess.check_output([
+                'git', 'grep', '-n', '-I', '-P', '\t'])
+            print(out)
+            raise AssertionError('Files have tabulations. Check output.')
+        except subprocess.CalledProcessError as e:
+            self.assertEquals(1, e.returncode,
+                              'when there is no tabs: git grep exit 1')
