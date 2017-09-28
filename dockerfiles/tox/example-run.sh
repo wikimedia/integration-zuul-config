4ba9dc3bffbd55ec2ba15911983a566ac2b2b406
Antoine Musso <hashar@free.fr>
2017-09-28 20:50:47 +0200

    docker: change tox example to use master
    Change-Id: I1db99afa7917eab4f78c119e1d5463455a8210bb

--- dockerfiles/tox/example-run.sh
+++ dockerfiles/tox/example-run.sh
@@ -4,8 +4,8 @@ install --mode 777 --directory log
 docker run \
     --rm --tty \
     --env ZUUL_URL=https://gerrit.wikimedia.org/r \
-    --env ZUUL_PROJECT=analytics/quarry/web \
-    --env ZUUL_COMMIT=a6173a23ae90dddef446eeea02019b685228e7ea \
-    --env ZUUL_REF=refs/changes/43/377043/1 \
+    --env ZUUL_PROJECT=integration/config \
+    --env ZUUL_COMMIT=master \
+    --env ZUUL_REF=master \
     --volume /"$(pwd)"/log://log \
      wmfreleng/tox:latest
