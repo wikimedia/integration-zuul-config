ec72ea243c0d455b47350a504fab7456174f586c
Antoine Musso <hashar@free.fr>
2017-09-28 20:47:26 +0200

    Convert tabs to 4 spaces
    Would update all jobs using the assert-no-mediawiki-errors script.
    
    Change-Id: I06ee99fbffb06a510677b5193d05d5f13d78b6b1

--- Rakefile
+++ Rakefile
@@ -4,15 +4,15 @@ task :default => [:help]
 
 Rake::Task[:lint].clear
 PuppetLint::RakeTask.new :lint do |config|
-	config.fail_on_warnings = true  # be strict
-	config.log_format = '%{path}:%{line} %{KIND} %{message} (%{check})'
+    config.fail_on_warnings = true  # be strict
+    config.log_format = '%{path}:%{line} %{KIND} %{message} (%{check})'
 end
 
 desc 'Run spec for dib/puppet'
 task :dib_spec do
-	Dir.chdir 'dib/puppet' do
-		fail unless system('rake spec')
-	end
+    Dir.chdir 'dib/puppet' do
+        fail unless system('rake spec')
+    end
 end
 
 desc 'Run all build/tests commands (CI entry point)'
@@ -20,5 +20,5 @@ task test: [:syntax, :lint, :dib_spec]
 
 desc 'Show the help'
 task :help do
-	system 'rake -T'
+    system 'rake -T'
 end
--- jjb/assert-no-mediawiki-errors.bash
+++ jjb/assert-no-mediawiki-errors.bash
@@ -21,82 +21,82 @@ WMF_MINOR=12
 
 # A valid version is a version > wmf/1.WMF_MAJOR.0-wmf.WMF_MINOR
 valid_wmf_version() {
-	local version major minor
-	version="$1"
+    local version major minor
+    version="$1"
 
-	major=${version%.*.*}
-	major=${major##*.}
+    major=${version%.*.*}
+    major=${major##*.}
 
-	minor=${version##*.}
+    minor=${version##*.}
 
-	if (( major == WMF_MAJOR )) && (( minor >= WMF_MINOR )); then
-		return 0
-	elif (( major > WMF_MAJOR )); then
-		return 0
-	else
-		return 1
-	fi
+    if (( major == WMF_MAJOR )) && (( minor >= WMF_MINOR )); then
+        return 0
+    elif (( major > WMF_MAJOR )); then
+        return 0
+    else
+        return 1
+    fi
 }
 
 # A valid version is a version > REL{REL_MAJOR}_{REL_MINOR}
 valid_rel_version() {
-	local version major minor
-	version="$1"
-	major=${version%%_*}
-	minor=${version##*_}
+    local version major minor
+    version="$1"
+    major=${version%%_*}
+    minor=${version##*_}
 
-	if (( major == REL_MAJOR )) && (( minor >= REL_MINOR )); then
-		return 0
-	elif (( major > REL_MAJOR )); then
-		return 0
-	else
-		return 1
-	fi
+    if (( major == REL_MAJOR )) && (( minor >= REL_MINOR )); then
+        return 0
+    elif (( major > REL_MAJOR )); then
+        return 0
+    else
+        return 1
+    fi
 }
 
 # Ensures that this is a ZUUL_BRANCH for which we want this to be a valid test
 valid_version() {
-	local version wmf_version rel_version
+    local version wmf_version rel_version
 
-	version="$1"
+    version="$1"
 
-	# Master is valid
-	if [[ "$version" == "master" ]]; then
-		return 0
-	fi
+    # Master is valid
+    if [[ "$version" == "master" ]]; then
+        return 0
+    fi
 
-	# Is this is a wmf/something
-	wmf_version="${version#wmf\/}"
-	if (( ${#version} != ${#wmf_version} )); then
-		valid_wmf_version "$wmf_version" && return 0
-	fi
+    # Is this is a wmf/something
+    wmf_version="${version#wmf\/}"
+    if (( ${#version} != ${#wmf_version} )); then
+        valid_wmf_version "$wmf_version" && return 0
+    fi
 
-	# Is this is a RELsomething
-	rel_version="${version#REL}"
-	if (( ${#version} != ${#rel_version} )); then
-		valid_rel_version "$rel_version" && return 0
-	fi
+    # Is this is a RELsomething
+    rel_version="${version#REL}"
+    if (( ${#version} != ${#rel_version} )); then
+        valid_rel_version "$rel_version" && return 0
+    fi
 
-	# Unidentified version: invalid
-	return 1
+    # Unidentified version: invalid
+    return 1
 }
 
 if ! valid_version "$ZUUL_BRANCH"; then
-	printf 'Test not for branch: %s\n' "$ZUUL_BRANCH"
-	exit 0
+    printf 'Test not for branch: %s\n' "$ZUUL_BRANCH"
+    exit 0
 fi
 
 log_files="$( cd "$WORKSPACE/log"; ls "${ERROR_FILES[@]}" 2> /dev/null || :)"
 if [ ! "$log_files" ]; then
-	echo "No error files. GOOD"
-	exit 0
+    echo "No error files. GOOD"
+    exit 0
 fi
 
 echo "Dumping file(s) $log_files"
 set +e
-	# Use `grep --color . file list` to ensure that file names appear next
-	# to log messages
-	(cd "$WORKSPACE/log"; grep --color . "${ERROR_FILES[@]}" 2> /dev/null)
+    # Use `grep --color . file list` to ensure that file names appear next
+    # to log messages
+    (cd "$WORKSPACE/log"; grep --color . "${ERROR_FILES[@]}" 2> /dev/null)
 set -e
 echo -e "MediaWiki emitted some errors. Check output above."
 exit 1
