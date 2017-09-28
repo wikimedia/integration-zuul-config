57dfbf6f50ec19ca9e681457a9dfb9bca9844425
addshore <addshorewiki@gmail.com>
2017-09-28 19:15:14 +0100

    mwext-php70-phan-docker job
    Change-Id: I42a74b187383569cd851c09b465175666b657362

---
+++ dockerfiles/ci-src-setup/example-run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+docker run \
+    --rm --tty \
+    --env ZUUL_URL=https://gerrit.wikimedia.org/r \
+    --env ZUUL_PROJECT=mediawiki/extensions/ElectronPdfService \
+    --env ZUUL_COMMIT=d016986bda3bec88bf246fa5c34be2941c54ba70 \
+    --env ZUUL_REF=refs/changes/08/371108/4 \
+    --env EXT_DEPENDENCIES=mediawiki/extensions/BetaFeatures \
+    --env SKIN_DEPENDENCIES= \
+    --entrypoint "bash" \
+    wmfreleng/ci-src-setup:latest \
+    //srv/setup-mw.sh
