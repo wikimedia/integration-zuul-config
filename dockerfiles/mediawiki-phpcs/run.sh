d1c509b80a0394032b59fab99d219a92f74d67a3
Tyler Cipriani <tcipriani@wikimedia.org>
2017-09-29 12:49:37 -0600

    Don't checkout submodules for phpcs-docker
    Bug: T177104
    Change-Id: I020d5def90a378f947e0333585caa581f3a03ebe

--- dockerfiles/mediawiki-phpcs/run.sh
+++ dockerfiles/mediawiki-phpcs/run.sh
@@ -5,7 +5,6 @@ cd /src
 git init
 git fetch --quiet --depth 1 "${ZUUL_URL}/${ZUUL_PROJECT}" "$ZUUL_REF"
 git checkout --quiet FETCH_HEAD
-git submodule --quiet update --init --recursive
 
 /srv/composer/vendor/bin/composer install --no-progress
 ./vendor/bin/phpcs -p -s --report-full "--report-checkstyle=/log/checkstyle.xml"
