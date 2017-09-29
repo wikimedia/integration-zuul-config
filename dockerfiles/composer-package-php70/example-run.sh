c8d133ae061e878f7613b607affb408deb2cf600
Kunal Mehta <legoktm@member.fsf.org>
2017-09-28 21:27:24 -0700

    Add experimental "composer-package-php70-docker" job
    Change-Id: I3cc35d39b39a7b6e9a38455b9264bb0d2b243ee2

---
+++ dockerfiles/composer-package-php70/example-run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+install --mode 777 --directory log
+docker run \
+    --rm --tty \
+    --env ZUUL_URL=https://gerrit.wikimedia.org/r \
+    --env ZUUL_PROJECT=utfnormal \
+    --env ZUUL_COMMIT=668604441afd899efb073ce4c6b5545341ef6582 \
+    --env ZUUL_REF=refs/changes/57/375857/1 \
+    --volume /$(pwd)/log://var/lib/jenkins/log \
+     wmfreleng/composer-package-php70:latest
---
+++ dockerfiles/composer-package-php70/run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+cd /src
+
+git init
+git fetch --quiet --depth 1 "${ZUUL_URL}/${ZUUL_PROJECT}" "$ZUUL_REF"
+git checkout FETCH_HEAD
+git submodule --quiet update --init --recursive
+
+/srv/composer/vendor/bin/composer --ansi validate
+/srv/composer/vendor/bin/composer install --no-progress
+/srv/composer/vendor/bin/composer test
