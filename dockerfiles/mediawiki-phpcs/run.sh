9dc20b516528041bfbf40d6458be8f80c4b8cc24
addshore <addshorewiki@gmail.com>
2017-09-29 10:09:44 +0100

    docker: quiet fetch in mediawiki-phpcs
    Change-Id: I975b2a723cfc9ca21d61ce9394866f33bc78caa7

--- dockerfiles/mediawiki-phpcs/run.sh
+++ dockerfiles/mediawiki-phpcs/run.sh
@@ -4,7 +4,7 @@ cd /src
 
 git init
 git fetch --quiet --depth 1 "${ZUUL_URL}/${ZUUL_PROJECT}" "$ZUUL_REF"
-git checkout FETCH_HEAD
+git checkout --quiet FETCH_HEAD
 git submodule --quiet update --init --recursive
 
 /srv/composer/vendor/bin/composer install --no-progress
