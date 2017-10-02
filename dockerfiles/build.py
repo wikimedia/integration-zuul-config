7422c72a331b46207c4d3dc6ff0c26b3ea722669
Antoine Musso <hashar@free.fr>
2017-10-02 14:58:30 +0200

    docker: lame dependency tree
    Try me:
        ./build.py --deps|jq .
        ./build.py --stages|jq .
    
    Bug: T177272
    Change-Id: I8d35f3b9615cb0b94d336ed94743fa5a30f2d2f5

--- dockerfiles/build.py
+++ dockerfiles/build.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
 import argparse
+from collections import defaultdict
 import configparser
 from datetime import datetime
 from glob import glob
+import json
 import logging
 import os.path
+import re
 import subprocess
 import sys
 
@@ -15,6 +18,10 @@ DOCKER_HUB_ACCOUNT = 'wmfreleng'
 
 
 class DockerBuilder(object):
+
+    def __init__(self):
+        self.deps_tree = {}
+        self.stages = {}
 
     def run(self):
         self.parse_args()
@@ -31,6 +38,16 @@ class DockerBuilder(object):
             ]
         else:
             dockerfiles = self.find_docker_files()
+
+        self.gen_deps_tree(dockerfiles)
+        self.gen_staged_deps()
+        if self.args.deps:
+            print(json.dumps(self.deps_tree))
+            return True
+        if self.args.stages:
+            print(json.dumps(self.stages))
+            return True
+
         return all(map(self.build, dockerfiles))
 
     def load_config(self):
@@ -43,14 +60,94 @@ class DockerBuilder(object):
 
     def parse_args(self):
         parser = argparse.ArgumentParser()
-        parser.add_argument('directory', nargs='*', default=[])
         parser.add_argument(
             '-v', '--verbose', action='store_const',
             const=logging.DEBUG, default=logging.INFO)
+
+        group = parser.add_mutually_exclusive_group()
+        group.add_argument('directory', nargs='*', default=[])
+        group.add_argument(
+            '--deps', action='store_true',
+            help='Dump images dependency tree and exit')
+        group.add_argument(
+            '--stages', action='store_true',
+            help='Dump stages grouping sibling images')
+
         self.args = parser.parse_args()
 
     def find_docker_files(self):
         return sorted(glob(os.path.join(BASE_DIR, '*/Dockerfile')))
+
+    def gen_deps_tree(self, files):
+        froms = {}
+        for f in files:
+            image_name = 'wmfreleng/' + os.path.basename(os.path.dirname(f))
+            with open(f) as fp:
+                froms[image_name] = []
+                for l in fp.readlines():
+                    m = self._parse_FROM(l)
+                    if m:
+                        froms[image_name].append(m['image'])
+                        # Register images not yet known
+                        if m['image'] not in froms:
+                            froms[m['image']] = [None]
+
+            deps = set(froms[image_name])
+            self.log.debug('%s dependenc%s: %s' % (
+                image_name,
+                'y' if (len(deps) == 1) else 'ies',
+                ', '.join(deps)
+            ))
+
+        tree = {}
+
+        def build_tree(tree, parent, froms):
+            childs = [p for (p, v) in froms.items()
+                      if parent in v]
+            for child in childs:
+                tree[child] = {}
+                build_tree(tree[child], child, froms)
+
+        build_tree(tree, None, froms)
+        self.log.debug(tree)
+        self.deps_tree = tree
+        return tree
+
+    def gen_staged_deps(self):
+
+        def flatten(stages, depth, deps):
+            depth = depth + 1
+
+            stages[depth].extend(deps.keys())
+            stages[depth] = sorted(set(stages[depth]))
+
+            for sub_deps in deps.values():
+                if sub_deps:
+                    flatten(stages, depth, sub_deps)
+
+        stages = defaultdict(list)
+        flatten(stages, 0, self.deps_tree)
+
+        for (num, images) in stages.items():
+            self.log.debug('Stage %s: %s' % (num, ', '.join(images)))
+        self.stages = stages
+
+    def _parse_FROM(self, line):
+        # https://docs.docker.com/engine/reference/builder/#from
+        m = re.match(
+            '''FROM\s+
+                (?P<image>\S+?)
+                (
+                    :(?P<tag>\S+)
+                    |
+                    @(?P<digest>\S+)
+                )?
+                (?:\s+as\s+.+)?
+                \n$
+            ''',
+            line, re.X + re.I)
+        if m:
+            return m.groupdict()
 
     def build(self, dockerfile):
         self.log.debug('Building %s' % dockerfile)
