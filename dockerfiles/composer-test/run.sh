2764dc980e40f68873e2817e1b1942b8a618cb97
Kunal Mehta <legoktm@member.fsf.org>
2017-10-04 21:08:52 -0700

    Port composer-php70 job to docker
    Change-Id: I58b74bd5e350f2c3a696f0902a1fe8c4ef0cdfa2

---
+++ dockerfiles/composer-test/run.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+cd /src
+
+git init
+git fetch --quiet --depth 1 "${ZUUL_URL}/${ZUUL_PROJECT}" "$ZUUL_REF"
+git checkout FETCH_HEAD
+git submodule --quiet update --init --recursive
+
+composer --ansi validate --no-check-publish
+composer install --no-progress
+composer test
