069db00dde0939e71d30f4be9dcd9ea0fd38b187
Kunal Mehta <legoktm@member.fsf.org>
2017-10-04 19:37:02 -0700

    docker: Directly inherit from composer image
    Instead of inheriting from php and then copying in composer,
    just inherit from the composer image. This also lets us take
    advantage of the /usr/bin/composer symlink.
    
    Change-Id: Iaff980df608779b986a4c626163b5659615cea49

--- dockerfiles/mediawiki-phpcs/run.sh
+++ dockerfiles/mediawiki-phpcs/run.sh
@@ -6,5 +6,5 @@ git init
 git fetch --quiet --depth 1 "${ZUUL_URL}/${ZUUL_PROJECT}" "$ZUUL_REF"
 git checkout --quiet FETCH_HEAD
 
-/srv/composer/vendor/bin/composer install --no-progress
+composer install --no-progress
 ./vendor/bin/phpcs -p -s --report-full "--report-checkstyle=/log/checkstyle.xml"
