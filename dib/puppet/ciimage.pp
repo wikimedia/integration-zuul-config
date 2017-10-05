d0451b282aa065f43df54793f786357412c430c2
Hashar <hashar@free.fr>
2017-10-05 11:53:45 +0000

    Revert "dib: php5.5 packages are now in contint::packages::php"
    Causes:
    
     Duplicate declaration:
     Apt::Repository[jessie-ci-php55] is already declared
     in file modules/contint/manifests/packages/php.pp:71;
     cannot redeclare at dib/puppet/ciimage.pp:44
     on node snapshot-ci-jessie-1507204001.eqiad.wmflabs
    
    This reverts commit 5cb4a61aeff93b98de6c5f7c51ef4d0a03919e40.
    
    Change-Id: I3ef9666c6cee2ff5dd55514e832a4e47469cad0e

--- dib/puppet/ciimage.pp
+++ dib/puppet/ciimage.pp
@@ -40,7 +40,33 @@ include contint::packages::php
 
 if os_version('debian == jessie') {
     apt::repository { 'jessie-ci-php55':
-        ensure       => absent,
+        uri        => 'http://apt.wikimedia.org/wikimedia',
+        dist       => 'jessie-wikimedia',
+        components => 'component/ci',
+        source     => false,
+    }
+    package { [
+        'php5.5-cli',
+        'php5.5-common',
+        'php5.5-curl',
+        'php5.5-dev',
+        'php5.5-gd',
+        'php5.5-gmp',
+        'php5.5-intl',
+        'php5.5-ldap',
+        'php5.5-luasandbox',
+        'php5.5-mbstring',
+        'php5.5-mcrypt',
+        'php5.5-mysql',
+        'php5.5-redis',
+        'php5.5-sqlite3',
+        'php5.5-tidy',
+        'php5.5-xsl',
+        ]: ensure => present,
+        require   => [
+          Apt::Repository['jessie-ci-php55'],
+          Exec['apt-get update'],
+        ],
     }
 }
 
