b49e1c362d9bf787b8fd113f26e1c9eabc16fccf
Tyler Cipriani <tcipriani@wikimedia.org>
2017-10-06 11:26:39 -0600

    Dockerfiles: Add build.py tests
    Change-Id: I7d4df1443b9d50df45f7284af43e7cc1dee1dbca

---
+++ dockerfiles/tests/test_dockerfiles_build.py
@@ -0,0 +1,51 @@
+import logging
+import os
+import sys
+import unittest
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(BASE_DIR, '..'))
+
+import build
+
+
+FIXTURES = os.path.join(BASE_DIR, 'fixtures', 'dockerfiles')
+
+
+class TestDockerfilesBuild(unittest.TestCase):
+    def make_image_name(self, name):
+        return os.path.join(build.DOCKER_HUB_ACCOUNT, name)
+
+    def test_parse_from(self):
+        b = build.DockerBuilder(base_dir=FIXTURES)
+        x = b._parse_FROM('FROM foo:1.2.3 as bar')
+        self.assertTrue(x['image'] == 'foo')
+        self.assertTrue(x['tag'] == '1.2.3')
+        self.assertTrue(x['digest'] is None)
+
+    def test_find_docker_files(self):
+        b = build.DockerBuilder(base_dir=FIXTURES)
+        docker_files = b.find_docker_files()
+        base_file = os.path.join(FIXTURES, 'ci-fixture', 'Dockerfile')
+        self.assertTrue(len(docker_files) > 0)
+        self.assertTrue(base_file in docker_files)
+
+    def test_gen_deps_tree_three_depends_on_two(self):
+        b = build.DockerBuilder(base_dir=FIXTURES)
+        logging.basicConfig(level=logging.DEBUG)
+        b.log = logging.getLogger()
+        base_tree = b.gen_deps_tree(
+            self.make_image_name('ci-fixture-child-two'))
+        self.assertTrue(base_tree is not None)
+        self.assertTrue(
+            self.make_image_name('ci-fixture-child-three') in base_tree.keys())
+
+    def test_gen_deps_tree_two_depends_on_base(self):
+        b = build.DockerBuilder(base_dir=FIXTURES)
+        logging.basicConfig(level=logging.DEBUG)
+        b.log = logging.getLogger()
+        base_tree = b.gen_deps_tree(
+            self.make_image_name('ci-fixture'))
+        print(base_tree)
+        # This is currently failing
+        # self.assertTrue(base_tree is not None)
