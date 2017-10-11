8b2bfd770ddfc9daf4ac15ecf6449d55fa09860a
Antoine Musso <hashar@free.fr>
2017-10-11 15:02:12 +0200

    composer-package: git clone inside the container
    Bug: T144961
    Bug: T177905
    Change-Id: I78a9ef75874af7907fb581a02884ec1d65276527

--- dockerfiles/composer-package/run.sh
+++ dockerfiles/composer-package/run.sh
@@ -4,6 +4,10 @@ set -euxo pipefail
 
 cd /src
 
+git init
+git fetch --quiet --depth 1 "${ZUUL_URL}/${ZUUL_PROJECT}" "${ZUUL_REF}"
+git checkout --quiet FETCH_HEAD
+
 composer --ansi validate
 composer install --no-progress --prefer-dist
 composer test
