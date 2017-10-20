cd25680632bd20160cc7e7eb74e6ee5ad8bd5de2
Antoine Musso <hashar@free.fr>
2017-10-20 17:17:25 +0200

    Castor for Docker jobs
    Change-Id: I3ac8679feb199f1347b395ac5359aaf3102bec8a

--- jjb/castor-save-sync.bash
+++ jjb/castor-save-sync.bash
@@ -1,19 +1,30 @@
 set -eu +x
 
+[[ $TRIGGERED_JOB_NAME == *'docker'* ]] && is_docker=1 || is_docker=''
+
 ssh_config=($TRIGGERED_SSH_CONNECTION)
 REMOTE_INSTANCE="${ssh_config[2]}"
 
 # Destination in the central cache
 DEST="/srv/jenkins-workspace/caches/${CASTOR_NAMESPACE}"
 
-echo "Ensure cache directories exist on remote $REMOTE_INSTANCE"
-ssh -q -a -T \
-  -o ConnectTimeout=6 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
-  jenkins@"${REMOTE_INSTANCE}" \
-  'mkdir -v -p .cache/composer .composer/cache .m2/repository .cache/pip .npm workspace/vendor/bundle'
+if [ ! $is_docker ]; then
+    echo "Ensure cache directories exist on remote $REMOTE_INSTANCE"
+    ssh -q -a -T \
+      -o ConnectTimeout=6 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
+      jenkins@"${REMOTE_INSTANCE}" \
+      'mkdir -v -p .cache/composer .composer/cache .m2/repository .cache/pip .npm workspace/vendor/bundle'
+fi
 
 echo "Creating directory holding cache:"
 mkdir -v -p "${DEST}"
+
+if [ $is_docker ]; then
+    fetches=":{$TRIGGERED_WORKSPACE}/cache"
+else
+    fetches=':.cache/composer :.composer/cache :.m2/repository :.cache/pip :.npm :workspace/vendor/bundle'
+    relative=1
+fi
 
 echo -e "Syncing cache\nFrom.. ${REMOTE_INSTANCE}\nTo.... ${DEST}"
 rsync \
@@ -21,7 +32,7 @@ rsync \
   --compress \
   --rsh="ssh -a -T  -o ConnectTimeout=6 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" \
   --delete-delay \
-  --relative \
-  jenkins@"${REMOTE_INSTANCE}":.cache/composer :.composer/cache :.m2/repository :.cache/pip :.npm :workspace/vendor/bundle "${DEST}/"
+  ${relative:+--relative} \
+  jenkins@"${REMOTE_INSTANCE}""${fetches}" "${DEST}/"
 
 echo -e "\nDone"
