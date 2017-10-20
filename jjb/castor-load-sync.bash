878c65d8253fd213e2fcbbd00213df11e5f1c5aa
Antoine Musso <hashar@free.fr>
2017-10-20 17:17:25 +0200

    Castor for Docker jobs
    Create castor-save-workspace-cache
    Update integration-config-tox-docker
    
    Bug: T179208
    Change-Id: I3ac8679feb199f1347b395ac5359aaf3102bec8a

--- jjb/castor-load-sync.bash
+++ jjb/castor-load-sync.bash
@@ -2,11 +2,20 @@
 # Load cache from central repository
 set -u
 
+[[ $JOB_NAME == *'docker'* ]] && is_docker=1 || is_docker=''
+
+if [ $is_docker ]; then
+    DEST="/cache"
+else
+    DEST="$HOME"
+fi
+
 echo "Syncing..."
 rsync \
   --archive \
+  --delete-delay \
   --compress \
   --contimeout 3 \
-  rsync://castor02.integration.eqiad.wmflabs:/caches/${CASTOR_NAMESPACE}/ $HOME \
+  rsync://castor02.integration.eqiad.wmflabs:/caches/"$CASTOR_NAMESPACE"/ "$DEST" \
   || :
 echo -e "\nDone"
---
+++ jjb/castor-save-workspacecache.bash
@@ -0,0 +1,25 @@
+set -eu +x
+
+ssh_config=($TRIGGERED_SSH_CONNECTION)
+REMOTE_INSTANCE="${ssh_config[2]}"
+
+# Destination in the central cache
+DEST="/srv/jenkins-workspace/caches/${CASTOR_NAMESPACE}"
+
+echo "Creating directory holding cache:"
+mkdir -v -p "${DEST}"
+
+remote_cache_dir="${TRIGGERED_WORKSPACE}/cache"
+cache_dir='/cache'
+
+echo -e "Syncing cache\nFrom.. ${REMOTE_INSTANCE}:${remote_cache_dir}\nTo.... ${DEST}"
+set -x
+rsync \
+  --archive \
+  --compress \
+  --rsh="/usr/bin/ssh -a -T -o ConnectTimeout=6 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" \
+  --rsync-path="docker run --rm -i --volume ${remote_cache_dir}:/${cache_dir} --entrypoint=/usr/bin/rsync wmfreleng/castor:v2017.10.30.16.04" \
+  --delete-delay \
+  jenkins-deploy@"${REMOTE_INSTANCE}:/${cache_dir}/" "${DEST}"
+
+echo -e "\nDone"
