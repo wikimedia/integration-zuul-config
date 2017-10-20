4b7f1800529150466310fb99e592919e3daded23
Antoine Musso <hashar@free.fr>
2017-10-20 17:17:25 +0200

    Castor for Docker jobs
    For Docker, we mount $WORKSPACE/cache as cache, it is being written to
    by packages managers honoring XDG_CACHE_HOME. Since containers run as
    'nobody', the Jenkins agent might not be able to replace/delete files in
    there.  The user to ssh to instance and paths to sync are also slightly
    different compared to Nodepool.
    
    Make castor-load to vary based on the job name. Whenever job had
    'docker' it would restore to $WORKSPACE/cache
    
    Copy paste most of the glue for castor-save and adjusted user/paths. To
    be able to save files owned by nobody and that might not be readable by
    jenkins-deploy, castor-save would instruct rsync to use a rsync docker
    container (wmfreleng/castor) on the remote.  This way it runs as nobody
    and can grab files just fine.
    
    Create castor-save-workspace-cache
    Update integration-config-tox-docker
    
    Add the docker job for integration/config repository as a non voting
    job. Will give it some grace period before it replaces
    integration-config-tox-jessie.
    
    Bug: T179208
    Change-Id: I3ac8679feb199f1347b395ac5359aaf3102bec8a

--- jjb/castor-load-sync.bash
+++ jjb/castor-load-sync.bash
@@ -2,11 +2,22 @@
 # Load cache from central repository
 set -u
 
+[[ $JOB_NAME == *'docker'* ]] && is_docker=1 || is_docker=''
+
+if [ $is_docker ]; then
+    # For containers we mount $WORKSPACE/cache from the host to /cache in the
+    # container. It is also the value of XDG_CACHE_HOME
+    DEST="/cache"
+else
+    DEST="$HOME"
+fi
+
 echo "Syncing..."
 rsync \
   --archive \
+  --delete-delay \
   --compress \
   --contimeout 3 \
-  rsync://castor02.integration.eqiad.wmflabs:/caches/${CASTOR_NAMESPACE}/ $HOME \
+  rsync://castor02.integration.eqiad.wmflabs:/caches/"$CASTOR_NAMESPACE"/ "$DEST" \
   || :
 echo -e "\nDone"
---
+++ jjb/castor-save-workspacecache.bash
@@ -0,0 +1,27 @@
+set -eu +x
+
+ssh_config=($TRIGGERED_SSH_CONNECTION)
+REMOTE_INSTANCE="${ssh_config[2]}"
+
+# Destination in the central cache
+DEST="/srv/jenkins-workspace/caches/${CASTOR_NAMESPACE}"
+
+echo "Creating directory holding cache:"
+mkdir -v -p "${DEST}"
+
+remote_cache_dir="${TRIGGERED_WORKSPACE}/cache"
+cache_dir='/cache'
+
+echo -e "Syncing cache\nFrom.. ${REMOTE_INSTANCE}:${remote_cache_dir}\nTo.... ${DEST}"
+set -x
+# On the sender, run rsync in a container (--rsync-path) to have it run has
+# user 'nobody'.
+rsync \
+  --archive \
+  --compress \
+  --rsh="/usr/bin/ssh -a -T -o ConnectTimeout=6 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" \
+  --rsync-path="docker run --rm -i --volume ${remote_cache_dir}:${cache_dir} --entrypoint=/usr/bin/rsync wmfreleng/castor:v2017.10.30.21.03" \
+  --delete-delay \
+  jenkins-deploy@"${REMOTE_INSTANCE}:${cache_dir}/" "${DEST}"
+
+echo -e "\nDone"
