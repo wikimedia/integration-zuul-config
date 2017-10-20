8a06a586ecd0912b96ec31b02018f1297eb782f0
Antoine Musso <hashar@free.fr>
2017-10-20 17:17:25 +0200

    Castor for Docker jobs
    Change-Id: I3ac8679feb199f1347b395ac5359aaf3102bec8a

--- jjb/castor-load-sync.bash
+++ jjb/castor-load-sync.bash
@@ -2,11 +2,19 @@
 # Load cache from central repository
 set -u
 
+[[ $JOB_NAME == *'docker'* ]] && is_docker=1 || is_docker=''
+
+if [ $is_docker ]; then
+    DEST="/cache/"
+else
+    DEST="$HOME"
+fi
+
 echo "Syncing..."
 rsync \
   --archive \
   --compress \
   --contimeout 3 \
-  rsync://castor02.integration.eqiad.wmflabs:/caches/${CASTOR_NAMESPACE}/ $HOME \
+  rsync://castor02.integration.eqiad.wmflabs:/caches/"$CASTOR_NAMESPACE"/ "$DEST" \
   || :
 echo -e "\nDone"
