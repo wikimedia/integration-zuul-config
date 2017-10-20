a22882d1a1337c1269966316808b46615a212d8d
Antoine Musso <hashar@free.fr>
2017-10-20 17:17:25 +0200

    Castor for Docker jobs
    Bug: T179208
    Change-Id: I3ac8679feb199f1347b395ac5359aaf3102bec8a

---
+++ dockerfiles/castor/run.bash
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+case ${1:-missing} in
+    config)
+        source castor/castor-define-namespace.bash
+        ;;
+    load)
+        source castor/castor-define-namespace.bash
+        source castor/castor-load-sync.bash
+        ;;
+    *)
+        echo "USAGE:"
+        echo "  castor config: show the castor namespace"
+        echo "  castor load: restore from central cache"
+        exit 1
+        ;;
+esac
