7e563d0f1e9b2d2870e50ece2953f9e4f9f3e079
Kunal Mehta <legoktm@member.fsf.org>
2017-10-24 13:08:15 -0700

    Introduce ci-src-setup-simple and use it in composer-test
    Moves git cloning out of composer-test image. This is a different
    image from ci-src-setup as it doesn't use zuul-cloner and just clones
    the current repository into /src.
    
    Change-Id: I5fe4e53e367a7a5e949fb0f45c558cb499c8da15

---
+++ dockerfiles/ci-src-setup-simple/run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+cd /src
+
+git init
+git fetch --quiet --depth 1 "${ZUUL_URL}/${ZUUL_PROJECT}" "$ZUUL_REF"
+git checkout FETCH_HEAD
+git submodule --quiet update --init --recursive
