090eca96d0ec8970c19158c22cdf71b6afbc00f7
Kunal Mehta <legoktm@member.fsf.org>
2017-10-27 23:52:44 -0700

    Convert mediawiki-phpcs to use ci-src-setup-simple
    Change-Id: I5ac62223ffdd39527fd0f44b2ad278d618f4b0d7

--- dockerfiles/mediawiki-phpcs/run.sh
+++ dockerfiles/mediawiki-phpcs/run.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-cd /src
+umask 002
 
-git init
-git fetch --quiet --depth 1 "${ZUUL_URL}/${ZUUL_PROJECT}" "$ZUUL_REF"
-git checkout --quiet FETCH_HEAD
+cd /src
 
 composer install --no-progress --prefer-dist
 ./vendor/bin/phpcs -p -s --report-full "--report-checkstyle=/log/checkstyle.xml"
