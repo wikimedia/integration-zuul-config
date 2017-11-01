db7ca3cd1151dcde0651f9b748e5a14fbb2dba36
Antoine Musso <hashar@free.fr>
2017-11-01 15:35:27 +0100

    docker: add dev dependencies to tox
    Required to build some modules. That is only the basis though, most
    probably we would need some additional libraries, that can be enhanced
    later.
    
    Add python-wheel / python3-wheel, that lets pip generate a wheel of the
    compiled module and save it in the cache. That speeds up next builds
    once the cache has been warmed up.
    
    Update python3 pip version while at it. The jessie one is outdated.
    
    Add an example that installs 'netifaces' and ensure a wheel file has
    been populated in the cache.
    
    Change-Id: I6c0e7b00f8d7f3541be475f72144a8932d6b693e

--- dockerfiles/tox/example-run.sh
+++ dockerfiles/tox/example-run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+declare -i err=0
 
 install --mode 2777 --directory log
 docker run \
@@ -9,3 +10,17 @@ docker run \
     --env ZUUL_REF=master \
     --volume /"$(pwd)"/log://log \
      wmfreleng/tox:latest
+err+=$?
+
+# Ensure we can compile 'netifaces' and a wheel is hold in the cache
+docker run --rm --tty \
+    --entrypoint=/bin/bash wmfreleng/tox:latest \
+    -c 'pip install --disable-pip-version-check --target /src/install --cache-dir /src/cache -v netifaces && find /src/cache -name "netifaces-*.whl"'
+err+=$?
+
+if [ $err == 0 ]; then
+    echo "[OK] all examples passed"
+else
+    echo "[KO] $err example(s) failed"
+    exit 1
+fi
