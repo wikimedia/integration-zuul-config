9b51961276aec718d110052e0b4b8a898bddb42d
Antoine Musso <hashar@free.fr>
2017-11-03 23:08:01 +0100

    Fix castor deleting /home/jenkins/workspace on Nodepool
    4b7f18005 made castor-load to delete files at the destination. For
    Docker jobs it is /cache which we actually want to be 100% in sync.
    
    On Nodepool the destination is /home/jenkins and that ended up wiping
    the job workspace.
    
    It only surfaced for jobs using castor-load and deployed after 4b7f18005
    since I have not updated all the jobs when merging that change.
    
    Spotted on cumin-tox-publish which kept failling with:
    
    [castor-load]
    rsync --archive --delete-delay --compress --contimeout 3
    rsync://castor02.integration.eqiad.wmflabs:/caches/operations-software-cumin/master/cumin-tox-publish/
    /home/jenkins
    
    And then:
        java.io.IOException: Process working directory
        '/home/jenkins/workspace/cumin-tox-publish' doesn't exist!
    
    Bug: T179593
    Change-Id: Ibdbc616dd7b24a5cd69540f445ff3681fe9452ec

--- jjb/castor-load-sync.bash
+++ jjb/castor-load-sync.bash
@@ -8,14 +8,18 @@ if [ $is_docker ]; then
     # For containers we mount $WORKSPACE/cache from the host to /cache in the
     # container. It is also the value of XDG_CACHE_HOME
     DEST="/cache"
+    # cache might persist between builds on the Docker slaves
+    rsync_delete='--delete-delay'
 else
     DEST="$HOME"
+    # On Nodepool it is guaranteed to be empty. Deleting would wipe
+    # /home/jenkins/workspace!
 fi
 
 echo "Syncing..."
 rsync \
   --archive \
-  --delete-delay \
+  ${rsync_delete:-} \
   --compress \
   --contimeout 3 \
   rsync://castor02.integration.eqiad.wmflabs:/caches/"$CASTOR_NAMESPACE"/ "$DEST" \
