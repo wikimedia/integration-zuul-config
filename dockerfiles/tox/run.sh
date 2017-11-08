07e4e814efa1f4a72bacd27017f474edcd2ad61f
Antoine Musso <hashar@free.fr>
2017-11-08 09:02:05 +0100

    docker: enhance tox image for signals
    Change Docker ENTRYPOINT to exec form, else our script is wrapped in a
    sh -c '', which shallows signals.
    
    Make run.sh executable.
    
    Drop a misleading comment about setting envlogdir in tox.
    
    Rename the trap function and register it after the git commands. In case
    git fails, that will no more attempts to capture inexistents tox log
    files.
    
    Stop logging to stdout. It is already in the Jenkins console.
    
    Change-Id: Ia1f481f1f7fb1d41dee671e7fad429a694e60dfd

--- dockerfiles/tox/run.sh
+++ dockerfiles/tox/run.sh
@@ -7,17 +7,12 @@ umask 002
 LOG_DIR=/log
 export LOG_DIR
 
-capture_logs() {
+capture_tox_logs() {
     # XXX unlike `mv`, `cp` does not preserve permissions and hence the
     # destination files will inherit the group thanks to log having setgid.
-    #
-    # XXX later tox version supports specifying the envs log directory
-    #
     cp --recursive /src/.tox/*/log/*.log "${LOG_DIR}" || /bin/true
     cp --recursive /src/.tox/log "${LOG_DIR}" || /bin/true
 }
-
-trap capture_logs EXIT
 
 cd /src
 
@@ -26,6 +21,8 @@ git fetch --depth 2 --quiet "${ZUUL_URL}/${ZUUL_PROJECT}" "$ZUUL_REF"
 git checkout --quiet FETCH_HEAD
 git submodule --quiet update --init --recursive
 
+trap capture_tox_logs EXIT
+
 # Run tests. Pass all environment variables to tox since the environment here
 # is already pretty restrictive.
-TOX_TESTENV_PASSENV="*" PY_COLORS=1 tox -v | tee "${LOG_DIR}/stdout.log"
+TOX_TESTENV_PASSENV="*" PY_COLORS=1 tox -v
