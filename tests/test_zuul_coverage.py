085b14c35a611b5fb11975742c68b2d78efb4b0d
Antoine Musso <hashar@free.fr>
2017-11-28 10:10:38 +0100

    Get rid of class in test_zuul_coverage
    The qa suite spurts a long difference between the list of Zuul repos and
    the ones in Gerrit. It is not very convenient to track over time which
    repos got fixed.
    A way to fix it is to have a test per repository.  Nosetest supports
    yelding tests, unfortunately it does not support it when the test class
    extends unittest.TestCase.
    
    Refactor tests/test_zuul_coverage.py toward a module with functions
    instead of being a class.
    
    Change-Id: I1709f2dbf167c75291ebc4450e886bd8c64175b4

--- tests/test_zuul_coverage.py
+++ tests/test_zuul_coverage.py
@@ -37,67 +37,80 @@ GERRIT_IGNORE = (
     'wiktionary/anagrimes',  # some legacy perl
 )
 
+GERRIT_REPOS = {}
+ZUUL_PROJECTS = []
+
+
+def getGerritRepos():
+    req = urllib.urlopen(
+        'https://gerrit.wikimedia.org/r/projects/?type=code&description')
+    # Strip Gerrit json harness: )]}'
+    req.readline()
+    return {
+        name: meta['state']
+        for (name, meta) in json.load(req).iteritems()
+    }
+
+
+def getZuulLayoutProjects():
+    layout = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        '../zuul/layout.yaml')
+    with open(layout) as f:
+        projects = [p['name'] for p in yaml.safe_load(f)['projects']]
+    return projects
+
+
+def setup():
+    global GERRIT_REPOS
+    global ZUUL_PROJECTS
+    GERRIT_REPOS = getGerritRepos()
+    ZUUL_PROJECTS = getZuulLayoutProjects()
+
+test = unittest.TestCase('__init__')
+test.maxDiff = None
+test.longMessage = True
+qa = True  # attribute for nose filtering
+
+
+def assert_have_gate_and_submit(prefix):
+    missing = [repo for (repo, state) in GERRIT_REPOS.iteritems()
+               if repo.startswith(prefix)
+               and len(repo.split('/')) == 3  # skip sub repos
+               and state == 'ACTIVE'
+               and repo not in ZUUL_PROJECTS]
+    test.assertEqual(
+        [], sorted(missing),
+        '%s %s are not configured in zuul' % (len(missing), prefix))
+
 
 @attr('qa')
-class TestZuulCoverage(unittest.TestCase):
+def test_all_extensions_have_gate_and_submit():
+    assert_have_gate_and_submit('mediawiki/extensions/')
 
-    maxDiff = None
-    _repos = None
 
-    @classmethod
-    def setUpClass(cls):
-        req = urllib.urlopen(
-            'https://gerrit.wikimedia.org/r/projects/?type=code&description')
-        # Strip Gerrit json harness: )]}'
-        req.readline()
-        cls._repos = {
-            name: meta['state']
-            for (name, meta) in json.load(req).iteritems()
-        }
+@attr('qa')
+def test_all_skins_have_gate_and_submit():
+    assert_have_gate_and_submit('mediawiki/skins/')
 
-    def getLayoutProjects(self):
-        layout = os.path.join(
-            os.path.dirname(os.path.abspath(__file__)),
-            '../zuul/layout.yaml')
-        with open(layout) as f:
-            projects = [p['name'] for p in yaml.safe_load(f)['projects']]
-        return projects
 
-    def assert_have_gate_and_submit(self, prefix):
-        projects = self.getLayoutProjects()
-        missing = [repo for (repo, state) in self._repos.iteritems()
-                   if repo.startswith(prefix)
-                   and len(repo.split('/')) == 3  # skip sub repos
-                   and state == 'ACTIVE'
-                   and repo not in projects]
+@attr('qa')
+def test_zuul_projects_are_in_gerrit():
+    zuul = set(ZUUL_PROJECTS)
+    gerrit = set(GERRIT_REPOS)
+    test.assertEqual(
+        [], sorted((zuul & gerrit) ^ zuul),
+        'Projects configured in Zuul but no more active in Gerrit')
 
-        self.longMessage = True
-        self.assertEqual(
-            [], sorted(missing),
-            '%s %s are not configured in zuul' % (len(missing), prefix))
 
-    def test_all_extensions_have_gate_and_submit(self):
-        self.assert_have_gate_and_submit('mediawiki/extensions/')
-
-    def test_all_skins_have_gate_and_submit(self):
-        self.assert_have_gate_and_submit('mediawiki/skins/')
-
-    def test_zuul_projects_are_in_gerrit(self):
-        zuul = set(self.getLayoutProjects())
-        gerrit = set(self._repos.keys())
-        self.longMessage = True
-        self.assertEqual(
-            [], sorted((zuul & gerrit) ^ zuul),
-            'Projects configured in Zuul but no more active in Gerrit')
-
-    def test_gerrit_active_projects_are_in_zuul(self):
-        zuul = set(self.getLayoutProjects())
-        gerrit_active = set([
-            repo for (repo, state) in self._repos.iteritems()
-            if state == 'ACTIVE'
-            and repo not in GERRIT_IGNORE
-        ])
-        self.longMessage = True
-        self.assertEqual(
-            [], sorted((zuul & gerrit_active) ^ gerrit_active),
-            'Projects in Gerrit are all configured in Gerrit')
+@attr('qa')
+def test_gerrit_active_projects_are_in_zuul():
+    zuul = set(ZUUL_PROJECTS)
+    gerrit_active = set([
+        repo for (repo, state) in GERRIT_REPOS.iteritems()
+        if state == 'ACTIVE'
+        and repo not in GERRIT_IGNORE
+    ])
+    test.assertEqual(
+        [], sorted((zuul & gerrit_active) ^ gerrit_active),
+        'Projects in Gerrit are all configured in Gerrit')
