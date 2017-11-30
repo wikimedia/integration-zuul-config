a62c6117cdf82db1a69501c9c713f8aae4772fe2
Tyler Cipriani <tcipriani@wikimedia.org>
2017-11-30 16:28:23 -0700

    Fix composer-test example-run.sh
    The example-run for this image has not been updated since the creation
    of ci-src-setup. As a result, the example-run will fail since the image
    no longer checks out source code by itself.
    
    Change-Id: I81bc37b6f125d9db4a8790e7eaebe75de20c7563

--- dockerfiles/composer-test/example-run.sh
+++ dockerfiles/composer-test/example-run.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-install --mode 777 --directory log
+mkdir -m 2777 -p log src cache
+
+git init src
+git -C src fetch --quiet --depth 1 \
+    "https://gerrit.wikimedia.org/r/integration/jenkins" \
+    "refs/changes/31/316231/4"
+git -C src checkout FETCH_HEAD
+
 docker run \
     --rm --tty \
-    --env ZUUL_URL=https://gerrit.wikimedia.org/r \
-    --env ZUUL_PROJECT=integration/jenkins \
-    --env ZUUL_COMMIT=7a4ee7963a15dbdc5d5afb363600d63574bb31a0 \
-    --env ZUUL_REF=refs/changes/31/316231/4 \
-    --volume /$(pwd)/log://var/lib/jenkins/log \
-     wmfreleng/composer-test:latest
+    --volume /$(pwd)/log:/log \
+    --volume /$(pwd)/cache:/cache \
+    --volume /$(pwd)/src:/src \
+    wmfreleng/composer-test:latest
+    # docker-registry.wikimedia.org/releng/composer-test:0.1.0
+
+rm -rf src log cache
