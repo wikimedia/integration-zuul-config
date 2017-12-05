accf497309577f96782a08ef4cbfae335cb38892
Antoine Musso <hashar@free.fr>
2017-12-05 21:26:00 +0100

    I DONT KNOW WHAT I AM DOING
    Change-Id: I7a895aede4ebb5f0a100f826de6bb4cb590cb0f1

--- dockerfiles/npm-browser-test/example-run.sh
+++ dockerfiles/npm-browser-test/example-run.sh
@@ -5,9 +5,10 @@ set -euo pipefail
 mkdir -m 777 -p log
 mkdir -m 777 -p src
 mkdir -m 777 -p cache
+mkdir -m 777 -p cache/node_modules
 cd src
 git init
-git fetch --quiet --depth 1 https://gerrit.wikimedia.org/r/data-values/value-view
+git fetch --quiet --depth 1 https://gerrit.wikimedia.org/r/VisualEditor/VisualEditor
 git checkout FETCH_HEAD
 cd ..
 
@@ -15,6 +16,7 @@ docker run \
     --rm --tty \
     --volume /"$(pwd)"/log://var/lib/jenkins/log \
     --volume /"$(pwd)"/cache://cache \
+    --volume /"$(pwd)"/cache/node_modules://src/node_modules \
     --volume /"$(pwd)"/log://log \
     --volume /"$(pwd)"/src://src \
      wmfreleng/npm-browser-test:latest
