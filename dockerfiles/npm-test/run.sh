e15cf19c63f65f65f9dc2f8e7f8117f16a65932b
Antoine Musso <hashar@free.fr>
2017-12-05 21:26:00 +0100

    (WIP) Cache node_modules between runs
    The rough idea is after 'npm install', to copy the resulting
    'node_modules' to XDG_CACHE_CONFIG.  That is then saved by castor and
    restored on the next build.
    
    In case package.json has changed the dependencies, 'npm prune' takes
    care of bringing the cached node_modules up-to-date.
    
    That is solely for Docker containers though.
    
    Bug: T159591
    Change-Id: I7a895aede4ebb5f0a100f826de6bb4cb590cb0f1

--- dockerfiles/npm-test/run.sh
+++ dockerfiles/npm-test/run.sh
@@ -8,6 +8,23 @@ cd /src
 
 node --version
 npm --version
-rm -rf node_modules
+
+# Attempt to reste node_modules from the cache if any.
+rsync \
+    --archive \
+    --ignore-missing-args \
+    --delete-delay \
+    "$XDG_CACHE_HOME"/node_modules /src
+# Clean out packages that might no more be listed on the dependencies list
+npm prune
+
 npm install --no-progress
+
+# Save node_modules to the cache
+rsync \
+    --archive \
+    --ignore-missing-args \
+    --delete-delay \
+    /src/node_modules "$XDG_CACHE_HOME"
+
 npm test
