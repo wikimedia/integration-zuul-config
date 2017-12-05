e1485ff79e0fc1ec82c64e5979c7897eb6db4d41
Antoine Musso <hashar@free.fr>
2017-12-05 21:26:00 +0100

    I DONT KNOW WHAT I AM DOING
    Change-Id: I7a895aede4ebb5f0a100f826de6bb4cb590cb0f1

--- dockerfiles/npm-browser-test/example-run.sh
+++ dockerfiles/npm-browser-test/example-run.sh
@@ -5,11 +5,14 @@ set -euo pipefail
 mkdir -m 777 -p log
 mkdir -m 777 -p src
 mkdir -m 777 -p cache
-cd src
-git init
-git fetch --quiet --depth 1 https://gerrit.wikimedia.org/r/data-values/value-view
-git checkout FETCH_HEAD
-cd ..
+
+docker run \
+    --rm --tty \
+    --volume /"$(pwd)"/src://src \
+    -e ZUUL_URL=https://gerrit.wikimedia.org/r/ \
+    -e ZUUL_PROJECT=VisualEditor/VisualEditor \
+    -e ZUUL_REF=master \
+    wmfreleng/ci-src-setup-simple:v2017.10.28.06.19
 
 docker run \
     --rm --tty \
