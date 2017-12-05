339cf36d5629bd60ff21a53441ee37694b44ba35
Antoine Musso <hashar@free.fr>
2017-12-05 21:26:00 +0100

    I DONT KNOW WHAT I AM DOING
    Change-Id: I7a895aede4ebb5f0a100f826de6bb4cb590cb0f1

--- dockerfiles/npm-test/run.sh
+++ dockerfiles/npm-test/run.sh
@@ -8,6 +8,16 @@ cd /src
 
 node --version
 npm --version
-rm -rf node_modules
+
+rsync \
+    --archive \
+    --delete-delay \
+    "$XDG_CACHE_HOME"/node_modules/ /src/node_modules/
+npm prune
 npm install --no-progress
+
+rsync \
+    --archive \
+    --delete-delay \
+    /src/node_modules/ "$XDG_CACHE_HOME"/node_modules/
 npm test
