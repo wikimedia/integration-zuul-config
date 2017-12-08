819dce60db48827815c18fe695741f732e6a571a
Antoine Musso <hashar@free.fr>
2017-12-08 17:12:13 +0100

    Migrate Graphoid npm job to Docker
    Change-Id: Iaf38d4e8a4b62f2685d3c6800e366b63302442d1

---
+++ dockerfiles/npm-test-graphoid/example-run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -eux -o pipefail
+
+install --mode 777 --directory log cache src
+cd src
+git init
+git fetch --quiet --depth 1 "https://gerrit.wikimedia.org/r/mediawiki/services/graphoid" "master"
+git checkout FETCH_HEAD
+cd ..
+
+docker run \
+    --rm --tty \
+    --env JENKINS_URL=1 \
+    --volume /"$(pwd)"/log://var/lib/jenkins/log \
+    --volume /"$(pwd)"/cache://cache \
+    --volume /"$(pwd)"/src://src \
+     wmfreleng/npm-test-graphoid:latest
