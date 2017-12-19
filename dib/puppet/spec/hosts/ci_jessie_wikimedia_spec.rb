27bed685c7e3358a7dd3a28d2b16beafef9d34dd
Antoine Musso <hashar@free.fr>
2017-12-19 22:36:23 +0100

    dib set facts via rspec-puppet-facts
    With puppet 4.8.2 and since rspec-puppet 2.6.4, the spec were failing
    when being run on Debian Stretch with:
    
    Warning: Found multiple default providers for service:
       runit, debian, redhat; using runit
     RuntimeError:
       Could not find the daemon directory (tested [/etc/sv,/var/lib/service])
    
    The reason is rspec-puppet better isolate facts/providers with v2.6.4
    and Puppet 4.8.2 does not recognize Debian Stretch (version 9).
    
    One could try to set the few facts that are needed. Instead Giuseppe
    pointed at rspec-puppet-facts which set the proper facts automagically.
    
    Add rspec-puppet-facts and include it in the sole spec we have.
    Stop setting facts manually and rely on the gem.
    Merge in our custom {initsystem: 'systemd'}
    
    We only care on Jessie for Nodepool.
    
    rspec-puppet issue I filled about it:
    https://github.com/rodjek/rspec-puppet/issues/629
    
    Signed-off-by: Giuseppe Lavagetto <glavagetto@wikimedia.org>
    Change-Id: I7c9ca7619dede601dc65479b60bf9c06c26afb25

--- dib/puppet/spec/hosts/ci_jessie_wikimedia_spec.rb
+++ dib/puppet/spec/hosts/ci_jessie_wikimedia_spec.rb
@@ -1,5 +1,7 @@
 require 'rspec-puppet'
+require 'rspec-puppet-facts'
 require 'puppetlabs_spec_helper/puppet_spec_helper'
+include RspecPuppetFacts
 
 RSpec.configure do |c|
     c.manifest = File.expand_path(File.join(__dir__, '..', '..', 'ciimage.pp'))
@@ -19,10 +21,20 @@ if ENV['PUPPET_DEBUG']
 end
 
 describe 'ci-jessie-wikimedia' do
-    let(:facts) { {
-        :initsystem => 'systemd',
-        :lsbdistrelease => '8.9',
-        :lsbdistid => 'Debian',
-    } }
-    it { should compile }
+    test_on = {
+        supported_os: [
+            {
+                'operatingsystem' => 'Debian',
+                'operatingsystemrelease' => ['8'],
+            }
+        ],
+    }
+    on_supported_os(test_on).each do |os, facts|
+        context "on #{os}" do
+            let(:facts) do
+                facts.merge({initsystem: 'systemd'})
+            end
+            it { should compile }
+        end
+    end
 end
