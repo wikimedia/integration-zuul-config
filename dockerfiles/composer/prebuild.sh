0fe46013b31de64d0e39fea177a6485c18afac9a
Antoine Musso <hashar@free.fr>
2018-01-11 14:19:17 +0100

    Migrate composer to docker-pkg
    Change-Id: Ieaee9bc2714cc065399de093710c3253b240d5f9

--- dockerfiles/composer/prebuild.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-# This is copied in Dockerfile to ensure that a build step grabs a fresh
-# copy of the git repo when it is updated rather than using a layer from
-# the local Docker cache.
-
-git ls-remote --exit-code https://gerrit.wikimedia.org/r/p/integration/composer.git refs/heads/master > .cache-buster-composer
