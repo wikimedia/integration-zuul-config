e4bffb414e725915f7fb37141b3d17b3d6bdb0da
Antoine Musso <hashar@free.fr>
2018-01-11 14:30:17 +0100

    Migrate composer-package to docker-pkg
    Update:
     composer-package-php70-docker
     mwgate-composer-package-php70-docker
    
    Bug: T177276
    Change-Id: I5365eff94ab411c4cce30fab991d4a695772c58c

--- dockerfiles/composer-package/example-run.sh
+++ dockerfiles/composer-package/example-run.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
+set -eux -o pipefail
+
+mkdir -m 777 -p cache
 mkdir -m 777 -p log
 mkdir -m 777 -p src
+(
 cd src
 git init
-git fetch --quiet --depth 1 "https://gerrit.wikimedia.org/r/utfnormal" "refs/changes/57/375857/1"
+git fetch --quiet --depth 1 "https://gerrit.wikimedia.org/r/utfnormal" "master"
 git checkout FETCH_HEAD
-cd ..
+)
 
 mkdir -p log
 docker run \
     --rm --tty \
-    --volume /$(pwd)/log://var/lib/jenkins/log \
-    --volume /$(pwd)/src://src \
-     wmfreleng/composer-package:latest
-rm -rf src
-rm -rf log
+    --volume "/$(pwd)/cache://cache" \
+    --volume "/$(pwd)/log://var/lib/jenkins/log" \
+    --volume "/$(pwd)/src://src" \
+    docker-registry.wikimedia.org/releng/composer-package:latest
--- dockerfiles/composer-package/run.sh
+++ dockerfiles/composer-package/run.sh
@@ -8,4 +8,4 @@ cd /src
 
 composer --ansi validate
 composer install --no-progress --prefer-dist
-composer test
+exec composer test
