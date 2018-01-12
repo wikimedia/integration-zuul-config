0bd5a68c451533f47c49511e3bbf43d470d842c6
Antoine Musso <hashar@free.fr>
2018-01-12 14:54:00 +0100

    Migrate mediawiki-phpcs to docker-pkg
    Bug: T177276
    Change-Id: I3dbfabe51fc7eb620e30b288dd90957e5dd4bf4f

--- dockerfiles/mediawiki-phpcs/example-run.sh
+++ dockerfiles/mediawiki-phpcs/example-run.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
-install --mode 777 --directory log
+set -euo pipefail
+
+install --mode 777 --directory cache log src
+
 docker run \
     --rm --tty \
     --env ZUUL_URL=https://gerrit.wikimedia.org/r \
     --env ZUUL_PROJECT=mediawiki/core \
     --env ZUUL_COMMIT=e447a97de58e9e2b4dd6a6e31a91edc68dde1217 \
     --env ZUUL_REF=refs/changes/52/378752/2 \
-    --volume /$(pwd)/log://var/lib/jenkins/log \
-     wmfreleng/mediawiki-phpcs:latest
+    --volume "/$(pwd)/cache://cache" \
+    --volume "/$(pwd)/src://src" \
+    docker-registry.wikimedia.org/releng/ci-src-setup-simple:latest
+
+docker run \
+    --rm --tty \
+    --volume "/$(pwd)/cache:/cache" \
+    --volume "/$(pwd)/log://var/lib/jenkins/log" \
+    --volume "/$(pwd)/src://src" \
+    docker-registry.wikimedia.org/releng/mediawiki-phpcs:latest
