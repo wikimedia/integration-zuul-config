817815c30b1f82902bc17d0b7bf213ab3243e062
Željko Filipin <zeljko.filipin@gmail.com>
2018-01-18 13:17:02 +0100

    WIP AdvancedSearch needs CirrusSearch
    Change-Id: Id875d14bfbbee5479861d52e8ea4c2eee0691530

--- zuul/parameter_functions.py
+++ zuul/parameter_functions.py
@@ -140,6 +140,7 @@ dependencies = {
     # One can add a skin by using: 'skin/XXXXX'
     '3D': ['MultimediaViewer'],
     'AbuseFilter': ['AntiSpoof'],
+    'AdvancedSearch': ['CirrusSearch'],
     'ApiFeatureUsage': ['Elastica'],
     'Arrays': ['Loops', 'ParserFunctions', 'Variables'],
     'ArticlePlaceholder': ['Wikibase', 'Scribunto'],
