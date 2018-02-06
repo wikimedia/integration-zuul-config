5744cef022a0b1de7dcc9f711ec56921e944d5e0
Tpt <thomaspt@hotmail.fr>
2018-02-06 21:07:53 +0100

    Adds a dependency on Wikibase to Wikisource extension
    Change-Id: I31f95e74da946bf3eec053bf2e0344eebe4687a1

--- zuul/parameter_functions.py
+++ zuul/parameter_functions.py
@@ -318,6 +318,7 @@ dependencies = {
     'WikiEditor': ['EventLogging'],
     'WikimediaBadges': ['Wikibase'],
     'WikimediaEvents': ['EventLogging'],
+    'Wikisource': ['Wikibase'],
     'wikihiero': ['VisualEditor'],
     'ZeroBanner': ['Echo', 'JsonConfig', 'MobileFrontend', 'VisualEditor',
                    'ZeroPortal'],
