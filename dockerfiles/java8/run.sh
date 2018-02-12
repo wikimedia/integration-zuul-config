b4f567bae8ccfe5fdce1c87c4fe8ba80996fc7ee
Antoine Musso <hashar@free.fr>
2018-02-12 17:20:45 +0100

    docker: fix java8 entrypoint
    Change-Id: Ia3e761972e3501ebaaaf451e2dcf28975ae65069

--- dockerfiles/java8/run.sh
+++ dockerfiles/java8/run.sh
@@ -11,4 +11,4 @@ git fetch --depth 2 --quiet "${ZUUL_URL}/${ZUUL_PROJECT}" "$ZUUL_REF"
 git checkout --quiet FETCH_HEAD
 git submodule --quiet update --init --recursive
 
-exec maven "${@}"
+exec /usr/local/bin/mvn "${@}"
