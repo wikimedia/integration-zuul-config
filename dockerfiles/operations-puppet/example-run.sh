0c051ba03468969844434d2783d31a278908751c
Valentin Gutierrez <vgutierrez@wikimedia.org>
2018-03-27 17:16:29 +0200

    Docker: use new operations-puppet image
    Bug: T184942
    Change-Id: I03e3b24a64c50688ff65c797fbd31e014b749f5a

--- dockerfiles/operations-puppet/example-run.sh
+++ dockerfiles/operations-puppet/example-run.sh
@@ -8,4 +8,4 @@ docker run \
     --env ZUUL_COMMIT=72d31ffb0fa612482d268b6f5484785842cd0fda \
     --env ZUUL_REF=refs/changes/49/374349/2 \
     --volume /$(pwd)/log://srv/workspace/log \
-    docker-registry.wikimedia.org/releng/operations-puppet:0.3.0
+    docker-registry.wikimedia.org/releng/operations-puppet:0.3.1
