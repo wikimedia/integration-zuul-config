7b32825bce41a23a3e9c5ea92716ed166d09db31
Antoine Musso <hashar@free.fr>
2018-04-19 18:08:33 +0200

    mediawiki/core phpcs job and docker container
    Superseeded by Quibble.
    
    Change-Id: Ica4d334203e2a43411a987dfcbdc8a96e81f43cc

--- dockerfiles/mediawiki-phpcs/Dockerfile.template
+++
@@ -1,5 +0,0 @@
-FROM {{ "composer" | image_tag }}
-
-USER nobody
-COPY run.sh /run.sh
-ENTRYPOINT ["/run.sh"]
--- dockerfiles/mediawiki-phpcs/run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-umask 002
-
-cd /src
-
-composer install --no-progress --prefer-dist
-./vendor/bin/phpcs -p -s --report-full "--report-checkstyle=/log/checkstyle.xml"
