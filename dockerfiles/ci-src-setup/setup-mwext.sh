b4afc1f196858e47414b9aa8a3a092c89f6883a2
Kunal Mehta <legoktm@member.fsf.org>
2018-05-01 23:40:43 -0700

    Use $THING_SUBNAME in ci-src-setup image
    Change-Id: Ib7baec713f70271ccb9b966cd2c05bdf9320cbc6

--- dockerfiles/ci-src-setup/setup-mwext.sh
+++ dockerfiles/ci-src-setup/setup-mwext.sh
@@ -39,5 +39,5 @@ set -u
 composer --ansi validate --no-check-publish
 /srv/deployment/integration/slave-scripts/bin/mw-create-composer-local.py "/tmp/extensions_load.txt" composer.local.json
 composer update --ansi --no-progress --prefer-dist --profile --no-dev
-cd /src/extensions/$EXT_NAME
+cd /src/$THING_SUBNAME
 composer update --ansi --no-progress --prefer-dist --profile
