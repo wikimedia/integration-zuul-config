481a4b59dd672cd9066bd3af9b0a62c612dad50a
Kunal Mehta <legoktm@member.fsf.org>
2018-05-13 23:25:15 -0700

    audit-resources: Clean up empty log files
    Change-Id: Ieb67bae40b36171c1c64a0d052a61b60b5fab208

--- dockerfiles/wikimedia-audit-resources/run.sh
+++ dockerfiles/wikimedia-audit-resources/run.sh
@@ -10,3 +10,5 @@ python3 -m virtualenv -p python3 venv
 source venv/bin/activate
 pip install -r requirements.txt
 ./run.py --start-xvfb --threads 1 --logdir /log --fast
+# Clean up empty log files
+find /log -empty -type f -delete
