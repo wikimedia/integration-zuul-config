4d1d3cb452a0f860ddc08a1d81b9b93a9eb6057a
Chad Horohoe <chadh@wikimedia.org>
2018-05-19 08:17:08 -0700

    WIP: Bazel build with java8
    Totally untested. Probably busted as hell
    
    Change-Id: Ic08e5ea99fe6b12f0a0b7d18c34374ffcbeed822

---
+++ dockerfiles/java8-bazel/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+umask 002
+
+cd /src
+
+git init
+git fetch --depth 2 --quiet "${ZUUL_URL}/${ZUUL_PROJECT}" "$ZUUL_REF"
+git checkout --quiet FETCH_HEAD
+git submodule --quiet update --init --recursive
+
+exec /usr/local/bin/bazel "${@}"
