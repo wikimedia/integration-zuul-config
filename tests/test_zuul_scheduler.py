wikimedia/integration-zuul-config

commit bc1dfe4571af83978b76c8a54617a1ecb2cfd97a

Only run tests for tarball extensions in shared job on REL1_XX branches
Change-Id: I8a625b0450e14bf2337400b4d1402cc071922cb6

zuul/parameter_functions.py
@@ -436,15 +436,22 @@ def get_dependencies(key, mapping):
     return resolve_deps(key)
 
 
+tarballextensions = [
+    'Cite',
+    'CodeEditor',
+    'ConfirmEdit',
+    'ParserFunctions',
+    'PdfHandler',
+    'SpamBlacklist',
+    'WikiEditor',
+]
+
 gatedextensions = [
     'AbuseFilter',
     'Babel',
     'CheckUser',
     'CirrusSearch',
-    'Cite',
     'cldr',
-    'CodeEditor',
-    'ConfirmEdit',
     'Echo',
     'Elastica',
     'EventLogging',
@@ -457,10 +464,7 @@ gatedextensions = [
     'MobileFrontend',
     'MwEmbedSupport',
     'NavigationTiming',
-    'ParserFunctions',
-    'PdfHandler',
     'SandboxLink',
-    'SpamBlacklist',
     'SiteMatrix',
     'Thanks',
     'TimedMediaHandler',
@@ -469,7 +473,6 @@ gatedextensions = [
     'VisualEditor',
     # Note: pre-1.31 this is switched out for Wikidata build extension
     'Wikibase',
-    'WikiEditor',
     'ZeroBanner',
     'ZeroPortal',
 ]
@@ -486,12 +489,12 @@ def set_gated_extensions(item, job, params):
     ):
         deps.append(params['ZUUL_PROJECT'].split('/')[-1])
 
-    deps.extend(gatedextensions)
-    # Prior to 1.31, we need to use the "Wikidata" build extension
-    use_wikidata = ('REL1_27', 'REL1_29', 'REL1_30')
-    if params['ZUUL_BRANCH'] in use_wikidata:
-        deps.remove('Wikibase')
-        deps.append('Wikidata')
+    deps.extend(tarballextensions)
+
+    # Only run gate extensions on non REL1_XX branches
+    if not params['ZUUL_BRANCH'].startswith('REL1_'):
+        deps.extend(gatedextensions)
+
     deps = sorted(list(set(deps)))
 
     params['EXT_DEPENDENCIES'] = '\\n'.join(

tests/test_zuul_scheduler.py
@@ -814,7 +814,10 @@ class TestZuulScheduler(unittest.TestCase):
             os.path.dirname(os.path.abspath(__file__)),
             '../zuul/parameter_functions.py'),
             global_env)
+        tarballextensions = set(global_env['tarballextensions'])
         gatedextensions = set(global_env['gatedextensions'])
+
+        allextensions = tarballextensions.union(gatedextensions)
 
         # Grab projects having the gate job 'mediawiki-extensions-hhvm'
         gated_in_zuul = set([
@@ -825,7 +828,7 @@ class TestZuulScheduler(unittest.TestCase):
         ])
 
         self.assertSetEqual(
-            gated_in_zuul, gatedextensions,
+            gated_in_zuul, allextensions,
             msg='Zuul projects triggering gate jobs (first set) and '
             'dependencies list in zuul/parameter_functions.py (second set) '
             'must be equals.\n'

tests/test_zuul_set_gated_extensions.py
@@ -48,7 +48,7 @@ class TestSetGatedExtensions(unittest.TestCase):
         self.assertNotIn('\\nmediawiki/extensions/Wikidata',
                          params['EXT_DEPENDENCIES'])
 
-    def test_wikibase_onethirty(self):
+    def test_wikibase_notonethirty(self):
         params = {
             'ZUUL_PIPELINE': 'test',
             'ZUUL_PROJECT': 'mediawiki/core',
@@ -56,7 +56,5 @@ class TestSetGatedExtensions(unittest.TestCase):
         }
         gate_job = FakeJob('mediawiki-extensions-foo')
         set_gated_extensions(None, gate_job, params)
-        self.assertIn('\\nmediawiki/extensions/Wikidata',
-                      params['EXT_DEPENDENCIES'])
         self.assertNotIn('\\nmediawiki/extensions/Wikibase',
                          params['EXT_DEPENDENCIES'])

commit a127068961e38bc13a84cd1f31a3ea421e3b510c

Gated extensions now use Quibble on REL branches
For gated extensions, such as Translate or TimedMediaHandler, the
extension-gate was still triggering the old mediawiki-extensions jobs
for release branches.

bc1dfe4571af caused the jobs to no more inject the dependencies, or at
least to inject them improperly (T197933).

Introduce 'release-quibble-*' jobs which are merely an alias for the
regular quibble jobs.
Flag them in Zuul so that they only get triggered on REL branches.
For extensions and skins, skip mediawiki-extensions jobs on REL
branches.
Add a few random tests.

This way for gated extensions we use:

| master | wmf-quibble
| wmf    | wmf-quibble
| REL    | release-quibble

For regular extensions, they use the regular jobs regardless of the
branch.

The mediawiki-extensions jobs are now solely triggered by mediawiki/core
and mediawiki/vendor until Quibble is ready for them (T197469).

Bug: T197469
Bug: T197933
Change-Id: Ic17a870a70ecafd97aadb059314944a772bdebd6

jjb/mediawiki.yaml
@@ -524,6 +524,14 @@
         - timestamps
         - ansicolor
 
+# For the REL branches
+- job-template:
+    !!merge : *job_quibble
+    name: release-quibble-{packages-source}-{database}-{php}-docker
+    triggers:
+        - zuul
+
+# For the master and wmf/* branches
 - job-template:
     !!merge : *job_quibble
     name: wmf-quibble-vendor-mysql-{php}-docker
@@ -652,6 +660,7 @@
         - quibble-{packages-source}-{database}-{php}-docker
         - mediawiki-quibble-{packages-source}-{database}-{php}-docker
         - mediawiki-quibble-composertest-{php}-docker
+        - release-quibble-{packages-source}-{database}-{php}-docker
         - wmf-quibble-vendor-mysql-{php}-docker:
             # We never use composer on wmf cluster
             packages-source: vendor

zuul/layout.yaml
@@ -832,7 +832,7 @@ jobs:
       - branch: (?:^(REL1_(2[789]|30))$)
 
   # Run MediaWiki PHP 5.5 tests on MW < 1.31
-  - name: ^(quibble|mediawiki|mwext|mwskin|mwgate).*php55.*$
+  - name: ^(release-quibble|quibble|mediawiki|mwext|mwskin|mwgate).*php55.*$
     branch: ^(REL1_(2[789]|30))$
 
   - name: ^quibble-.*-docker$
@@ -841,6 +841,8 @@ jobs:
     skip-if:
       - project: '^mediawiki/core$'
         branch: ^(fundraising/REL1_27)$
+      # For release branches we use release-quibble-* jobs
+      - branch: ^(REL)
 
   - name: lintr-docker
     files:
@@ -942,12 +944,14 @@ jobs:
     branch: (?!fundraising/REL.*)
     queue-name: mediawiki
     skip-if:
-      # We use Quibble for extensions and skins but not on release branches
+      # We use Quibble for extensions and skins
       # T197469
       - project: ^mediawiki/(extensions|skins)/
-        branch: ^(?!REL)
 
   # Replacements for mediawiki-extensions jobs
+
+  - name: ^release-quibble-
+    branch: ^REL
 
   - name: ^wmf-quibble-
     # Gated extensions are only for master and Wikimedia deployment. We do not
@@ -1298,15 +1302,19 @@ project-templates:
       - mediawiki-extensions-php55-jessie
       - mediawiki-extensions-php70-jessie
       - wmf-quibble-vendor-mysql-php70-docker
+      - release-quibble-vendor-mysql-php55-docker
     gate-and-submit: &extension-gate-gate-and-submit
       - mediawiki-extensions-hhvm-jessie
       - mediawiki-extensions-php55-jessie
       - mediawiki-extensions-qunit-jessie
       - mediawiki-extensions-php70-jessie
       # Quibble replacements for mediawiki-extensions
-      # Only triggered on master and wmf/ branches
+      # Only triggered on master and wmf/ branches:
       - wmf-quibble-vendor-mysql-hhvm-docker
       - wmf-quibble-vendor-mysql-php70-docker
+      # Only triggered on REL branches:
+      - release-quibble-vendor-mysql-hhvm-docker
+      - release-quibble-vendor-mysql-php70-docker
     gate-and-submit-swat: *extension-gate-gate-and-submit
     experimental:
       - mwext-php70-phan-docker

zuul/parameter_functions.py
@@ -68,6 +68,7 @@ def set_parameters(item, job, params):
         'mwskin-php70-phan',
         'mwext-phpunit-coverage',
         'mediawiki-quibble',
+        'release-quibble',
         'quibble',
         )
     if job.name.startswith(mw_deps_jobs_starting_with):

tests/test_zuul_parameters.py
@@ -49,3 +49,15 @@ class TestZuulSetParameters(unittest.TestCase):
         self.assertIn('EXT_DEPENDENCIES', params)
         self.assertIn('mediawiki/extensions/AbuseFilter\\n',
                       params['EXT_DEPENDENCIES'])
+
+    def test_release_quibble_jobs_get_dependencies(self):
+        job = FakeJob('release-quibble-anything')
+        params = {
+            'ZUUL_PROJECT': 'mediawiki/extensions/Translate',
+            'ZUUL_PIPELINE': 'test',
+            'ZUUL_BRANCH': 'REL1_31',
+            }
+        set_parameters(None, job, params)
+        self.assertIn('EXT_DEPENDENCIES', params)
+        self.assertIn('mediawiki/extensions/UniversalLanguageSelector\\n',
+                      params['EXT_DEPENDENCIES'])

tests/test_zuul_scheduler.py
@@ -962,6 +962,8 @@ class TestZuulScheduler(unittest.TestCase):
             'mediawiki-quibble-vendor-mysql-hhvm-docker': True,
             'mediawiki-quibble-composertest-php70-docker': True,
             'mediawiki-core-hhvmlint': True,
+            'release-quibble-vendor-mysql-hhvm-docker': False,
+            'release-quibble-vendor-mysql-php70-docker': False,
             'wmf-quibble-vendor-mysql-hhvm-docker': False,
         }
         expected_gate = {
@@ -978,6 +980,8 @@ class TestZuulScheduler(unittest.TestCase):
             'mediawiki-quibble-vendor-mysql-hhvm-docker': True,
             'mediawiki-quibble-composertest-php70-docker': True,
             'mediawiki-core-hhvmlint': True,
+            'release-quibble-vendor-mysql-hhvm-docker': False,
+            'release-quibble-vendor-mysql-php70-docker': False,
             'wmf-quibble-vendor-mysql-hhvm-docker': False,
             'wmf-quibble-vendor-mysql-php70-docker': False,
         }
@@ -1023,6 +1027,10 @@ class TestZuulScheduler(unittest.TestCase):
             'mediawiki/extensions/AbuseFilter',
             'gate-and-submit',
             'mediawiki-extensions-hhvm-jessie')
+        release_job = self.getJob(
+            'mediawiki/extensions/AbuseFilter',
+            'gate-and-submit',
+            'release-quibble-vendor-mysql-php70-docker')
 
         change = zuul.model.Change('mediawiki/extensions/AbuseFilter')
 
@@ -1034,6 +1042,9 @@ class TestZuulScheduler(unittest.TestCase):
         self.assertFalse(
             gate_job.changeMatches(change),
             'gate job is no more used on wmf branches')
+        self.assertFalse(
+            release_job.changeMatches(change),
+            'release job is not for wmf branches')
 
         change.branch = 'master'
         self.assertTrue(
@@ -1042,15 +1053,21 @@ class TestZuulScheduler(unittest.TestCase):
         self.assertFalse(
             gate_job.changeMatches(change),
             'gate job is no more used on master branch')
+        self.assertFalse(
+            release_job.changeMatches(change),
+            'release job is not for master branch')
 
         # But not the release branches
         change.branch = 'REL1_42'
         self.assertFalse(
             wmf_quibble_job.changeMatches(change),
             'wmf-quibble jobs are not for REL branches')
-        self.assertTrue(
+        self.assertFalse(
             gate_job.changeMatches(change),
-            'gate job is still used on REL branches')
+            'gate job is no more used on REL branches')
+        self.assertTrue(
+            release_job.changeMatches(change),
+            'release jobs run on REL branches')
 
     def test_wmf_quibble_is_not_ready_for_core_or_vendor(self):
         # On core/vendor Quibble runs all tests which fails
@@ -1071,3 +1088,33 @@ class TestZuulScheduler(unittest.TestCase):
                 self.assertTrue(
                     gate_job.changeMatches(change),
                     'gate job is still used on core/vendor')
+
+    def test_gated_extension_behavior(self):
+        repo = 'mediawiki/extensions/Translate'
+
+        wmf_quibble_job = self.getJob(
+            repo, 'gate-and-submit',
+            'wmf-quibble-vendor-mysql-php70-docker')
+        gate_job = self.getJob(
+            repo, 'gate-and-submit',
+            'mediawiki-extensions-hhvm-jessie')
+        release_job = self.getJob(
+            repo, 'gate-and-submit',
+            'release-quibble-vendor-mysql-php70-docker')
+
+        change = zuul.model.Change(repo)
+
+        change.branch = 'master'
+        self.assertTrue(wmf_quibble_job.changeMatches(change))
+        self.assertFalse(gate_job.changeMatches(change))
+        self.assertFalse(release_job.changeMatches(change))
+
+        change.branch = 'wmf/1.99.9-wmf.999'
+        self.assertTrue(wmf_quibble_job.changeMatches(change))
+        self.assertFalse(gate_job.changeMatches(change))
+        self.assertFalse(release_job.changeMatches(change))
+
+        change.branch = 'REL1_42'
+        self.assertFalse(wmf_quibble_job.changeMatches(change))
+        self.assertFalse(gate_job.changeMatches(change))
+        self.assertTrue(release_job.changeMatches(change))