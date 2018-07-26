91118a339418b9c54135a7bdb94913d24768af12
Valentin Gutierrez <vgutierrez@wikimedia.org>
2018-07-26 14:48:17 +0200

    Provide a container with pebble for certcentral
    Bug: T200405
    Change-Id: I880859cdbaa0ad57cf8ec41beb51847c9392489a

---
+++ dockerfiles/tox-certcentral/example-run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+declare -i err=0
+
+install --mode 2777 --directory log
+for project in analytics/quarry/web labs/tools/crosswatch; do
+    docker run \
+        --rm --tty \
+        --env JENKINS_URL=1 \
+        --env ZUUL_URL=https://gerrit.wikimedia.org/r \
+        --env ZUUL_PROJECT="$project" \
+        --env ZUUL_COMMIT=master \
+        --env ZUUL_REF=master \
+        --volume /"$(pwd)"/log://log \
+        docker-registry.wikimedia.org/releng/tox:latest
+    err+=$?
+done
+
+# Ensure we can compile 'netifaces' and a wheel is hold in the cache
+docker run --rm --tty \
+    --entrypoint=/bin/bash docker-registry.wikimedia.org/releng/tox:latest \
+    -c 'pip install --disable-pip-version-check --target /src/install --cache-dir /src/cache -v netifaces && find /src/cache -name "netifaces-*.whl"'
+err+=$?
+
+if [ $err == 0 ]; then
+    echo "[OK] all examples passed"
+else
+    echo "[KO] $err example(s) failed"
+    exit 1
+fi
