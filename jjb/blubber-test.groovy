d5e7987498d122b68beb0eb7b5c8f563ed5f2b72
Dan Duvall <dduvall@wikimedia.org>
2018-07-31 12:27:33 -0700

    Provide a generic job for testing via Blubber
    Any project for which this job is scheduled to run can provide a
    `.pipeline/blubber.yaml` file which defines a test image variant and
    entry point.
    
    Bug: T200843
    Change-Id: I31b367f2ab8b897fa988e70222f70d84186a631b

---
+++ jjb/blubber-test.groovy
@@ -0,0 +1,19 @@
+@Library('wikimedia-integration-pipelinelib') import org.wikimedia.integration.*
+
+node('blubber') {
+  def patchset = PatchSet.fromZuul(params)
+  def runner = new PipelineRunner(this)
+  def imageID
+
+  stage('Checkout patch') {
+    checkout(patchset.getSCM())
+  }
+
+  stage('Build image') {
+    imageID = runner.build('test')
+  }
+
+  stage('Run tests') {
+    runner.run(imageID)
+  }
+}
