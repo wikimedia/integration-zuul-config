aa8bd4c94d23402fd3fe6c25cbbffd78daf84210
Addshore <addshorewiki@gmail.com>
2018-08-13 13:33:02 +0000

    Revert "Temporarily exclude WikibaseLexeme from jobs including Wikibase"
    This reverts commit 4de949baaeacf1044922d63c9590b7e9cac1cc2b.
    
    Bug: T200508
    Change-Id: Idab7aba13dfda499323ea5e046a6b9af106ba715

--- zuul/parameter_functions.py
+++ zuul/parameter_functions.py
@@ -338,7 +338,8 @@ dependencies = {
         'PropertySuggester',
         'WikibaseQuality',
         'WikibaseQualityConstraints',
-        'WikimediaBadges'
+        'WikimediaBadges',
+        'WikibaseLexeme'
     ],
     'WikibaseJavaScriptApi': ['Wikibase'],
     'WikibaseLexeme': ['Wikibase'],
