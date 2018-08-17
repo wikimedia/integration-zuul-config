63a8c9a5554eab4c530377c00fc51cc5013dffa0
James D. Forrester <jforrester@wikimedia.org>
2018-08-17 10:48:30 -0700

    dockerfiles: Add npm6-browser-test docker image
    Change-Id: I34fd79ddf7e97ff93f686662d555a0387f2103a1

---
+++ dockerfiles/npm6-browser-test/example-run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -euo pipefail
+
+mkdir -m 777 -p cache log src
+(
+cd src
+git init
+git fetch --quiet --depth 1 https://gerrit.wikimedia.org/r/data-values/value-view
+git checkout FETCH_HEAD
+)
+
+docker run \
+    --rm --tty \
+    --volume /"$(pwd)"/log://var/lib/jenkins/log \
+    --volume /"$(pwd)"/cache://cache \
+    --volume /"$(pwd)"/log://log \
+    --volume /"$(pwd)"/src://src \
+    docker-registry.wikimedia.org/releng/npm-browser-test:latest
