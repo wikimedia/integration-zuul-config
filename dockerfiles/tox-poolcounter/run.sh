04f5324cd1881267db4baab19d0415903373c678
Kunal Mehta <legoktm@member.fsf.org>
2018-08-28 22:34:05 -0700

    Add tox-poolcounter image
    Installs build-essential and libevent-dev to build poolcounter, and then
    runs `make` before `tox`.
    
    Change-Id: I83f1f4e51e60181de20081fbbf28aa79fc1e1861

---
+++ dockerfiles/tox-poolcounter/run.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+umask 002
+
+LOG_DIR=/log
+export LOG_DIR
+
+capture_tox_logs() {
+    # XXX unlike `mv`, `cp` does not preserve permissions and hence the
+    # destination files will inherit the group thanks to log having setgid.
+    cp --recursive /src/.tox/*/log/*.log "${LOG_DIR}" || /bin/true
+    cp --recursive /src/.tox/log "${LOG_DIR}" || /bin/true
+}
+
+cd /src
+
+git init
+git fetch --depth 2 --quiet "${ZUUL_URL}/${ZUUL_PROJECT}" "$ZUUL_REF"
+git checkout --quiet FETCH_HEAD
+git submodule --quiet update --init --recursive
+
+make
+
+trap capture_tox_logs EXIT
+
+function relay_signals() {
+    for signal ; do
+        trap 'kill -$signal $tox_pid; wait $tox_pid' "$signal"
+    done
+}
+
+# Run tests.
+# Pass all environment variables to tox since the environment here is already
+# pretty restrictive.
+# tox is backgrounded in bash job control to let bash handles traps (eg
+# SIGTERM) immediately. That is merely to capture the testenv log files.
+TOX_TESTENV_PASSENV="*" PY_COLORS=1 tox -v "${@}" &
+tox_pid=$!
+relay_signals SIGINT SIGTERM
+wait "$tox_pid"
