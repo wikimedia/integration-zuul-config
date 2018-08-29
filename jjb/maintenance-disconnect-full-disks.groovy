dbc0116c1fedb1acedde7c5b92c9d04e400f5a2c
Tyler Cipriani <tcipriani@wikimedia.org>
2018-08-29 12:37:03 -0600

    Maintenance: disconnect full disk notifications
    Since post-build actions are not supported by pipeline projects, this
    implements the publishers in groovy.
    
    Change-Id: I46455cc429f7eb3bebd261760ed6a9597dde08fc

--- jjb/maintenance-disconnect-full-disks.groovy
+++ jjb/maintenance-disconnect-full-disks.groovy
@@ -1,3 +1,4 @@
+import hudson.plugins.ircbot.v2.IRCConnectionProvider;
 import hudson.slaves.OfflineCause
 import hudson.util.RemotingDiagnostics
 
@@ -8,6 +9,7 @@ def computerNamePrefix = 'integration-slave-'
 def offlineMessage = 'Offline due to full partition'
 
 def failed = false
+def failedMessage = "Project ${env.JOB_NAME} build ${env.BUILD_NUMBER}"
 
 def diskFull = { diskSize ->
     for (line in diskSize.split('\n')) {
@@ -23,40 +25,67 @@ def diskFull = { diskSize ->
 }
 
 node('contint1001') {
-    stage('Check agents') {
-        for (slave in hudson.model.Hudson.instance.slaves) {
-            def computer = slave.computer
-            def computerName = computer.getName()
+    try {
+        stage('Check agents') {
+            for (slave in hudson.model.Hudson.instance.slaves) {
+                def computer = slave.computer
+                def computerName = computer.getName()
 
-            // Only check nodes that are named like integration-agents
-            if (!computerName.startsWith(computerNamePrefix)) {
-                continue
-            }
+                // Only check nodes that are named like integration-agents
+                if (!computerName.startsWith(computerNamePrefix)) {
+                    continue
+                }
 
-            println "Checking ${computerName}..."
+                println "Checking ${computerName}..."
 
-            if (computer.isOffline()) {
-                if (computer.getOfflineCauseReason() == offlineMessage) {
-                    println "${computerName} /srv or / FULL! Already offline."
+                if (computer.isOffline()) {
+                    if (computer.getOfflineCauseReason() == offlineMessage) {
+                        println "${computerName} /srv or / FULL! Already offline."
+                        failed = true
+                    }
+                    continue
+                }
+
+                def channel = computer.getChannel()
+                def diskSize = RemotingDiagnostics.executeGroovy(groovyScript, channel)
+
+                if (diskFull(diskSize)) {
+                    println "${computerName} /srv or / FULL! Taking offline..."
+                    computer.setTemporarilyOffline(true,
+                        new OfflineCause.ByCLI(offlineMessage))
                     failed = true
                 }
-                continue
-            }
-
-            def channel = computer.getChannel()
-            def diskSize = RemotingDiagnostics.executeGroovy(groovyScript, channel)
-
-            if (diskFull(diskSize)) {
-                println "${computerName} /srv or / FULL! Taking offline..."
-                computer.setTemporarilyOffline(true,
-                    new OfflineCause.ByCLI(offlineMessage))
-                failed = true
             }
         }
-    }
-    stage('Agents are OK') {
-        if (failed) {
-            currentBuild.result = 'FAILURE'
+        stage('Agents are OK') {
+            if (failed) {
+                currentBuild.result = 'FAILURE'
+            }
+        }
+    } finally {
+        stage('Send notifications') {
+            def currentResult = currentBuild.result ?: 'SUCCESS'
+            def previousResult = currentBuild.previousBuild?.result
+            def changed = previousResult != null && previousResult != currentResult
+            def isFailure = currentBuild.resultIsWorseOrEqualTo('FAILURE')
+
+            if (isFailure) {
+
+                // First failure
+                if (changed) {
+                    println "Sending first failure email..."
+                    mail to: 'qa-alerts@lists.wikimedia.org',
+                        subject: "${failedMessage}: ${currentResult}"
+                        body: "${env.BUILD_URL}"
+                        attachLog: false
+                }
+                println "Notifying in IRC"
+                IRCConnectionProvider.getInstance().currentConnection().send(
+                    '#wikimedia-releng',
+                    "${failedMessage}: ${currentResult}"
+                )
+            }
+
         }
     }
 }
