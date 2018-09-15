de53646def6446213482f9eb07bb39b47c4d23c6
Dan Duvall <dduvall@wikimedia.org>
2018-09-15 16:52:54 -0700

    Refactor service-pipeline job to use integration/pipelinelib
    Much of the implementation of service-pipeline was factored out into a
    separate Groovy library.
    
    Bug: T196940
    Change-Id: I5df2b72fa411adf05027c8f6745284e7e1dedf7a

--- jjb/service-pipeline.groovy
+++ jjb/service-pipeline.groovy
@@ -1,23 +1,15 @@
+@Library('wikimedia-integration-pipelinelib') import org.wikimedia.integration.*
+
 node(nodeLabel) {
-  def blubberConfig = '.pipeline/blubber.yaml'
-  def helmConfig = '.pipeline/helm.yaml'
-  def servicePort = 31000 + env.EXECUTOR_NUMBER.toInteger()
+  def patchset = PatchSet.fromZuul(params)
+  def runner = new PipelineRunner(this)
+  def imageID
 
-  def dockerCredential = 'docker-registry-uploader'
-  def dockerRepository = 'wikimedia'
+  def imageName = params.ZUUL_PROJECT.replaceAll(/\//, '-')
+  def timestamp = new Date().format("yyyyMMddHHmmss", TimeZone.getTimeZone("UTC"))
 
-  def name = params.ZUUL_PROJECT.replaceAll(/\//, '-')
-  def shortName = name.split('-').last()
-
-  def tag = new Date().format("yyyyMMddHHmmss", TimeZone.getTimeZone("UTC"))
-
-  def tagTest = "${tag}-test"
-  def tagCandidate = "${tag}-candidate"
-  def tagProduction = "${tag}-production"
-
-  def testImage = "$dockerRegistry/$dockerRepository/$name:$tagTest"
-  def candidateImage = "$dockerRegistry/$dockerRepository/$name:$tagCandidate"
-  def productionImage = "$dockerRegistry/$dockerRepository/$name:$tagProduction"
+  def candidateTag = "${timestamp}-candidate"
+  def productionTag = "${timestamp}-production"
 
   def imageLabels = [
     "zuul.commit=${params.ZUUL_COMMIT}",
@@ -25,110 +17,50 @@ node(nodeLabel) {
     "jenkins.build=${env.BUILD_ID}"
   ]
 
-  Map scm = [
-    $class: 'GitSCM',
-    userRemoteConfigs: [[
-      url: "${params.ZUUL_URL}/${params.ZUUL_PROJECT}",
-      refspec: params.ZUUL_REF
-    ]],
-    branches: [[
-      name: params.ZUUL_COMMIT
-    ]],
-    extensions: [
-      [$class: 'WipeWorkspace'],
-      [$class: 'CloneOption', shallow: true],
-      [$class: 'SubmoduleOption', recursiveSubmodules: true]
-    ]
-  ]
-
-  // Shell argument quoting ripped off from
-  // https://issues.jenkins-ci.org/browse/JENKINS-44231#comment-323802
-  //
-  def arg = { s -> "'" + s.replace("'", "'\\''") + "'" }
-
-  def buildImage = { variant, imageName ->
-    def labels = imageLabels.collect { "--label ${arg(it)}" }.join(' ')
-    sh "set -o pipefail; blubber $blubberConfig $variant | docker build --pull --tag ${arg(imageName)} $labels --file - ."
-  }
-
-  def runImage = { imageName ->
-    timeout(time: 20, unit: 'MINUTES') {
-      sh "exec docker run --rm ${arg(imageName)}"
-    }
-  }
-
-  def publishImage = { imageName ->
-    sh "sudo /usr/local/bin/docker-pusher ${arg(imageName)}"
-  }
-
-  def tagImage = { sourceImage, targetImage ->
-    sh "docker tag ${sourceImage} ${targetImage}"
-  }
-
-  def cleanImage = { imageName ->
-    sh "docker rmi ${arg(imageName)}"
-  }
-
-  def cleanImages = { imageNames ->
-    imageNames.each { cleanImage(it) }
-  }
-
-  def testDeployment = {
-    def config = readYaml(file: helmConfig)
-
-    assert config.chart : "you must define 'chart: <helm chart url>' in ${helmConfig}"
-
-    def release = "${shortName}-${tagCandidate}"
-    def timeout = 120
-    def overrides = [
-      "docker.registry=${dockerRegistry}",
-      "main_app.image=${dockerRepository}/${name}",
-      "main_app.version=${tagCandidate}",
-      "service.port=${servicePort}",
-    ].collect { "--set ${arg(it)}" }.join(' ')
-
-    try {
-      sh "KUBECONFIG=/etc/kubernetes/ci-staging.config helm --tiller-namespace=ci install --namespace=ci ${overrides} -n ${arg(release)} --debug --wait --timeout ${timeout} ${arg(config.chart)}"
-      sh "KUBECONFIG=/etc/kubernetes/ci-staging.config helm --tiller-namespace=ci test --cleanup ${arg(release)}"
-    } finally {
-      sh "KUBECONFIG=/etc/kubernetes/ci-staging.config helm --tiller-namespace=ci delete --purge ${arg(release)}"
-    }
-  }
-
   stage('Checkout patch') {
-    checkout(scm)
+    checkout(patchset.getSCM())
   }
 
   stage('Build test image') {
-    buildImage('test', testImage)
+    imageID = runner.build('test', imageLabels)
   }
 
   stage('Run test image') {
-    runImage(testImage)
+    runner.run(imageID)
+
+    // Clean up images on production machines
+    if (pushProductionImage) {
+      runner.removeImage(imageID)
+    }
   }
 
-  // Build a candidate production image to be tested in the CI namespace
   if (testProductionImage || pushProductionImage) {
-    stage('Build production image') {
-      buildImage('production', candidateImage)
+    stage('Build candidate image') {
+      imageID = runner.build('production', imageLabels)
     }
   }
 
   if (testProductionImage) {
-    publishImage(candidateImage)
     stage('Test deployment') {
-      testDeployment()
+      runner.registerAs(imageID, imageName, candidateTag)
+
+      def release = runner.deploy(imageName, candidateTag)
+
+      try {
+        runner.testRelease(release)
+      } finally {
+        runner.purgeRelease(release)
+      }
     }
   }
 
   if (pushProductionImage) {
-    // Retag the candidate image as the final production image
-    tagImage(candidateImage, productionImage)
     stage('Register production image') {
-      publishImage(productionImage)
+      try {
+        runner.registerAs(imageID, imageName, productionTag)
+      } finally {
+        runner.removeImage(imageID)
+      }
     }
-
-    // Don't keep images on production machine
-    cleanImages([testImage, candidateImage, productionImage])
   }
 }
