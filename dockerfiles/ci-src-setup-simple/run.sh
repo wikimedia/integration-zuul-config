c25582f18028defc89c3e3d13fd1b8c2628fa290
Tyler Cipriani <tcipriani@wikimedia.org>
2018-10-04 15:13:45 -0600

    ci-src-setup-simple: Add a layer of depth
    To test only what has changed for a given patchset we need a bit more
    history, otherwise git show HEAD gives you the entire repository.
    
    Change-Id: Ie24a4100bdf0fb9058623bc83836f5a5dc9a9e32
    Signed-off-by: Dan Duvall <dduvall@wikimedia.org>

--- dockerfiles/ci-src-setup-simple/run.sh
+++ dockerfiles/ci-src-setup-simple/run.sh
@@ -7,7 +7,7 @@ set -euxo pipefail
 cd /src
 
 git init
-git fetch --quiet --depth 1 "${ZUUL_URL}/${ZUUL_PROJECT}" "$ZUUL_REF"
+git fetch --quiet --depth 2 "${ZUUL_URL}/${ZUUL_PROJECT}" "$ZUUL_REF"
 git checkout FETCH_HEAD
 
 set +x
