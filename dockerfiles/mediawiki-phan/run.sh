2c58b46961697d7ecd96caa68412cc6b3a072082
Kunal Mehta <legoktm@member.fsf.org>
2018-10-12 21:33:48 -0700

    Move check for existence of tests/phan/config.php into docker image
    Bug: T206893
    Change-Id: I6254ae4ba7d15c2733f12c8dd1aaae2168b69004

--- dockerfiles/mediawiki-phan/run.sh
+++ dockerfiles/mediawiki-phan/run.sh
@@ -4,4 +4,9 @@ set -euxo pipefail
 
 umask 002
 
+if [ ! -f "/mediawiki/$THING_SUBNAME/tests/phan/config.php" ]; then
+    echo "Phan config not found\n"
+    exit 0
+fi
+
 /mediawiki/tests/phan/bin/phan $@
