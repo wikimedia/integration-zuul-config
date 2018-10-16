6825b3f6d9d298d20dc19f75327cbe5b604ccd2a
Kunal Mehta <legoktm@member.fsf.org>
2018-10-15 20:47:01 -0700

    docker: Add composer security-check.sh script
    Change-Id: Ib05c4475d1f75a8b4faeb7b2074dcf12a077c9e6

---
+++ dockerfiles/composer/security-check.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+umask 002
+
+set -euxo pipefail
+
+cd /src
+
+# If there's no lock file, then generate one
+if [ ! -f "composer.lock" ]; then
+    composer install --no-progress --prefer-dist
+fi
+
+curl -i -H "Accept: text/plain" https://security.sensiolabs.org/check_lock -F lock=@composer.lock -o sensiolabs.check
+cat sensiolabs.check && grep -F "X-Alerts: 0" sensiolabs.check
