f046fd027a7b5f4f6734e45779c7a42d417db5c5
Tyler Cipriani <tcipriani@wikimedia.org>
2018-10-22 12:35:36 -0600

    Ensure pipeline images are cleaned in production
    During a quick cleanup on contint1001 today, I noticed a lot of
    blubber-created images hanging around. This should eliminate the ways by
    which images have been avoiding deletion.
    
    Change-Id: Iade47fbc32ad853e4e021b069ad58d4c10996fed

--- jjb/service-pipeline.groovy
+++ jjb/service-pipeline.groovy
@@ -5,7 +5,8 @@ node(nodeLabel) {
   def runner = new PipelineRunner(this,
                                   kubeConfig: "/etc/kubernetes/ci-staging.config",
                                   registry: dockerRegistry)
-  def imageID
+  def testImageID
+  def productionImageID
 
   def imageName = params.ZUUL_PROJECT.replaceAll(/\//, '-')
   def timestamp = new Date().format("yyyyMMddHHmmss", TimeZone.getTimeZone("UTC"))
@@ -23,50 +24,47 @@ node(nodeLabel) {
     checkout(patchset.getSCM())
   }
 
-  stage('Build test image') {
-    imageID = runner.build('test', imageLabels)
-  }
+  try {
+    stage('Build test image') {
+      testImageID = runner.build('test', imageLabels)
+    }
 
-  stage('Run test image') {
-    try {
-      runner.run(imageID)
-    } finally {
-      // Clean up test images on production machines
-      if (pushProductionImage) {
-        runner.removeImage(imageID)
+    stage('Run test image') {
+      runner.run(testImageID)
+    }
+
+    if (testProductionImage || pushProductionImage) {
+      stage('Build candidate image') {
+        productionImageID = runner.build('production', imageLabels)
       }
     }
-  }
 
-  if (testProductionImage || pushProductionImage) {
-    stage('Build candidate image') {
-      imageID = runner.build('production', imageLabels)
-    }
-  }
+    if (testProductionImage) {
+      stage('Test deployment') {
+        runner.registerAs(productionImageID, imageName, candidateTag)
+        def release = runner.deploy(imageName, candidateTag)
 
-  if (testProductionImage) {
-    stage('Test deployment') {
-      runner.registerAs(imageID, imageName, candidateTag)
-      def release = runner.deploy(imageName, candidateTag)
-
-      try {
-        runner.testRelease(release)
-      } catch (exception) {
-        runner.removeImage(imageID)
-        throw exception
-      } finally {
-        runner.purgeRelease(release)
+        try {
+          runner.testRelease(release)
+        } catch (exception) {
+          runner.removeImage(productionImageID)
+          throw exception
+        } finally {
+          runner.purgeRelease(release)
+        }
       }
     }
-  }
 
-  if (pushProductionImage) {
-    stage('Register production image') {
-      try {
-        runner.registerAs(imageID, imageName, productionTag)
-      } finally {
-        runner.removeImage(imageID)
+    if (pushProductionImage) {
+      stage('Register production image') {
+        runner.registerAs(productionImageID, imageName, productionTag)
       }
+    }
+  } finally {
+    // Clean up test images on production machines
+    if (pushProductionImage) {
+      runner.removeImage(testImageID)
+      runner.removeImage(productionImageID)
     }
   }
 }
