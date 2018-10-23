b9af45633ebc5b6693bf9dda1573a63e00e89142
Kunal Mehta <legoktm@member.fsf.org>
2018-10-23 16:33:25 -0700

    Don't shell out for test_have_no_tabs
    This seems to be a few seconds faster on my system.
    
    This is unlikely to make a major difference since the real slowdown is
    test_integration.py.
    
    Change-Id: Id22b272fcaaa8c9bf8629c2f95e0baae1d384b0f

--- tests/test_files_structure.py
+++ tests/test_files_structure.py
@@ -1,15 +1,20 @@
-import subprocess
+import os
 import unittest
 
 
 class TestFilesStructure(unittest.TestCase):
 
     def test_have_no_tabs(self):
-        try:
-            out = subprocess.check_output([
-                'git', 'grep', '-n', '-I', '-P', '\t'])
-            print(out)
-            raise AssertionError('Files have tabulations. Check output.')
-        except subprocess.CalledProcessError as e:
-            self.assertEquals(1, e.returncode,
-                              'when there is no tabs: git grep exit 1')
+        root_dir = os.path.dirname(os.path.dirname(__file__))
+        for root, dirs, files in os.walk(root_dir):
+            for name in files:
+                if name.endswith(('.pyc', '.gpg')):
+                    # Dunno why these files contain tabs
+                    continue
+                fname = os.path.join(root, name)
+                with open(fname) as f:
+                    if '\t' in f.read():
+                        raise AssertionError('%s has tabs' % fname)
+            for name in dirs:
+                if name.startswith(('.', '__pycache__')):
+                    dirs.remove(name)
