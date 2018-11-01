913e2e1524c8caa22e34b82a9728888b8c9bd62b
Kunal Mehta <legoktm@member.fsf.org>
2018-11-01 08:08:04 -0700

    mediawiki-tarball: Redo filesystem layout
    Change-Id: Ibf70c807ae438edd47e4ffee176bd2aa73dbb91c

--- dockerfiles/mediawiki-tarball/run.sh
+++ dockerfiles/mediawiki-tarball/run.sh
@@ -9,16 +9,10 @@ git pull
 
 python3 -m virtualenv -p python3 venv
 source venv/bin/activate
+# TODO: Debianize so we're not trusting pypi
 pip install git-archive-all==1.18.2
 
 # Default to master if no branch set
 ZUUL_BRANCH=${ZUUL_BRANCH:-"master"}
 
-# FIXME does this use the git cache properly?
-git clone https://gerrit.wikimedia.org/r/mediawiki/core
-cd core && git submodule update --init
-cd /src
-
-mkdir dist
-./make-release/makerelease2.py /src/core $ZUUL_BRANCH --output-dir dist
-
+/opt/release/make-release/makerelease2.py /src $ZUUL_BRANCH --output-dir /dist
