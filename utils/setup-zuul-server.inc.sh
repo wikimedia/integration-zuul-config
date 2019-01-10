d746b083573c20ff9ed262ec22ef52d802d997f8
Antoine Musso <hashar@free.fr>
2019-01-10 11:52:55 +0100

    Port zuul layout related jobs to Docker
    Import utils/jenkins-jobs-list.py from integration/jenkins history was:
    
      Antoine Musso (1):
      * [0843024] jenkins-jobs-list.py dump list of jenkins jobs
      John Vandenberg (1):
      * [7a163b9] Fix Python 3 syntax and import errors
    
    Copy the logic of integration-zuul-layoutvalidation-gate and
    integration-zuul-layoutdiff to shell scripts under the new directory
    ./utils/
    
    The scripts would attempt to install the zuul_tests tox environment to
    provide the proper zuul-server version. Logic is shared in
    utils/setup-zuul-server-inc.sh
    
    For Zuul diff, when zuul/layout.yaml has modifications staged and/or in
    the working space, do the diff against the latest commit. That is
    usefull when hacking locally.
    Else lookup the previous commit to have altered zuul/layout.yaml, shows
    its sha1 and do a diff against that.
    When HEAD has not altered zuul/layout.yaml, do generate the layout (so
    one can inspect log/current.txt.gz, but skip the diff.
    
    We need a full clone, like integration-jjb-config-diff-docker and the
    two zuul commands would be executed in the docker container. Create a
    template that let us just vary the entrypoint.
    
    Create jobs:
    - integration-config-jjb-diff-docker
    - integration-config-zuul-layout-diff-docker
    - integration-config-zuul-layout-validate-docker
    
    The new jjb-diff job will eventually replae the current
    integration-jjb-config-diff-docker.
    
    Add them to the experimental pipeline.
    
    Bug: T210279
    Change-Id: I750df25742b61875bdf67fd0af072037cc07a9f7

---
+++ utils/setup-zuul-server.inc.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -eu
+
+_dir="$(dirname "$0")"
+_repodir="$(realpath "$_dir/..")"
+unset _dir
+
+ZUUL_SERVER_BIN=zuul-server
+if [ ! -v TOX_ENV_DIR ]; then
+    echo "Setting up zuul_tests tox environment"
+    tox -qq -e zuul_tests --notest
+    ZUUL_SERVER_BIN="$_repodir"/.tox/zuul_tests/bin/zuul-server
+fi
+unset _repodir
+
+export ZUUL_SERVER_BIN
