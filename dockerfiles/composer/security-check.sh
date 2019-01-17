c8a640c6a509867c517aff1c7d4dad0fcb515105
Kunal Mehta <legoktm@member.fsf.org>
2019-01-16 23:02:11 -0800

    Use php-security-checker.wmflabs.org for composer security checks
    Bug: T206442
    Change-Id: Ia04f3eb9453345ab7a667e2995a0c64808f2b7d9

--- dockerfiles/composer/security-check.sh
+++ dockerfiles/composer/security-check.sh
@@ -11,5 +11,5 @@ if [ ! -f "composer.lock" ]; then
     composer install --no-progress --prefer-dist
 fi
 
-curl -i -H "Accept: text/plain" https://security.sensiolabs.org/check_lock -F lock=@composer.lock -o sensiolabs.check
-cat sensiolabs.check && grep -iF "X-Alerts: 0" sensiolabs.check
+curl -i -H "Accept: text/plain" https://php-security-checker.wmflabs.org/check_lock -F lock=@composer.lock -o results.check
+cat results.check && grep -iF "X-Alerts: 0" results.check
