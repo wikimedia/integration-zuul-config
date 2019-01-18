84fca6ecd6404ae151480b28e40ba8486976b92b
Kunal Mehta <legoktm@member.fsf.org>
2019-01-18 11:27:37 -0800

    mediawiki-phan: Support newer versions of php-ast and phan
    Phan is hardcoded at 0.8.0, which will be 2 years old in a few days. We
    need to upgrade to get to a more useful version that has a smarter parser,
    better features, etc.
    
    But at the same time we need to do this in a non-breaking way, as we'll need
    to continue supporting older versions of phan that are in repositories that
    haven't been upgraded yet or when testing older branches.
    
    It's not possible to just upgrade php-ast to a version supported by modern
    versions of phan because of <https://github.com/phan/phan/issues/1134>, which
    prevents usage with any "deprecated" AST version.
    
    THe docker image now builds php-ast straight from source, and installs
    ast_012.so and ast_101.so. We introduce a small shell wrapper around
    /usr/bin/php to be able to pass e.g. -dextension=ast_012.so through an
    environment variable if we need the older version.
    
    Extensions will need to make some changes to upgrade:
    * Set .extra.phan in composer.json for the specific phan version (just like
      phan-taint-check-plugin)
    * Move the config file to .phan/config.php, which is where upstream expects
      it by default.
    * Switch from @suppress on individual lines using our post-process wrapper
      to upstream's new @phan-suppress-next-line:
      <https://github.com/phan/phan/wiki/Annotating-Your-Source-Code#phan-suppress-next-line>
    
    Migrating MediaWiki core to the new version will be taken care of once we've
    moved over a few extensions and found no new significant regressions.
    
    Bug: T174339
    Change-Id: I5001b6afec5db547d8afc178f4cfb59ae767f259

--- dockerfiles/mediawiki-phan/run.sh
+++ dockerfiles/mediawiki-phan/run.sh
@@ -4,9 +4,44 @@ set -euxo pipefail
 
 umask 002
 
-if [ ! -f "/mediawiki/$THING_SUBNAME/tests/phan/config.php" ]; then
-    echo "Phan config not found\n"
-    exit 0
+cd "/mediawiki/$THING_SUBNAME"
+
+if [ ! -f "/mediawiki/$THING_SUBNAME/.phan/config.php" ]; then
+    # Look in old location
+    if [ ! -f "/mediawiki/$THING_SUBNAME/tests/phan/config.php" ]; then
+        echo "Phan config not found"
+        exit 0
+    fi
 fi
 
-/mediawiki/tests/phan/bin/phan $@
+function install_phan {
+    # Subshell so we don't mess with path
+    (cd /srv/phan && composer require "phan/phan:$PHAN_VERSION")
+}
+
+# First look for phan version in root composer.json
+if jq -e '.extra."phan"' composer.json; then
+    PHAN_VERSION=$(jq -r '.extra."phan"' composer.json)
+else
+    # Then look for it via mediawiki-phan-config
+    CFG_COMPOSER="vendor/mediawiki/mediawiki-phan-config/composer.json"
+    if jq -e '.extra."phan"' $CFG_COMPOSER; then
+        PHAN_VERSION=$(jq -r '.extra."phan"' $CFG_COMPOSER)
+    else
+        # Old-style, fallback to 0.8
+        PHAN_VERSION="0.8"
+    fi
+fi
+
+if [ -f .phan/config.php ]; then
+    # new-style phan, using modern paths and newer ast
+    export PHP_ARGS='-dextension=ast_101.so'
+    install_phan
+    exec /srv/phan/vendor/bin/phan -d . -p
+else
+    # old-style, using tests/phan and MW wrapper
+    export PHP_ARGS='-dextension=ast_012.so'
+    install_phan
+    exec /mediawiki/tests/phan/bin/phan "$@"
+fi
+
