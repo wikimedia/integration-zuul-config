ecc75a170af7886e8ba72515cbedbdb4ab1c3ccd
Tyler Cipriani <tcipriani@wikimedia.org>
2019-02-25 13:10:31 -0700

    Train notes: automatically upload changelog
    When a new branch is cut matching the pattern:
    
        refs/heads/wmf/1.<minor-version+wmf-version>
    
    We should create changelog notes for this version and upload them to the
    appropriate spot on MediaWiki.org. Currently, this is a manual task
    where you have to provide the old and new version numbers to a job and
    the job makes the changelog and uploads it.
    
    To automatically create and upload changelog notes, we determine the two
    most recent versions via "git ls-remote". If the most recent branch is
    the branch being created, then we compare it with the next-most-recent
    branch to build a changelog.
    
    Change-Id: I9fed4726dece4036a17dc62d6e680bd8dd9f9424

--- dockerfiles/release-notes/run.sh
+++ dockerfiles/release-notes/run.sh
@@ -3,14 +3,40 @@
 set -euxo pipefail
 
 umask 002
-
 LOG_DIR=/log
 export LOG_DIR
 
-python3 /src/make-deploy-notes/makedeploynotes.py \
+MW_CORE='https://gerrit.wikimedia.org/r/mediawiki/core'
+
+# Strip "refs/heads/wmf" from "$ZUUL_REF" so we're just left with just a
+# version number like "1.33.0-wmf.18"
+ZUUL_REF="${ZUUL_REF/refs\/heads\/wmf\/}"
+
+# Get current sorted upstream mediawiki versions
+mapfile -t VERSIONS < <(git ls-remote -h "$MW_CORE" | \
+    awk '/wmf\// {gsub("refs/heads/wmf/", ""); print $2}' | \
+    sort --version-sort --reverse)
+
+if [[ "${VERSIONS[0]}" != "${ZUUL_REF}" ]]; then
+    echo "${ZUUL_REF} is not the latest version, taking no action..."
+    exit 1
+fi
+
+OLD_VERSION="${VERSIONS[1]}"
+NEW_VERSION="${VERSIONS[0]}"
+
+echo "Building Changelog for ${OLD_VERSION}..${NEW_VERSION}"
+
+# Update the mediawiki/tools/release repo, envvar set by dockerfile
+git -C "$TOOLS_RELEASE_DIR" pull --quiet origin master
+git -C "$TOOLS_RELEASE_DIR" submodule --quiet update --init --recursive
+
+# Actually make deploy notes
+python3 "$TOOLS_RELEASE_DIR"/make-deploy-notes/makedeploynotes.py \
     "$OLD_VERSION" "$NEW_VERSION" > \
         "$LOG_DIR/deploy-notes-${NEW_VERSION}"
 
-php /src/make-deploy-notes/jenkinsUploadChangelog.php \
+# Upload deploy notes
+php "$TOOLS_RELEASE_DIR"/make-deploy-notes/jenkinsUploadChangelog.php \
     "$NEW_VERSION" \
     "$LOG_DIR/deploy-notes-${NEW_VERSION}"
