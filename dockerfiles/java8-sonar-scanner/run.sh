b828c480c4e670b491457e4d5160c5b113219e2f
Kosta Harlan <kharlan@wikimedia.org>
2019-03-05 15:35:24 -0500

    Remove requirement for properties file, import coverage if present
    * Add NodeJS for eslint bridge and CSS analysis
    * Attempt analysis without sonar-project.properties file definition
    * Import PHP and JS code coverage if it exists
    * sonar-scanner version to 3.3.0
    
    Bug: T215176
    Bug: T209849
    Bug: T208522
    Change-Id: I1fd34783df52c6988cb1267ea43bdb9b1806c4de

--- dockerfiles/java8-sonar-scanner/run.sh
+++ dockerfiles/java8-sonar-scanner/run.sh
@@ -6,4 +6,21 @@ set -o pipefail
 umask 002
 
 set +x
+
+if [ ! -f /src/sonar-project.properties ] && [ ! -f /src/.sonar-project.properties ]; then
+    # Create directories if they don't already exist, so that sonar-scanner doesn't throw an error
+    mkdir -p /src/resources /src/includes /src/src /src/modules /src/maintenance /src/tests
+    touch /src/sonar-project.properties
+    echo "sonar.sources=includes,src,modules,maintenance" >> /src/sonar-project.properties
+    echo "sonar.test=tests" >> /src/sonar-project.properties
+    # If test coverage exists, add this to the properties file.
+    if [ -f /log/coverage/lcov.info ]; then
+        echo "sonar.javascript.lcov.reportPaths=/log/coverage/lcov.info" >> /src/sonar-project.properties
+    fi
+    if [ -f /log/coverage/junit.xml ] && [ -f /log/coverage/clover.xml ]; then
+        echo "sonar.php.tests.reportPath=/log/coverage/junit.xml" >> /src/sonar-project.properties
+        echo "sonar.php.coverage.reportPaths=/log/coverage/clover.xml" >> /src/sonar-project.properties
+    fi
+fi
+
 exec /opt/sonar-scanner/bin/sonar-scanner -Dsonar.login="$SONAR_API_KEY" "$@"
