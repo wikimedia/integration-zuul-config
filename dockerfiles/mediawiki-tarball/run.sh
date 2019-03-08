7ef20f25be8cd1dfe4cb9e6956ac59eb3db42e69
Mark A. Hershberger <mah@nichework.com>
2019-03-07 20:16:36 -0500

    Start work on docker-driven make to produce tarballs
    The tarball work was already done, just using Legoktm's tarball docker
    image for this.
    
    Change-Id: I16541e689467e57da105a429063d365ded5eb49a

--- dockerfiles/mediawiki-tarball/run.sh
+++ dockerfiles/mediawiki-tarball/run.sh
@@ -7,17 +7,7 @@ set -exo pipefail
 cd /opt/release
 # Pull in any new changes since image rebuild
 git pull
-cd /src
-# Clean up any composer stuffs
-git clean -ffdx
-git submodule update --init
 
-python3 -m virtualenv -p python3 /opt/release/venv
-source /opt/release/venv/bin/activate
-# TODO: Debianize so we're not trusting pypi
-pip install git-archive-all==1.18.2 requests
-
+cd /opt/release-driver
 # Default to master if no branch set
-ZUUL_BRANCH=${ZUUL_BRANCH:-"master"}
-
-/opt/release/make-release/makerelease2.py --output_dir /dist /src $ZUUL_BRANCH
+make ${TARGET:-"tarball"} workDir=/src releaseVer=${RELEASE_VERSION:-""} rebuildOkay=${REBUILD_OK:-"false"}
