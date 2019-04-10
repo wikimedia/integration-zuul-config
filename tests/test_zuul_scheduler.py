8a422edd07bdcb6e2a9b7e35e937a36dff55d0d2
Timo Tijhof <krinklemail@gmail.com>
2019-04-10 15:58:43 +0100

    Use php72 instead of php70 (5) - for mw postgres job
    See also Ic7dbc4e3b5b.
    
    This job have has 1 php variant. That variant is now php72 instead of php70.
    
    Need to re-order filters because a more general regex is now matching
    as well.
    
    Change-Id: I0e658bff57dd298c14a4a77c8d55856fa3cb885b

--- tests/test_zuul_scheduler.py
+++ tests/test_zuul_scheduler.py
@@ -975,7 +975,7 @@ class TestZuulScheduler(unittest.TestCase):
             'mediawiki-quibble-vendor-mysql-hhvm-docker': True,
             'mediawiki-quibble-composertest-php70-docker': True,
             'mediawiki-quibble-vendor-sqlite-php72-docker': True,
-            'mediawiki-quibble-vendor-postgres-php70-docker': True,
+            'mediawiki-quibble-vendor-postgres-php72-docker': True,
             'release-quibble-vendor-mysql-hhvm-docker': False,
             'release-quibble-vendor-mysql-php55-docker': False,
             'release-quibble-vendor-mysql-php70-docker': False,
@@ -1104,7 +1104,7 @@ class TestZuulScheduler(unittest.TestCase):
         repo = 'mediawiki/core'
         job = self.getJob(
             repo, 'gate-and-submit',
-            'mediawiki-quibble-vendor-postgres-php70-docker')
+            'mediawiki-quibble-vendor-postgres-php72-docker')
 
         change = zuul.model.Change(repo)
 
