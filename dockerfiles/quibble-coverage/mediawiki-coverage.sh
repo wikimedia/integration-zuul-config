b70c969362d839eecf36344341fb88427d61392b
Kosta Harlan <kharlan@wikimedia.org>
2019-06-28 10:28:20 -0400

    Codehealth: Adjust mediawiki-coverage script for SonarQube
    Use composer phpunit:coverage (introduced in
    Ie717b0ecf4fcfd089d46248f14853c80b7ef4a76) when in codehealth pipeline context,
    and make other adjustments (similar to mwext-phpunit-coverage.sh) for SonarQube.
    
    Change-Id: I21e5d93f120c65067805797bbc8ad92f3ec11385

--- dockerfiles/quibble-coverage/mediawiki-coverage.sh
+++ dockerfiles/quibble-coverage/mediawiki-coverage.sh
@@ -12,11 +12,17 @@ function relay_signals() {
 # failure. Normal test failures are not fatal and PHPUnit will still
 # generate console output, clover reports and html reports.
 set +e
+if [[ ! -v CODEHEALTH ]]; then
 php -d zend_extension=xdebug.so \
     tests/phpunit/phpunit.php \
         --exclude-group Dump,Broken,ParserFuzz,Stub \
         --coverage-clover "$LOG_DIR"/clover.xml \
         --coverage-html "$WORKSPACE/cover" &
+else
+  composer phpunit:coverage -- \
+  --coverage-clover "$LOG_DIR"/clover.xml \
+  --log-junit "$LOG_DIR"/junit.xml &
+fi
 cover_pid=$!
 relay_signals SIGINT SIGTERM
 wait "$cover_pid"
@@ -25,9 +31,18 @@ set -e
 # Make sure a coverage report has actually been generated. Else the
 # publishing step will end up syncing an empty directory, which deletes
 # the coverage report from doc.wikimedia.org.
-test -f "$WORKSPACE"/cover/index.html
+# If we're not operating the in the codehealth pipeline context, check to see
+# if the HTML coverage report was generated. If it was not, exit with a failure.
+if [[ ! -v CODEHEALTH ]]; then
+  test -f "$WORKSPACE"/cover/index.html
+fi
 
-if [ -s "$LOG_DIR"/clover.xml ]; then
+# In codehealth pipeline context, make necessary modifications to junit.xml
+if [[ -v CODEHEALTH ]] && [ -f "$LOG_DIR/junit.xml" ]; then
+  phpunit-junit-edit "$LOG_DIR/junit.xml"
+fi
+
+if [ -s "$LOG_DIR"/clover.xml ] && [[ ! -v CODEHEALTH ]] ; then
     # Since clover file is huge, compress it before archiving
     # We need to keep the original though for the cloverphp plugin
     gzip --best --keep "$LOG_DIR"/clover.xml
@@ -40,4 +55,9 @@ if [ -s "$LOG_DIR"/clover.xml ]; then
 
     # Publish a compressed form of the xml clover report
     gzip --best --stdout "$LOG_DIR"/clover.xml > "$WORKSPACE"/cover/clover.xml.gz
+else
+  # Codehealth pipeline context, use the raw clover file.
+  if [ -s "$LOG_DIR"/clover.xml ]; then
+    cp "$LOG_DIR"/clover.xml "$WORKSPACE"/cover/clover.xml
+  fi
 fi
