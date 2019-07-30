987eb37df14efe28aefce37e3524825ec35f613d
Giuseppe Lavagetto <glavagetto@wikimedia.org>
2019-07-30 18:05:22 +0200

    operations-puppet: allow to use to run tests locally
    Change-Id: Ic6f024ff53c389bfb92c5e9b1511242c2562b5eb

--- dockerfiles/operations-puppet/run.sh
+++ dockerfiles/operations-puppet/run.sh
@@ -7,32 +7,64 @@ RAKE_TARGET=${RAKE_TARGET:-test}
 LOG_DIR="/srv/workspace/log"
 export LOG_DIR
 
+TMP_PUPPET_DIR="/tmp/local-puppet-copy"
+export TMP_PUPPET_DIR
+
 capture_logs() {
     # Save logs
     mv "${PUPPET_DIR}"/.tox/*/log/*.log "${LOG_DIR}/" || /bin/true
     mv "${PUPPET_DIR}"/.tox/log/* "${LOG_DIR}/" || /bin/true
 }
 
-trap capture_logs EXIT
+local_cleanup() {
+    rm -rf "$TMP_PUPPET_DIR"
+}
 
-cd "$PUPPET_DIR"
 
-# Prepare patch set from zuul merger
-git remote add zuul "${ZUUL_URL}/${ZUUL_PROJECT}"
-git pull --quiet zuul production
-git fetch --quiet zuul "$ZUUL_REF"
-git checkout --quiet FETCH_HEAD
-git submodule --quiet update --init --recursive
+execute() {
+    # Update bundle if gemfile changed
+    if git diff --name-only docker-head Gemfile | grep -q 'Gemfile'; then
+        bundle update
+    fi;
+    # To force color output on non tty
+    export TOX_TESTENV_PASSENV='PY_COLORS'
+    export PY_COLORS=1
+    export SPEC_OPTS='--tty'
 
-# Update bundle if gemfile changed
-if git diff --name-only docker-head Gemfile | grep -q 'Gemfile'; then
-    bundle update
-fi;
+    # Run tests
+    bundle exec rake "${RAKE_TARGET}"
+}
 
-# To force color output on non tty
-export TOX_TESTENV_PASSENV='PY_COLORS'
-export PY_COLORS=1
-export SPEC_OPTS='--tty'
+execute_ci() {
+    trap capture_logs EXIT
+    cd "$PUPPET_DIR"
+    # Prepare patch set from zuul merger
+    git remote add zuul "${ZUUL_URL}/${ZUUL_PROJECT}"
+    git pull --quiet zuul production
+    git fetch --quiet zuul "$ZUUL_REF"
+    git checkout --quiet FETCH_HEAD
+    git submodule --quiet update --init --recursive
+    execute | tee "${LOG_DIR}/rake.log"
+}
 
-# Run tests
-bundle exec rake "${RAKE_TARGET}" | tee "${LOG_DIR}/rake.log"
+execute_local() {
+    set +x
+    trap local_cleanup exit
+    ORIGIN=${ORIGIN:-/src}
+    # get a copy of the local working directory
+    echo "Copying your working copy to the destination"
+    mkdir -p $TMP_PUPPET_DIR && cd $TMP_PUPPET_DIR
+    tar --ignore-failed-read -C "$ORIGIN" -c  .  | tar -xf -
+    # Reproduce the docker-head tag
+    TAG_REF=$(cd "$PUPPET_DIR" && git show-ref docker-head | cut -d\  -f1)
+    git tag docker-head "$TAG_REF"
+    execute
+}
+
+
+if [ -n "$ZUUL_REF" ]; then
+    execute_ci
+else
+    # Local copy. Output to stdout and don't capture logs
+    execute_local
+fi
