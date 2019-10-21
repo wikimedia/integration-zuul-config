f51e0c8cc89dc852daf5d00ab3df63142572f742
Antoine Musso <hashar@free.fr>
2019-10-21 15:12:22 +0200

    Use proper jjb version when diffing
    The integration-config-jjb-diff-docker job uses the jjb version from the
    change. The parent change configuration is thus generated with the same
    version, that prevents us from catching differences introduced when
    upgrading JJB.
    
    Install jjb using the parent context.
    Also display the jjb version.
    
    Bug: T236046
    Change-Id: I45e8ea2ee6b03e1ba46a5aa2821a49a87a30fa85

--- jjb/diff.sh
+++ jjb/diff.sh
@@ -4,9 +4,10 @@ set -eu -o pipefail
 tox -e jenkins-jobs --notest
 
 base_dir=$(realpath "$(dirname "$0")"/../)
-JJB_BIN="$base_dir"/.tox/jenkins-jobs/bin/jenkins-jobs
+
+JJB_BIN=.tox/jenkins-jobs/bin/jenkins-jobs
 DIFF_HIGHLIGHT_BIN="$base_dir"/.tox/jenkins-jobs/bin/diff-highlight
-JJB_CONF="$base_dir"/tests/fixtures/jjb-disable-query-plugins.conf
+JJB_CONF=tests/fixtures/jjb-disable-query-plugins.conf
 JJB_TEST="$JJB_BIN --conf $JJB_CONF -l warning test"
 
 test_dir=$(mktemp -d --tmpdir jjbdiff.XXXX)
@@ -15,12 +16,18 @@ trap 'echo Deleting "$test_dir"; rm -R "$test_dir"' EXIT
 mkdir -p "$test_dir"/{output-parent,output-proposed}
 
 echo "Generating config for proposed patchset..."
-$JJB_TEST "$base_dir"/jjb -o "$test_dir"/output-proposed
+(cd "$base_dir"
+ $JJB_BIN --version
+ $JJB_TEST ./jjb -o "$test_dir"/output-proposed)
 
 echo "Generating config for parent patchset..."
 parent_config=$(mktemp -d --tmpdir)
-git archive HEAD^ jjb|tar -C "$parent_config" -x
-$JJB_TEST "$parent_config"/jjb -o "$test_dir"/output-parent
+git archive HEAD^|tar -C "$parent_config" -x
+(cd "$parent_config"
+ tox -e jenkins-jobs --notest
+ $JJB_BIN --version
+ $JJB_TEST "$parent_config"/jjb -o "$test_dir"/output-parent
+)
 
 echo "--------------------------------------------"
 echo " File changed"
