f3c7cf0fe8eb0975d98bd0be3e44a9cb905d95fc
James D. Forrester <jforrester@wikimedia.org>
2019-12-03 08:17:35 -0800

    layout: [MediaWiki] Add PHP74 tests as voting for master
    This now passes in master, but is known to be broken in REL1_34 and
    earlier, so those are left as they were.
    
    Bug: T233012
    Change-Id: Ia5bcac7063d3317ecae8233a34c5201b21ec8280

--- tests/test_zuul_scheduler.py
+++ tests/test_zuul_scheduler.py
@@ -1112,6 +1112,7 @@ class TestZuulScheduler(unittest.TestCase):
             'mediawiki-core-jsduck-docker': True,
             'mediawiki-core-php72-phan-docker': True,
             'mediawiki-quibble-composer-mysql-php72-docker': True,
+            'mediawiki-quibble-vendor-mysql-php74-docker': True,
             'mediawiki-quibble-vendor-mysql-php73-docker': True,
             'mediawiki-quibble-vendor-mysql-php72-docker': True,
             'mediawiki-quibble-composertest-php72-docker': True,
