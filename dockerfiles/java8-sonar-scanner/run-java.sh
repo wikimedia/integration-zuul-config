c43368946857805e1ead8f8e424fa934cab2950d
Maryum Styles <mstyles@wikimedia.org>
2019-12-19 19:23:26 -0600

    dockerfiles: [java8-sonar-scanner] Add zuul branch properties
    Add zuul branch properties for sonar analysis to
    ensure that analysis run after change requests is
    done properly. Also, a maven clean install line
    was added to ensure that the project is properly
    compiled prior to analysis.
    
    Bug: T238004
    Change-Id: Id8b80c04d11d7e342caa0bd9b6b64da6c8edb613

--- dockerfiles/java8-sonar-scanner/run-java.sh
+++ dockerfiles/java8-sonar-scanner/run-java.sh
@@ -7,9 +7,15 @@ umask 002
 
 set +x
 
-# # run sonar analysis using maven
-./mvnw -gs /settings.xml sonar:sonar
+# run clean and install to compile
+./mvnw -gs /settings.xml clean install
 
+# run sonar analysis using maven and send extra args if not on master branch
+if [[ $ZUUL_BRANCH = "master" ]]; then
+  ./mvnw -gs /settings.xml sonar:sonar
+else
+  ./mvnw -gs /settings.xml sonar:sonar -Dsonar.branch.target="$ZUUL_BRANCH" -Dsonar.branch.name="$ZUUL_CHANGE-$ZUUL_PATCHSET"
+fi
 # Analysis is sent via a webhook from SonarQube to a web application (SonarQube Bot)
 # and the bot will comment in gerrit with Verified +1 for success or comment only
 # for failure.
