e835fa6fd3f9249bdf39e63a0b5cc5eb4d1e81b0
Maryum Styles <mstyles@wikimedia.org>
2020-01-15 17:47:52 -0800

    dockerfiles: [java8-sonar-scanner] add sonar flag
    Add sonar flag for java projects that allows
    the sonar bot to comment on changes made from
    the master branch
    
    Bug: T238004
    Change-Id: I29f7c2c53c42f9b5f90e72315bd1fbff9c072273

--- dockerfiles/java8-sonar-scanner/run-java.sh
+++ dockerfiles/java8-sonar-scanner/run-java.sh
@@ -10,12 +10,9 @@ set +x
 # run clean and install to compile
 ./mvnw -gs /settings.xml clean install
 
-# run sonar analysis using maven and send extra args if not on master branch
-if [[ $ZUUL_BRANCH = "master" ]]; then
-  ./mvnw -gs /settings.xml sonar:sonar
-else
-  ./mvnw -gs /settings.xml sonar:sonar -Dsonar.analysis.gerritProjectName="$ZUUL_PROJECT" -Dsonar.branch.target="$ZUUL_BRANCH" -Dsonar.branch.name="$ZUUL_CHANGE-$ZUUL_PATCHSET"
-fi
+# run sonar analysis using maven and send extra args for sonar bot
+./mvnw -gs /settings.xml sonar:sonar -Dsonar.analysis.allowCommentOnMaster="1" -Dsonar.analysis.gerritProjectName="$ZUUL_PROJECT" -Dsonar.branch.target="$ZUUL_BRANCH" -Dsonar.branch.name="$ZUUL_CHANGE-$ZUUL_PATCHSET"
+
 # Analysis is sent via a webhook from SonarQube to a web application (SonarQube Bot)
 # and the bot will comment in gerrit with Verified +1 for success or comment only
 # for failure.
