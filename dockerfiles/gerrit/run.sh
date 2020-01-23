64a0475245a214d0b7832cf58edcd5f7918db950
Paladox <thomasmulhall410@yahoo.com>
2020-01-23 23:59:24 +0000

    Gerrit: remove --incompatible_string_join_requires_strings option
    This is now removed and throws an error.
    
    Change-Id: Ibec84cea7e035b0b3c3d96e29e9f2f923278fad4

--- dockerfiles/gerrit/run.sh
+++ dockerfiles/gerrit/run.sh
@@ -12,8 +12,4 @@ bazel version
 # downloaded artifacts
 export HOME="$XDG_CACHE_HOME"/gerrithome
 
-# incompatible_string_join_requires_strings=false is due to Bazel 0.27.0
-# https://github.com/bazelbuild/bazel/issues/7802
-bazel build \
-    --incompatible_string_join_requires_strings=false \
-    release
+bazel build release
