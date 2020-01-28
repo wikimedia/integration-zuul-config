40852324e46df133d14a41016e4b55e6d2565384
Daimona Eaytoy <daimona.wiki@gmail.com>
2020-01-28 14:58:43 +0100

    dockerfiles: [quibble*] Add pcov and use it for test coverage
    pcov is now installed together with xdebug and used for test coverage.
    This means that xdebug should now be unused.
     - xdebug 2.9 is definitely faster than xdebug 2.7 (the one we're using)
     - pcov is way faster than xdebug 2.9
    
    Hence this should speed up the coverage job a lot (see task for
    estimates).
    
    Bug: T234020
    Change-Id: Icd6bff4f74dcdb82de1b4f6d20024ed68218e3f7

--- dockerfiles/quibble-coverage/mwext-phpunit-coverage-patch.sh
+++ dockerfiles/quibble-coverage/mwext-phpunit-coverage-patch.sh
@@ -23,5 +23,5 @@ cd "$MW_INSTALL_PATH/extensions/$EXT_NAME"
 phpunit-suite-edit "$MW_INSTALL_PATH/tests/phpunit/suite.xml" --cover-extension "$EXT_NAME"
 
 exec phpunit-patch-coverage check \
-    --command "php -d zend_extension=xdebug.so \"\$MW_INSTALL_PATH\"/tests/phpunit/phpunit.php" \
+    --command "php -d zend_extension=pcov.so -d pcov.enabled=1 -d pcov.directory=$PWD -d pcov.exclude='@tests@' \"\$MW_INSTALL_PATH\"/tests/phpunit/phpunit.php" \
     --html "$LOG_DIR"/coverage.html
