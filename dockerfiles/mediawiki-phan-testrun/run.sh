21e91c1528bfbf817244b7496dffc8949bc32b5d
Daimona Eaytoy <daimona.wiki@gmail.com>
2020-02-26 20:13:45 +0100

    dockerfiles: Create a dedicated docker image for phan testrun
    This allows us to use php-ast, and to install all dependencies for core.
    
    Bug: T226117
    Change-Id: I81ca109afed0a1afc97dbe24d79c0797f00c3eb0

---
+++ dockerfiles/mediawiki-phan-testrun/run.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -euxo pipefail
+
+umask 002
+
+cd /src
+
+exec composer remove --dev "mediawiki/mediawiki-phan-config"
+# Some black magic: add mw-phan in the merge-plugin section
+sed -i 's!\(\"include\":\s*\[\)!\1 \"./mediawiki/tools/phan/composer.json\",!' composer.json
+
+# Some blacker magic: re-point phan config in the merge-plugin section
+sed -i 's#vendor/mediawiki/mediawiki-phan-config/#mediawiki/tools/phan/#' .phan/config.php
+
+export PHP_ARGS='-dextension=ast_101.so'
+
+exec composer install
+
+# Bypass expensive Symfony\Component\Console\Terminal::getWidth() (T219114#5084302)
+export COLUMNS=80
+
+exec vendor/bin/phan --long-progress-bar
