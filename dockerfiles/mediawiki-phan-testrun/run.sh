614af71dc220aa8654b0ade05beae313a8d79e86
James D. Forrester <jforrester@wikimedia.org>
2020-02-26 13:50:53 -0800

    Dockerfiles: [mediawiki-phan-testrun] Don't use exec except at the end
    Change-Id: I0070f655e3b1bee0b2a97be05e60c674a77084ad

--- dockerfiles/mediawiki-phan-testrun/run.sh
+++ dockerfiles/mediawiki-phan-testrun/run.sh
@@ -6,7 +6,7 @@ umask 002
 
 cd /src
 
-exec composer remove --dev "mediawiki/mediawiki-phan-config"
+composer remove --dev "mediawiki/mediawiki-phan-config"
 # Some black magic: add mw-phan in the merge-plugin section
 sed -i 's!\(\"include\":\s*\[\)!\1 \"./mediawiki/tools/phan/composer.json\",!' composer.json
 
@@ -15,7 +15,7 @@ sed -i 's#vendor/mediawiki/mediawiki-phan-config/#mediawiki/tools/phan/#' .phan/
 
 export PHP_ARGS='-dextension=ast_101.so'
 
-exec composer install
+composer install
 
 # Bypass expensive Symfony\Component\Console\Terminal::getWidth() (T219114#5084302)
 export COLUMNS=80
