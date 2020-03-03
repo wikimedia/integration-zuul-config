d7a89090f296a820c61df5d7b001350c2a463ca8
James D. Forrester <jforrester@wikimedia.org>
2020-03-03 14:40:48 -0800

    dockerfiles: [mediawiki-phan-testrun] Use composer remove --no-update
    Change-Id: I8f9d35f94b907ed95f3c2fea807c391d8d44e6b2

--- dockerfiles/mediawiki-phan-testrun/run.sh
+++ dockerfiles/mediawiki-phan-testrun/run.sh
@@ -6,7 +6,7 @@ umask 002
 
 cd /src
 
-composer remove --dev "mediawiki/mediawiki-phan-config"
+composer remove --dev --no-update "mediawiki/mediawiki-phan-config"
 # Some black magic: add mw-phan in the merge-plugin section
 sed -i 's!\(\"include\":\s*\[\)!\1 \"./mediawiki/tools/phan/composer.json\",!' composer.json
 
