e550e5b7747a7da38e97264cf35db49ece8f118e
Antoine Musso <hashar@free.fr>
2020-03-26 17:48:09 +0100

    docker: java8 should checkout in a branch
    The Maven release plugin attempts to parse the branch and fails in
    detached state.
    
    Bug: T210271
    Change-Id: I6c2928ccd82d1539c7f27aa120f0d3f5ba487961

--- dockerfiles/java8/run.sh
+++ dockerfiles/java8/run.sh
@@ -8,7 +8,7 @@ cd /src
 
 git init
 git fetch --depth 2 --quiet "${ZUUL_URL}/${ZUUL_PROJECT}" "$ZUUL_REF"
-git checkout --quiet FETCH_HEAD
+git checkout --quiet -b "${ZUUL_BRANCH}" FETCH_HEAD
 git submodule --quiet update --init --recursive
 
 exec /usr/local/bin/mvn "${@}"
