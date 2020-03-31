443cf72a33360ee0916387759f22c1da54afaa04
Antoine Musso <hashar@free.fr>
2020-03-31 19:34:00 +0200

    docker: container for refinery jar updater
    analytics/refinery uses git-fat and refers to jar files in Archiva. The
    jars get updated via the script bin/update-refinery-source-jars. It
    depends on wget and git-fat.
    
    Bug: T210271
    Depends-On: Ib042ba36b70281b5467b9ba68e676c5e7213967a
    Change-Id: I3ff8eb8dee6351848dcae4447dfe33e612173e00

---
+++ dockerfiles/jar-updater/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT=/src/bin/update-refinery-source-jars
+
+if [ ! -x "$SCRIPT" ]; then
+    echo "$SCRIPT not found"
+    echo "You must clone the source repository and volume mount it on /src"
+    exit 1
+
+cd /src
+exec "$SCRIPT"
