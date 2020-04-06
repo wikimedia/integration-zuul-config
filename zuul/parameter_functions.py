8d46cf94e0b408fb3475aaa35e454379112a102d
Reedy <reedy@wikimedia.org>
2020-04-06 17:31:50 +0100

    Don't install EventStreamConfig at all in REL1_3[134]
    Bug: T249514
    Change-Id: I4c90d0ac60a501bc8cf77114a1e634c62814023a

--- zuul/parameter_functions.py
+++ zuul/parameter_functions.py
@@ -634,8 +634,7 @@ def set_mw_dependencies(item, job, params):
     # EventStreamConfig only requires MW >= REL1_35.
     # Therefore remove it from REL1_31, REL1_33, REL1_34. T249514
     if (
-        params['ZUUL_PROJECT'] == 'mediawiki/extensions/EventLogging'
-        and params['ZUUL_BRANCH'] in ['REL1_31', 'REL1_33', 'REL1_34']
+        params['ZUUL_BRANCH'] in ['REL1_31', 'REL1_33', 'REL1_34']
         and 'EventStreamConfig' in ext_deps
     ):
         ext_deps.remove('EventStreamConfig')
