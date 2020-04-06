b06814d4cae070db9193b51bb7101cc3c3a83710
James D. Forrester <jforrester@wikimedia.org>
2020-04-06 09:50:07 -0700

    jjb: Adjust comment mentioning REL1_26 for my sanity
    Change-Id: I9a22a099340b06770de3415e39370c511a01cb71

--- jjb/castor-define-namespace.bash
+++ jjb/castor-define-namespace.bash
@@ -14,11 +14,11 @@ if [[ "$ZUUL_PROJECT" =~ ^mediawiki/(extensions|skins)/ ]]; then
     NS_PROJECT="castor-mw-ext-and-skins"
 fi
 
-# Ex: mediawiki-core/REL1_26/tox-jessie
+# Ex: mediawiki-core/REL1_36/tox-jessie
 # Prefer TRIGGERED_JOB_NAME when it is set
 NS_JOB=${TRIGGERED_JOB_NAME:-$JOB_NAME}
 
-# Ex: mediawiki-core/REL1_26/tox-jessie
+# Ex: mediawiki-core/REL1_36/tox-jessie
 CASTOR_NAMESPACE="${NS_PROJECT}/${NS_BRANCH}/${NS_JOB}"
 export CASTOR_NAMESPACE
 echo "Defined: CASTOR_NAMESPACE=\"$CASTOR_NAMESPACE\""
