83a8528e5b92554f9e657a02ff01eaaa911c865a
Reedy <reedy@wikimedia.org>
2020-04-06 01:01:42 +0100

    dockerfiles: [composer-php70] Replace integration/composer repo with phar
    Replaces composer 1.6.5 from integration/composer with 1.10.5 via composer phar.
    
    Changes: https://github.com/composer/composer/compare/1.6.5...1.10.5
    
    Docker changes created by:
    
    docker-pkg \
            -c dockerfiles/config.yaml \
            --info \
            update \
                    --reason "Replace integration/composer repo with composer.phar" \
                    --version 0.2.0 \
            composer-php70 \
            dockerfiles/
    
    Bug: T249461
    Bug: T249464
    Change-Id: Iecf82b8fbc9d503b35995ddfa6ad0196a2c18184

--- dockerfiles/composer-package-php70/run.sh
+++ dockerfiles/composer-package-php70/run.sh
@@ -7,7 +7,7 @@ set -euxo pipefail
 cd /src
 
 if [ -n "${COMPOSER_GITHUB_OAUTHTOKEN}" ]; then
-  /srv/composer/vendor/bin/composer config -g github-oauth.github.com "${COMPOSER_GITHUB_OAUTHTOKEN}"
+  composer config -g github-oauth.github.com "${COMPOSER_GITHUB_OAUTHTOKEN}"
 fi
 composer --ansi validate
 composer install --no-progress --prefer-dist
--- dockerfiles/composer-php70/run-composer.sh
+++ dockerfiles/composer-php70/run-composer.sh
@@ -3,6 +3,6 @@
 # If COMPOSER_GITHUB_OAUTHTOKEN is set in the environment, that value will be
 # installed as a token to pass to GitHub when fetching package contents.
 if [ -n "${COMPOSER_GITHUB_OAUTHTOKEN}" ]; then
-  /srv/composer/vendor/bin/composer config -g github-oauth.github.com "${COMPOSER_GITHUB_OAUTHTOKEN}"
+  /usr/bin/composer config -g github-oauth.github.com "${COMPOSER_GITHUB_OAUTHTOKEN}"
 fi
-exec /srv/composer/vendor/bin/composer "$@"
+exec /usr/bin/composer "$@"
