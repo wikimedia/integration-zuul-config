7d4b30fa56319ffe2156f97c8bd5f8dffe32eba6
Tyler Cipriani <tcipriani@wikimedia.org>
2020-04-10 10:52:31 -0600

    deploy-notes: Don't checkout core to build notes
    Rather than checkout the zuul revision to determine if
    "DefaultSettings.php" was modified with a new version of mediawiki,
    query gitiles to see if there are changes on the branch that are ahead
    of master.
    
    If so, check that this is a change to the latest branch by querying the
    gerrit repo for mediawiki core.
    
    If this is the latest branch, rebuild deploy notes.
    
    This will rebuild deploy notes when there are backports to the latest
    branch, but, to me, this is a feature not a bug.
    
    Change-Id: Ie0189e9d1ff70ab5a475c4ee777df0a587678dd9

--- dockerfiles/release-notes/run.sh
+++ dockerfiles/release-notes/run.sh
@@ -6,35 +6,18 @@ umask 002
 LOG_DIR=/log
 export LOG_DIR
 
-MW_CORE='https://gerrit.wikimedia.org/r/mediawiki/core'
+GIT='https://gerrit.wikimedia.org/r'
+GITILES='https://gerrit.wikimedia.org/g'
+MW_CORE="${GIT}/mediawiki/core"
+BRANCH="${ZUUL_BRANCH##wmf/}"
 
-# Outputs the version value assigned to $wgVersion
-parse_version() {
-    read -r pattern <<-end
-s/.*["']\([^"']*\)['"];$/\1/
-end
-    sed "$pattern"
-}
+# Check that there are commits on the branch that differ from master
+COMMITS=$(curl -Ls "${GITILES}"'/mediawiki/core/+log/master..'"${ZUUL_BRANCH}"'?format=JSON&no-merges' | \
+            tail -c+6 | \
+            jq '.log[]')
 
-# Shows commit/diff for includes/DefaultSettings.php
-settings_diff() {
-    git -C /src diff HEAD~ HEAD -- includes/DefaultSettings.php
-}
-
-# Check whether wgVersion has changed in DefaultSettings.php
-if ! settings_diff | grep -qm 1 '^-\$wgVersion ='; then
-    echo "wgVersion hasn't changed"
-    echo "taking no action..."
-    exit 1
-fi
-
-# Parse old and new values of wgVersion
-OLD_WG_VERSION="$(settings_diff | grep -m 1 '^-\$wgVersion =' | cut -c 2- | parse_version)"
-NEW_WG_VERSION="$(settings_diff | grep -m 1 '^+\$wgVersion =' | cut -c 2- | parse_version)"
-
-if [[ "${OLD_WG_VERSION##*-}" != "alpha" ]]; then
-    echo "wgVersion has changed from ${OLD_WG_VERSION} which is not an -alpha version"
-    echo "taking no action..."
+if [ -z "$COMMITS" ]; then
+    echo "No commits found for ${ZUUL_BRANCH}. Exiting..."
     exit 1
 fi
 
@@ -43,8 +26,8 @@ mapfile -t VERSIONS < <(git ls-remote -h "$MW_CORE" | \
     awk '/wmf\// {gsub("refs/heads/wmf/", ""); print $2}' | \
     sort --version-sort --reverse)
 
-if [[ "${VERSIONS[0]}" != "${NEW_WG_VERSION}" ]]; then
-    echo "${NEW_WG_VERSION} does not match the latest wmf branch (${VERSIONS[0]})"
+if [[ "${VERSIONS[0]}" != "$BRANCH" ]]; then
+    echo "${ZUUL_BRANCH} does not match the latest wmf branch (${VERSIONS[0]})"
     echo "taking no action..."
     exit 1
 fi
