wikimedia/integration-zuul-config

commit fcf154f24e9785998f518728d9089ece6cfef150

dockerfiles: abstract common git operations
This strike several bugs at once:
--------------------------------------------------------
T256462 - abstract out common logic
T103990 - first phase toward supporting git protocol v2
T141607 - checkout to a branch instead of detached HEAD
T186496 - fetch tags locally
--------------------------------------------------------

We have multiple images all sharing the same logic to fetch and checkout
a patch from CI.  That makes it a bit annoying to update whenever a
change has to be made.

Move the CI repository setup to a new scratch image releng/ci-common.
Change releng/ci-src-setup to use that instead and remove the old
script.

Update the script to support:
* T141607 checking out patch to a local branch
* T186496 have the reference fetched to be represented locally. Notably
  if it is a tag, it means it will be available. The trick is to use:
  +$ZUUL_REF:$ZUUL_REF

Craft a generic gitconfig file which explicitly force protocol.version=2
(T103990) for the git version that support it but do not enable it
default.

Include those bits in all the CI base image (ci-jessie, ci-stretch and
ci-buster).

Change releng/ci-src-setup from Stretch to Buster which upgrade git from
2.11.0 to 2.20.1 and thus get git protocol v2 turned on by default.

All the other images will need to be updated in a follow up change.

Bug: T256462
Bug: T103990
Bug: T141607
Bug: T186496
Change-Id: I8cfbb5aec06b77b65798af5101afc5bc5e3a8096

dockerfiles/ci-common/content/ci-src-setup.sh
@@ -7,8 +7,8 @@ set -euxo pipefail
 cd /src
 
 git init
-git fetch --quiet --depth 2 "${ZUUL_URL}/${ZUUL_PROJECT}" "$ZUUL_REF"
-git checkout FETCH_HEAD
+git fetch --quiet --depth 2 "${ZUUL_URL}/${ZUUL_PROJECT}" "+$ZUUL_REF:$ZUUL_REF"
+git checkout -B "${ZUUL_BRANCH}" FETCH_HEAD
 
 set +x
 if [ -z "${GIT_NO_SUBMODULES:-}" ]; then

dockerfiles/ci-buster/Dockerfile.template
@@ -1,4 +1,9 @@
+FROM {{ "ci-common" | image_tag }} AS ci-common
+
 FROM docker-registry.wikimedia.org/wikimedia-buster:latest
+
+COPY --from=ci-common /utils /utils
+COPY --from=ci-common /utils/gitconfig /etc/gitconfig
 
 # Keep the following in sync with ci-buster
 ARG DEBIAN_FRONTEND=noninteractive

dockerfiles/ci-buster/control
@@ -1,3 +1,4 @@
 Package: ci-buster
+Depends: ci-common
 Description: Base CI image based on Debian Buster.
 Maintainer: Kunal Mehta <legoktm@member.fsf.org>

dockerfiles/ci-common/Dockerfile.template
@@ -1,0 +1,2 @@
+FROM scratch
+COPY /content /utils

dockerfiles/ci-common/content/gitconfig
@@ -1,0 +1,4 @@
+[protocol]
+    version = 2
+[submodule]
+    fetchJobs = 4

dockerfiles/ci-common/control
@@ -1,0 +1,1 @@
+Description: Collection of shared CI utilities

dockerfiles/ci-jessie/Dockerfile.template
@@ -1,4 +1,9 @@
+FROM {{ "ci-common" | image_tag }} AS ci-common
+
 FROM docker-registry.wikimedia.org/wikimedia-jessie:latest
+
+COPY --from=ci-common /utils /utils
+COPY --from=ci-common /utils/gitconfig /etc/gitconfig
 
 # Keep the following in sync with ci-stretch
 ARG DEBIAN_FRONTEND=noninteractive

dockerfiles/ci-jessie/control
@@ -1,3 +1,4 @@
 Package: ci-jessie
+Depends: ci-common
 Description: Base jessie image for CI tasks
 Maintainer: Antoine Musso <hashar@free.fr>

commit 43c33dbde1d38ac793c6cbc1667d5dd16a3a5a93

dockerfiles: allow fetch when ZUUL_REF=master
An empty repository defaults to point to `master`:

 $ git init .
 $ cat .git/HEAD
 ref: refs/heads/master

Hence when ZUUL_REF is `master`, ci-src-setup.sh bails out cause git
fetch tries to update the current branch. I though the leading + in the
refspec will be enough but the current branch is exempted.

Passing `--update-head-ok` to git fetch lets it update the current
branch, which work down to git 2.1.4 from Jessie.

The issue has been introduced by fcf154f24e9 when changing the refspec
to +$ZUUL_REF:ZUUL_REF.

Rebuild the containers that have that script.

Bug: T256759
Change-Id: I66ae094a5fa54f69aa741fd38670d23be32c917e

dockerfiles/ci-common/content/ci-src-setup.sh
@@ -7,7 +7,7 @@ set -euxo pipefail
 cd /src
 
 git init
-git fetch --quiet --depth 2 "${ZUUL_URL}/${ZUUL_PROJECT}" "+$ZUUL_REF:$ZUUL_REF"
+git fetch --quiet --update-head-ok --depth 2 "${ZUUL_URL}/${ZUUL_PROJECT}" "+$ZUUL_REF:$ZUUL_REF"
 git checkout -B "${ZUUL_BRANCH}" FETCH_HEAD
 
 set +x