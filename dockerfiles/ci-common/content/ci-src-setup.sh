b7414ff34a3f49ff8a16c1694b99be5aed40ebf3
Antoine Musso <hashar@free.fr>
2020-08-26 11:54:07 +0200

    dockerfiles: stop cd /src in ci-src-setup.sh
    The script hardcoded 'cd /src', use WORKDIR instead and set it in
    ci-src-setup-simple, doxygen and rake images.  Other images java8,
    lintr, typos already had the proper WORKDIR.
    
    Sibling images of ci-common involves pretty much everything and will be
    updated later on.
    
    Change-Id: Ie166ba860a0abca17719fdbb058cadce723483e5

--- dockerfiles/ci-common/content/ci-src-setup.sh
+++ dockerfiles/ci-common/content/ci-src-setup.sh
@@ -4,8 +4,6 @@ umask 002
 
 set -euxo pipefail
 
-cd /src
-
 git init
 git fetch --quiet --update-head-ok --depth 2 "${ZUUL_URL}/${ZUUL_PROJECT}" "+$ZUUL_REF:$ZUUL_REF"
 git checkout -B "${ZUUL_BRANCH}" FETCH_HEAD
