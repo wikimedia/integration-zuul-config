73101e82915c679e7a889b447e424ddf9dec84bd
Reedy <reedy@wikimedia.org>
2020-09-08 02:55:47 +0100

    Update url to php-security-checker
    Bug: T262236
    Change-Id: Id4db2f04b6b5aab31cf944ae60325ef68bd2d481

--- dockerfiles/composer-php70/security-check.sh
+++ dockerfiles/composer-php70/security-check.sh
@@ -11,5 +11,5 @@ if [ ! -f "composer.lock" ]; then
     composer install --no-progress --prefer-dist
 fi
 
-curl -i -H "Accept: text/plain" https://php-security-checker.wmflabs.org/check_lock -F lock=@composer.lock -o results.check
+curl -i -H "Accept: text/plain" https://php-security-checker.wmcloud.org/check_lock -F lock=@composer.lock -o results.check
 cat results.check && grep -iF "X-Alerts: 0" results.check
