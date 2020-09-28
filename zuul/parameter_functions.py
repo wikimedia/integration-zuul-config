edef3001ef834c9806899bd45a04329e431eb31a
Daimona Eaytoy <daimona.wiki@gmail.com>
2020-09-28 22:24:13 +0000

    Revert "Add Echo and Thanks extensions as dependencies for AbuseFilter"
    This reverts commit 7f49880c463ada6ab1b330d908cebc6deca55942.
    
    Reason for revert: The associated task is stalled, and I believe no progress on that will
    be made in the near future. These dependencies, aside from increasing the build time, make
    the SQLite experimental job fail (actually, *Flow* makes it fail), see T224771. This revert
    can be reverted once the work on the Thanks addition will resume.
    
    Change-Id: I85bbc956731ea1be1f76409ed679e91187024e7a

--- zuul/parameter_functions.py
+++ zuul/parameter_functions.py
@@ -148,7 +148,7 @@ dependencies = {
     # One can add a skin by using: 'skin/XXXXX'
     '3D': ['MultimediaViewer'],
     'AbuseFilter': ['AntiSpoof', 'CentralAuth', 'CodeEditor',
-                    'CheckUser', 'Echo', 'Renameuser', 'Thanks'],
+                    'CheckUser', 'Renameuser'],
     'AdvancedSearch': ['Translate'],
     'ApiFeatureUsage': ['Elastica'],
     'Arrays': ['Loops', 'ParserFunctions', 'Variables'],
