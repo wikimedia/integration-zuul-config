260468be98f35704a9911f632b5548ae80b65d02
James D. Forrester <jforrester@wikimedia.org>
2020-10-18 11:37:47 -0700

    dockerfiles: Drop composer-php70, unused
    Change-Id: Iec520a4cc56d104cc64374bc1f8a2153bcd3a3a4

--- dockerfiles/composer-php70/run-composer.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Entrypoint for running composer.
-# If COMPOSER_GITHUB_OAUTHTOKEN is set in the environment, that value will be
-# installed as a token to pass to GitHub when fetching package contents.
-if [ -n "${COMPOSER_GITHUB_OAUTHTOKEN}" ]; then
-  /usr/bin/composer config -g github-oauth.github.com "${COMPOSER_GITHUB_OAUTHTOKEN}"
-fi
-exec /usr/bin/composer "$@"
