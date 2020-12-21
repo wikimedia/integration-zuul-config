a221b65a6b83de180199e2c6ff9d916be04fa012
Antoine Musso <hashar@free.fr>
2020-12-21 11:45:25 +0100

    zuul: remove no more used PHP_BIN parameter
    The PHP_BIN environment variable was used to switch between php
    versions. That is no more used anywhere, neither in Jenkins jobs nor in
    any Docker images.  Moreover integration/jenkins has been archived.
    
    Bug: T225451
    Change-Id: I5cca8e0ff54043f68002a76f54684cf2d9c103f4

--- zuul/parameter_functions.py
+++ zuul/parameter_functions.py
@@ -20,10 +20,6 @@ def set_parameters(item, job, params):
         # Quibble takes care of it
         del(params['DISPLAY'])
 
-    # Sets a $PHP_BIN variable based on the job name
-    if 'php70' in job.name:
-        params['PHP_BIN'] = 'php7.0'
-
     mw_deps_jobs_starting_with = (
         'mwselenium-quibble',
         'mwext-php72-phan',
