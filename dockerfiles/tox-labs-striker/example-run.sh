da32095e59326e1e93d1270010be57b0fc94bbec
James D. Forrester <jforrester@wikimedia.org>
2020-12-21 16:02:25 -0800

    jjb: switch striker jobs to one based off buster
    Affected jobs:
     - labs-striker-tox-docker
     - labs-tools-ldap-tox-docker
    
    Adjust the image example-run script to run against both repositories
    (labs/tools/ldap and labs/striker).
    
    Change-Id: I46129bd4cd82dd4e265c4d49a6108c9a7886c615

--- dockerfiles/tox-labs-striker/example-run.sh
+++ dockerfiles/tox-labs-striker/example-run.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
+set -eu
+
 install --mode 777 --directory log cache
 
-docker run \
-    --rm --tty \
-    --env JENKINS_URL=1 \
-    --env ZUUL_URL=https://gerrit.wikimedia.org/r \
-    --env ZUUL_PROJECT=labs/striker \
-    --env ZUUL_COMMIT=master \
-    --env ZUUL_REF=master \
-    --volume /"$(pwd)"/log://log \
-    --volume /"$(pwd)"/cache://cache \
-    docker-registry.wikimedia.org/releng/tox-labs-striker:latest
+projects=(labs/striker labs/tools/ldap)
+
+for project in "${projects[@]}"; do
+    printf "=== Running for %s ====\n" "$project"
+    docker run \
+        --rm --tty \
+        --env JENKINS_URL=1 \
+        --env ZUUL_URL=https://gerrit.wikimedia.org/r \
+        --env ZUUL_PROJECT=labs/striker \
+        --env ZUUL_COMMIT=master \
+        --env ZUUL_REF=master \
+        --volume /"$(pwd)"/log://log \
+        --volume /"$(pwd)"/cache://cache \
+        docker-registry.wikimedia.org/releng/tox-labs-striker:latest
+done;
+echo "Successfully ran examples."
