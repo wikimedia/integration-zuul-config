ff90de9220538eac5ef48b49c26b0ddfed514782
Jeena Huneidi <jhuneidi@wikimedia.org>
2021-03-03 13:35:07 -0800

    Define and pass allowed credentials to pipeline
    Adds parsing of allowedCredentials property from
    jjb/project-pipelines.yaml and passes them to the pipeline builder
    in service-pipeline-builder.groovy
    
    Adds globalAllowedCredentials property to the pipeline job template in
    jjb/service-pipeline.yaml, also for passing to the pipeline builder and
    combining with the per pipeline allowed credentials.
    
    Bug: T269900
    Depends-On: I1114d82293f263fcf3d57d80b9d803e052255c11
    Change-Id: Icbbd6698202870fe7f351448b35c56dbf1e4180a

--- jjb/service-pipeline-builder.groovy
+++ jjb/service-pipeline-builder.groovy
@@ -1,4 +1,18 @@
 @Library('wikimedia-integration-pipelinelib') import org.wikimedia.integration.*
 
-def builder = new PipelineBuilder(".pipeline/config.yaml")
+import groovy.json.JsonSlurperClassic
+
+@NonCPS
+def parseJson(jsonString) {
+  def jsonSlurper = new JsonSlurperClassic()
+
+  jsonSlurper.parseText(jsonString)
+}
+
+def globalAllowedCredentials = parseJson('''{{ globalAllowedCredentials | default({}) | tojson }}''')
+def pipelineAllowedCredentials = parseJson('''{{ allowedCredentials | default({}) | tojson }}''')
+
+def allowedCredentials = globalAllowedCredentials + pipelineAllowedCredentials
+
+def builder = new PipelineBuilder([allowedCredentials: allowedCredentials], ".pipeline/config.yaml")
 builder.build(this, params.PLIB_PIPELINE)
